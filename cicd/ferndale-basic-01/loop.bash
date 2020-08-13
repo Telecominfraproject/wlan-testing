6f8c39a2fa2a2cdfb6c66f233b833a47e19db776
Ben Greear <greearb@candelatech.com>
2020-08-13 13:27:49 -0700

    cicd:  Ferndale testbed now uses cloud VM orchestrator.
    And disable cloud-sdk until I can get that working again.

---
+++ cicd/ferndale-basic-01/loop.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+      ../testbed_poll.pl --jfrog_passwd tip-read --jfrog_user tip-read --url http://orch/tip/testbeds/ferndale-basic-01/pending_work/
+      sleep 120
+done
--- testbeds/ferndale-basic-01/test_bed_cfg.bash
+++ testbeds/ferndale-basic-01/test_bed_cfg.bash
@@ -18,7 +18,7 @@ DPT_CFG_FILE=${DPT_CFG_FILE:-$PWD/dpt-pkt-sz.txt}
 SCENARIO_CFG_FILE=${SCENARIO_CFG_FILE:-$PWD/scenario.txt}
 
 # Default to enable cloud-sdk for this testbed, cloud-sdk is at IP addr below
-USE_CLOUD_SDK=${USE_CLOUD_SDK:-192.168.100.164}
+#USE_CLOUD_SDK=${USE_CLOUD_SDK:-192.168.100.164}
 
 # LANforge target machine
 LFMANAGER=${LFMANAGER:-192.168.100.209}
