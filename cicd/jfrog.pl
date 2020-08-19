c2930867162de4102c1f7100c4853ff723138ea3
Ben Greear <greearb@candelatech.com>
2020-08-19 06:28:06 -0700

    jfrog:  Whitespace fix.
    No functional change.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- cicd/jfrog.pl
+++ cicd/jfrog.pl
@@ -176,244 +176,243 @@ for ($z = 0; $z<@platforms; $z++) {
    # Interesting builds are now found in hardware sub-dirs
    my @subdirs = ("trunk", "dev");
    for (my $sidx = 0; $sidx<@subdirs; $sidx++) {
-	   my $sdir = $subdirs[$sidx];
-   my $cmd = "curl -u $user:$passwd $url/$pf/$sdir/";
-   print ("Calling command: $cmd\n");
-   $listing = `$cmd`;
-   @lines = split(/\n/, $listing);
-   for ($i = 0; $i<@lines; $i++) {
-      my $ln = $lines[$i];
-      chomp($ln);
+      my $sdir = $subdirs[$sidx];
+      my $cmd = "curl -u $user:$passwd $url/$pf/$sdir/";
+      print ("Calling command: $cmd\n");
+      $listing = `$cmd`;
+      @lines = split(/\n/, $listing);
+      for ($i = 0; $i<@lines; $i++) {
+         my $ln = $lines[$i];
+         chomp($ln);
 
-      #print("ln -:$ln:-\n");
+         #print("ln -:$ln:-\n");
 
-      if (($ln =~ /href=\"(.*)\">(.*)<\/a>\s+(.*)\s+\S+\s+\S+/)
-          || ($ln =~ /class=\"indexcolname\"><a href=\"(.*.tar.gz)\">(.*)<\/a>.*class=\"indexcollastmod\">(\S+)\s+.*/)) {
-         my $fname = $1;
-         my $name = $2;
-         my $date = $3;
+         if (($ln =~ /href=\"(.*)\">(.*)<\/a>\s+(.*)\s+\S+\s+\S+/)
+                || ($ln =~ /class=\"indexcolname\"><a href=\"(.*.tar.gz)\">(.*)<\/a>.*class=\"indexcollastmod\">(\S+)\s+.*/)) {
+            my $fname = $1;
+            my $name = $2;
+            my $date = $3;
 
-         # Skip header
-         if ($ln =~ /Last modified/) {
-            next;
-         }
-
-         # Skip parent-dir
-         if ($ln =~ /Parent Directory/) {
-            next;
-         }
-
-	 # Skip staging builds
-	 if ($ln =~ /staging/) {
-	    next;
-	 }
-
-	 # Skip dev directory
-	 #if ($ln =~ /href=\"dev\/\">dev\/<\/a>/) {
-	 #   next;
-	 #}
-
-	 #print("line matched -:$ln:-\n");
-	 #print("fname: $fname  name: $name  date: $date\n");
-
-         if ( grep( /^$fname\s+/, @processed ) ) {
-            # Skip this one, already processed.
-            next;
-         }
-
-         my $hw = "";
-         my $fdate = "";
-         my $githash = "";
-
-         if ($fname =~ /^(\S+)-(\d\d\d\d-\d\d-\d\d)-(\S+).tar.gz/) {
-            $hw = $1;
-            $fdate = $2;
-            $githash = $3;
-         } else {
-            print "ERROR:  Un-handled filename syntax: $fname, assuming file-name is hardware name.\n";
-            $hw = $fname;
-         }
-
-         # Find the least used testbed for this hardware.
-         my $dirs = `ls`;
-         my @dira = split(/\n/, $dirs);
-         my $best_tb = "";
-         my $best_backlog = 0;
-         my $di;
-         for ($di = 0; $di<@dira; $di++) {
-            my $dname = $dira[$di];
-            chomp($dname);
-            if (! -d $dname) {
+            # Skip header
+            if ($ln =~ /Last modified/) {
                next;
             }
-            if (! -f "$dname/TESTBED_INFO.txt") {
+
+            # Skip parent-dir
+            if ($ln =~ /Parent Directory/) {
                next;
             }
-            my $tb_info = `cat $dname/TESTBED_INFO.txt`;
-            my $tb_hw_type = "";
-            if ($tb_info =~ /TESTBED_HW=(.*)/g) {
-               $tb_hw_type = $1;
-            }
-            if (!hw_matches($tb_hw_type, $hw)) {
-               print "Skipping test bed $dname, jfrog hardware type: -:$hw:-  testbed hardware type: -:$tb_hw_type:-\n";
+
+            # Skip staging builds
+            if ($ln =~ /staging/) {
                next;
             }
-            print "Checking testbed $dname backlog..\n";
-            my $bklog = `ls $dname/pending_work/$cicd_prefix-*`;
-            my $bklog_count = split(/\n/, $bklog);
-            if ($best_tb eq "") {
-               $best_tb = $dname;
-               $best_backlog = $bklog_count;
+
+            # Skip dev directory
+            #if ($ln =~ /href=\"dev\/\">dev\/<\/a>/) {
+            #   next;
+            #}
+
+            #print("line matched -:$ln:-\n");
+            #print("fname: $fname  name: $name  date: $date\n");
+
+            if ( grep( /^$fname\s+/, @processed ) ) {
+               # Skip this one, already processed.
+               next;
+            }
+
+            my $hw = "";
+            my $fdate = "";
+            my $githash = "";
+
+            if ($fname =~ /^(\S+)-(\d\d\d\d-\d\d-\d\d)-(\S+).tar.gz/) {
+               $hw = $1;
+               $fdate = $2;
+               $githash = $3;
             } else {
-               if ($best_backlog > $bklog_count) {
-                  $best_tb = $dname;
-                  $best_backlog = $bklog_count;
-               }
+               print "ERROR:  Un-handled filename syntax: $fname, assuming file-name is hardware name.\n";
+               $hw = $fname;
             }
-         }
 
-         if ($best_tb eq "") {
-            print "ERROR:  No test bed found for hardware type: $hw\n";
-            last;
-         }
-
-         my $fname_nogz = $fname;
-         if ($fname =~ /(.*)\.tar\.gz/) {
-            $fname_nogz = $1;
-         }
-
-         my @tbs = ($best_tb);
-
-         # For more test coverage, send work to rest of the available test beds as well.
-         if ($duplicate_work) {
+            # Find the least used testbed for this hardware.
+            my $dirs = `ls`;
+            my @dira = split(/\n/, $dirs);
+            my $best_tb = "";
+            my $best_backlog = 0;
+            my $di;
             for ($di = 0; $di<@dira; $di++) {
                my $dname = $dira[$di];
                chomp($dname);
                if (! -d $dname) {
                   next;
                }
-               if ($dname eq $best_tb) {
-                  next;            # processed this one above
-               }
                if (! -f "$dname/TESTBED_INFO.txt") {
                   next;
                }
-
                my $tb_info = `cat $dname/TESTBED_INFO.txt`;
                my $tb_hw_type = "";
                if ($tb_info =~ /TESTBED_HW=(.*)/g) {
                   $tb_hw_type = $1;
                }
-
                if (!hw_matches($tb_hw_type, $hw)) {
                   print "Skipping test bed $dname, jfrog hardware type: -:$hw:-  testbed hardware type: -:$tb_hw_type:-\n";
                   next;
                }
-
-               push(@tbs, "$dname");
-            }
-         }
-
-         my $q;
-         for ($q = 0; $q < @tbs; $q++) {
-            $best_tb = $tbs[$q];
-            my $caseid_fast = "";
-            my $caseid_basic = "";
-
-            my $tb_info = `cat $best_tb/TESTBED_INFO.txt`;
-            if ($tb_info =~ /TESTBED_CASEID_FAST=(.*)/g) {
-               $caseid_fast = $1;
-            }
-            if ($tb_info =~ /TESTBED_CASEID_BASIC=(.*)/g) {
-               $caseid_basic = $1;
-            }
-
-	    
-            my $ttype = "fast";
-	    # Ensure duplicate runs show up individually.
-	    my $extra_run = 0;
-	    if (-e "$best_tb/reports/$ttype/${fname_nogz}") {
-	       $extra_run = 1;
-	       while (-e "$best_tb/reports/$ttype/${fname_nogz}-$extra_run") {
-		  $extra_run++;
-	       }
-            }
-
-	    my $erun = "";
-	    if ($extra_run > 0) {
-	       $erun = "-$extra_run";
-            }
-
-            my $work_fname = "$best_tb/pending_work/$cicd_prefix-$fname_nogz-$ttype";
-            my $work_fname_a = $work_fname;
-
-            system("mkdir -p $best_tb/pending_work");
-            system("mkdir -p $best_tb/reports/$ttype");
-
-            open(FILE, ">", "$work_fname");
-
-            print FILE "CICD_TYPE=$ttype\n";
-            print FILE "CICD_RPT_NAME=$fname_nogz$erun\n";
-            print FILE "CICD_RPT_DIR=$tb_url_base/$best_tb/reports/$ttype\n";
-
-            print FILE "CICD_HW=$hw\nCICD_FILEDATE=$fdate\nCICD_GITHASH=$githash\n";
-            print FILE "CICD_URL=$url/$pf/$sdir\nCICD_FILE_NAME=$fname\nCICD_URL_DATE=$date\n";
-            if ($caseid_fast ne "") {
-               print FILE "CICD_CASE_ID=$caseid_fast\n";
-            }
-
-            close(FILE);
-
-            print("Next: File Name: $fname  Display Name: $name  Date: $date  TType: $ttype\n");
-            print("Work item placed at: $work_fname\n");
-
-
-            $ttype = "basic";
-	    # Ensure duplicate runs show up individually.
-            $extra_run = 0;
-            if (-e "$best_tb/reports/$ttype/${fname_nogz}") {
-               $extra_run = 1;
-               while (-e "$best_tb/reports/$ttype/${fname_nogz}-$extra_run") {
-                  $extra_run++;
+               print "Checking testbed $dname backlog..\n";
+               my $bklog = `ls $dname/pending_work/$cicd_prefix-*`;
+               my $bklog_count = split(/\n/, $bklog);
+               if ($best_tb eq "") {
+                  $best_tb = $dname;
+                  $best_backlog = $bklog_count;
+               } else {
+                  if ($best_backlog > $bklog_count) {
+                     $best_tb = $dname;
+                     $best_backlog = $bklog_count;
+                  }
                }
             }
 
-            $erun = "";
-            if ($extra_run > 0) {
-               $erun = "-$extra_run";
+            if ($best_tb eq "") {
+               print "ERROR:  No test bed found for hardware type: $hw\n";
+               last;
             }
 
-            $work_fname = "$best_tb/pending_work/$cicd_prefix-$fname_nogz-$ttype";
-
-            system("mkdir -p $best_tb/reports/$ttype");
-
-            open(FILE, ">", "$work_fname");
-
-            print FILE "CICD_TYPE=$ttype\n";
-            print FILE "CICD_RPT_NAME=$fname_nogz$erun\n";
-            print FILE "CICD_RPT_DIR=$tb_url_base/$best_tb/reports/$ttype\n";
-
-            print FILE "CICD_HW=$hw\nCICD_FILEDATE=$fdate\nCICD_GITHASH=$githash\n";
-            print FILE "CICD_URL=$url/$pf/$sdir\nCICD_FILE_NAME=$fname\nCICD_URL_DATE=$date\n";
-            if ($caseid_basic ne "") {
-               print FILE "CICD_CASE_ID=$caseid_basic\n";
+            my $fname_nogz = $fname;
+            if ($fname =~ /(.*)\.tar\.gz/) {
+               $fname_nogz = $1;
             }
 
-            close(FILE);
+            my @tbs = ($best_tb);
 
-            print("Next: File Name: $fname  Display Name: $name  Date: $date TType: $ttype\n");
-            print("Work item placed at: $work_fname\n");
-            #print("To download: curl --location -o /tmp/$fname -u $user:$passwd $url/$pf/$fname\n");
-         }                         # for all testbeds
+            # For more test coverage, send work to rest of the available test beds as well.
+            if ($duplicate_work) {
+               for ($di = 0; $di<@dira; $di++) {
+                  my $dname = $dira[$di];
+                  chomp($dname);
+                  if (! -d $dname) {
+                     next;
+                  }
+                  if ($dname eq $best_tb) {
+                     next;      # processed this one above
+                  }
+                  if (! -f "$dname/TESTBED_INFO.txt") {
+                     next;
+                  }
 
-         # Note this one is processed
-         `echo -n "$fname " >> $files_processed`;
-         `date >> $files_processed`;
-      }
+                  my $tb_info = `cat $dname/TESTBED_INFO.txt`;
+                  my $tb_hw_type = "";
+                  if ($tb_info =~ /TESTBED_HW=(.*)/g) {
+                     $tb_hw_type = $1;
+                  }
 
-      #print "$ln\n";
-   }# for all lines in a directory listing
-   } # For all sub directories
+                  if (!hw_matches($tb_hw_type, $hw)) {
+                     print "Skipping test bed $dname, jfrog hardware type: -:$hw:-  testbed hardware type: -:$tb_hw_type:-\n";
+                     next;
+                  }
+
+                  push(@tbs, "$dname");
+               }
+            }
+
+            my $q;
+            for ($q = 0; $q < @tbs; $q++) {
+               $best_tb = $tbs[$q];
+               my $caseid_fast = "";
+               my $caseid_basic = "";
+
+               my $tb_info = `cat $best_tb/TESTBED_INFO.txt`;
+               if ($tb_info =~ /TESTBED_CASEID_FAST=(.*)/g) {
+                  $caseid_fast = $1;
+               }
+               if ($tb_info =~ /TESTBED_CASEID_BASIC=(.*)/g) {
+                  $caseid_basic = $1;
+               }
+
+               my $ttype = "fast";
+               # Ensure duplicate runs show up individually.
+               my $extra_run = 0;
+               if (-e "$best_tb/reports/$ttype/${fname_nogz}") {
+                  $extra_run = 1;
+                  while (-e "$best_tb/reports/$ttype/${fname_nogz}-$extra_run") {
+                     $extra_run++;
+                  }
+               }
+
+               my $erun = "";
+               if ($extra_run > 0) {
+                  $erun = "-$extra_run";
+               }
+
+               my $work_fname = "$best_tb/pending_work/$cicd_prefix-$fname_nogz-$ttype";
+               my $work_fname_a = $work_fname;
+
+               system("mkdir -p $best_tb/pending_work");
+               system("mkdir -p $best_tb/reports/$ttype");
+
+               open(FILE, ">", "$work_fname");
+
+               print FILE "CICD_TYPE=$ttype\n";
+               print FILE "CICD_RPT_NAME=$fname_nogz$erun\n";
+               print FILE "CICD_RPT_DIR=$tb_url_base/$best_tb/reports/$ttype\n";
+
+               print FILE "CICD_HW=$hw\nCICD_FILEDATE=$fdate\nCICD_GITHASH=$githash\n";
+               print FILE "CICD_URL=$url/$pf/$sdir\nCICD_FILE_NAME=$fname\nCICD_URL_DATE=$date\n";
+               if ($caseid_fast ne "") {
+                  print FILE "CICD_CASE_ID=$caseid_fast\n";
+               }
+
+               close(FILE);
+
+               print("Next: File Name: $fname  Display Name: $name  Date: $date  TType: $ttype\n");
+               print("Work item placed at: $work_fname\n");
+
+
+               $ttype = "basic";
+               # Ensure duplicate runs show up individually.
+               $extra_run = 0;
+               if (-e "$best_tb/reports/$ttype/${fname_nogz}") {
+                  $extra_run = 1;
+                  while (-e "$best_tb/reports/$ttype/${fname_nogz}-$extra_run") {
+                     $extra_run++;
+                  }
+               }
+
+               $erun = "";
+               if ($extra_run > 0) {
+                  $erun = "-$extra_run";
+               }
+
+               $work_fname = "$best_tb/pending_work/$cicd_prefix-$fname_nogz-$ttype";
+
+               system("mkdir -p $best_tb/reports/$ttype");
+
+               open(FILE, ">", "$work_fname");
+
+               print FILE "CICD_TYPE=$ttype\n";
+               print FILE "CICD_RPT_NAME=$fname_nogz$erun\n";
+               print FILE "CICD_RPT_DIR=$tb_url_base/$best_tb/reports/$ttype\n";
+
+               print FILE "CICD_HW=$hw\nCICD_FILEDATE=$fdate\nCICD_GITHASH=$githash\n";
+               print FILE "CICD_URL=$url/$pf/$sdir\nCICD_FILE_NAME=$fname\nCICD_URL_DATE=$date\n";
+               if ($caseid_basic ne "") {
+                  print FILE "CICD_CASE_ID=$caseid_basic\n";
+               }
+
+               close(FILE);
+
+               print("Next: File Name: $fname  Display Name: $name  Date: $date TType: $ttype\n");
+               print("Work item placed at: $work_fname\n");
+               #print("To download: curl --location -o /tmp/$fname -u $user:$passwd $url/$pf/$fname\n");
+            }                   # for all testbeds
+
+            # Note this one is processed
+            `echo -n "$fname " >> $files_processed`;
+            `date >> $files_processed`;
+         }
+
+         #print "$ln\n";
+      }# for all lines in a directory listing
+   }# For all sub directories
 }# for all URLs to process
 
 exit 0;
