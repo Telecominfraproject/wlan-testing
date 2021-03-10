57a432eba6d47043345e72643d3ff868d004dceb
lucasrahnTip <lucas.rahn@mail.telecominfraproject.com>
2021-03-10 18:57:33 -0500

    update

--- tests/cicd_sanity/test.py
+++ tests/cicd_sanity/test.py
@@ -1,7 +1,14 @@
 import ap_connect
+from datetime import datetime
+import os
+
+now = datetime.now()
+os.system('mkdir -p AP_Logs/' + now.strftime('%B') + '/' + now.strftime('%d'))
 
 logread_output, dmesg_output = ap_connect.copy_logread_dmesg('192.168.2.150', 'root', 'openwifi')
 
-print(logread_output)
-print('BUFFER--------------------------------------------------------')
-print(dmesg_output)
+with open('AP_Logs/' + now.strftime('%B') + '/' + now.strftime('%d') + '/log_read.log', 'w') as file:
+    file.write(logread_output)
+
+with open('AP_Logs/' + now.strftime('%B') + '/' + now.strftime('%d') + '/dmesg.log', 'w') as file:
+    file.write(dmesg_output)
