664069299ae5c1f13c7693549a5f1276cabe098a
shivamcandela <shivam.thakur@candelatech.com>
2021-05-21 16:50:19 +0530

    wct changes
    Signed-off-by: shivamcandela <shivam.thakur@candelatech.com>

--- tests/e2e/basic/performance_tests/wifi_capacity_test/test_nat_mode.py
+++ tests/e2e/basic/performance_tests/wifi_capacity_test/test_nat_mode.py
@@ -22,27 +22,42 @@ from LANforge.LFUtils import *
     sys.path.append('../py-scripts')'''
 from sta_connect2 import StaConnect2
 from eap_connect import EAPConnect
-
+import allure
 # @pytest.mark.sanity
-@pytest.mark.wifi_capacity_test
-@pytest.mark.wifi5
-@pytest.mark.wifi6
+setup_params_general = {
+    "mode": "NAT",
+    "ssid_modes": {
+        "open": [{"ssid_name": "ssid_open_2g", "appliedRadios": ["is2dot4GHz"]},
+                 {"ssid_name": "ssid_open_5g", "appliedRadios": ["is5GHzU", "is5GHz", "is5GHzL"]}],
+        "wpa": [{"ssid_name": "ssid_wpa_2g", "appliedRadios": ["is2dot4GHz"], "security_key": "something"},
+                {"ssid_name": "ssid_wpa_5g", "appliedRadios": ["is5GHzU", "is5GHz", "is5GHzL"],
+                 "security_key": "something"}],
+        "wpa2_personal": [
+            {"ssid_name": "ssid_wpa2_2g", "appliedRadios": ["is2dot4GHz"], "security_key": "something"},
+            {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["is5GHzU", "is5GHz", "is5GHzL"],
+             "security_key": "something"}]},
+    "rf": {},
+    "radius": False
+}
+
+
+@pytest.mark.basic
+@allure.feature("BRIDGE MODE CLIENT CONNECTIVITY")
 @pytest.mark.parametrize(
-    'setup_profiles, create_profiles',
-    [(["NAT"], ["NAT"])],
+    'setup_profiles',
+    [setup_params_general],
     indirect=True,
     scope="class"
 )
 @pytest.mark.usefixtures("setup_profiles")
-@pytest.mark.usefixtures("create_profiles")
 class TestWifiCapacityNatMode(object):
 
     @pytest.mark.wpa
+    @pytest.mark.mutto
     @pytest.mark.twog
-    def test_client_wpa_2g(self, request, get_lanforge_data, setup_profile_data, instantiate_testrail,
-                           instantiate_project, test_cases):
+    def test_client_wpa_2g(self, get_lanforge_data, test_cases):
         print("")
-        profile_data = setup_profile_data["NAT"]["WPA"]["2G"]
+        profile_data = setup_params_general["ssid_modes"]["wpa"][0]
         lanforge_ip = get_lanforge_data["lanforge_ip"]
         lanforge_port = int(get_lanforge_data["lanforge-port-number"])
         ssid = profile_data["ssid_name"]
@@ -62,7 +77,7 @@ class TestWifiCapacityNatMode(object):
         time.sleep(20)'''
 
         PASS = False
-        WFC_Test = WiFiCapacityTest(lf_host = lanforge_ip,
+        WFC_Test = WiFiCapacityTest(lfclient_host = lanforge_ip,
                                     lf_port = lanforge_port,
                                     lf_user = "lanforge",
                                     lf_password = "lanforge",
