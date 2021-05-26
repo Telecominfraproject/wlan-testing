7a9d9f24e960db70ddf5e51ef2794b16896d02c3
karthikaeyetea <karthika.subramani@candelatech.com>
2021-05-26 12:22:22 +0530

    wifi-capacity VLAN mode

--- tests/e2e/basic/performance_tests/wifi_capacity_test/test_nat_mode.py
+++ tests/e2e/basic/performance_tests/wifi_capacity_test/test_nat_mode.py
@@ -1,10 +1,8 @@
 import pytest
 import sys
 import time
-import lf_wifi_capacity_test
 from lf_wifi_capacity_test import WiFiCapacityTest
-#import create_station
-#from create_station import CreateStation
+from create_station import CreateStation
 pytestmark = [pytest.mark.wifi_capacity_test, pytest.mark.nat]
 
 for folder in 'py-json', 'py-scripts':
@@ -17,13 +15,10 @@ sys.path.append(f'../libs')
 sys.path.append(f'../libs/lanforge/')
 
 from LANforge.LFUtils import *
-
-'''if 'py-json' not in sys.path:
-    sys.path.append('../py-scripts')'''
 from sta_connect2 import StaConnect2
 from eap_connect import EAPConnect
 import allure
-# @pytest.mark.sanity
+
 setup_params_general = {
     "mode": "NAT",
     "ssid_modes": {
@@ -41,8 +36,12 @@ setup_params_general = {
 }
 
 
+def lanforge_ip(args):
+    pass
+
+
 @pytest.mark.basic
-@allure.feature("BRIDGE MODE CLIENT CONNECTIVITY")
+@allure.feature("NAT MODE CLIENT CONNECTIVITY")
 @pytest.mark.parametrize(
     'setup_profiles',
     [setup_params_general],
@@ -55,199 +54,87 @@ class TestWifiCapacityNatMode(object):
     @pytest.mark.wpa
     @pytest.mark.mutto
     @pytest.mark.twog
-    def test_client_wpa_2g(self, get_lanforge_data, test_cases):
-        print("")
+    def test_client_wpa_2g(self, test_cases,lf_test):
+        print("**********  test_client_wpa_2g  ***********")
         profile_data = setup_params_general["ssid_modes"]["wpa"][0]
-        lanforge_ip = get_lanforge_data["lanforge_ip"]
-        lanforge_port = int(get_lanforge_data["lanforge-port-number"])
-        ssid = profile_data["ssid_name"]
-        security_key = profile_data["security_key"]
-        security = "wpa"
-        upstream = get_lanforge_data["lanforge_bridge_port"]
-        radio = get_lanforge_data["lanforge_2dot4g"]
-        station_name = get_lanforge_data["lanforge_2dot4g_station"]
-        '''create_station = CreateStation(_host = lanforge_ip,
-                                       _port = lanforge_port,
-                                       _ssid = ssid,
-                                       _password = security_key,
-                                       _security = security,
-                                       _sta_list = [station_name],
-                                       _radio = radio)
-        create_station.build()
-        time.sleep(20)'''
+        print(profile_data)
 
-        PASS = False
-        WFC_Test = WiFiCapacityTest(lfclient_host = lanforge_ip,
-                                    lf_port = lanforge_port,
-                                    lf_user = "lanforge",
-                                    lf_password = "lanforge",
-                                    instance_name = "wct_instance",
-                                    config_name = "wifi_config",
-                                    upstream = f"1.1.{upstream}",
-                                    batch_size = "1",
-                                    loop_iter = "1",
-                                    protocol = "UDP-IPv4",
-                                    duration = "3000",
-                                    pull_report = True,
-                                    load_old_cfg = False,
-                                    upload_rate = "10Mbps",
-                                    download_rate = "1Gbps",
-                                    sort = "interleave",
-                                    stations = "1.1.sta0000,1.1.sta0001",
-                                    create_stations = True,
-                                    radio = radio,
-                                    security = security,
-                                    paswd = security_key,
-                                    ssid = ssid,
-                                    enables = [],
-                                    disables = [],
-                                    raw_lines = [],
-                                    raw_lines_file = "",
-                                    sets = [],
-                                    )
-        WFC_Test.setup()
-        WFC_Test.run()
-        PASS = True
-        #WFC_Test.check_influx_kpi(args)
+        create_station = CreateStation(_host = lf_test.lanforge_ip,
+                                       _port = lf_test.lanforge_port,
+                                       _ssid = profile_data["ssid_name"],
+                                       _password = profile_data["security_key"],
+                                       _security = "wpa",
+                                       _sta_list = ['1.1.sta0000','1.1.sta0001'],
+                                       _radio = lf_test.twog_radios)
+        create_station.build()
+        time.sleep(20)
+        PASS = lf_test.wifi_capacity(self, ssid = profile_data["ssid_name"], passkey = profile_data["security_key"],
+                              security = "wpa", station_name = [], mode = "NAT", band = "twog",
+                              instance_name = "wct_instance", )
         assert PASS
 
     @pytest.mark.wpa
     @pytest.mark.fiveg
-    def test_client_wpa_5g(self, get_lanforge_data, setup_profile_data):
-        profile_data = setup_profile_data["NAT"]["WPA"]["5G"]
-        lanforge_ip = get_lanforge_data["lanforge_ip"]
-        lanforge_port = int(get_lanforge_data["lanforge-port-number"])
-        ssid = profile_data["ssid_name"]
-        security_key = profile_data["security_key"]
-        security = "wpa"
-        upstream = get_lanforge_data["lanforge_bridge_port"]
-        radio = get_lanforge_data["lanforge_5g"]
-        # Write Your test case Here
-        PASS = False
-        WFC_Test = WiFiCapacityTest(lf_host=lanforge_ip,
-                                    lf_port=lanforge_port,
-                                    lf_user="lanforge",
-                                    lf_password="lanforge",
-                                    instance_name="wct_instance",
-                                    config_name="wifi_config",
-                                    upstream=f"1.1.{upstream}",
-                                    batch_size="1",
-                                    loop_iter="1",
-                                    protocol="UDP-IPv4",
-                                    duration="3000",
-                                    pull_report=True,
-                                    load_old_cfg=False,
-                                    upload_rate="10Mbps",
-                                    download_rate="1Gbps",
-                                    sort="interleave",
-                                    stations="1.1.sta0000,1.1.sta0001",
-                                    create_stations=True,
-                                    radio=radio,
-                                    security=security,
-                                    paswd=security_key,
-                                    ssid=ssid,
-                                    enables=[],
-                                    disables=[],
-                                    raw_lines=[],
-                                    raw_lines_file="",
-                                    sets=[],
-                                    )
-        WFC_Test.setup()
-        WFC_Test.run()
-        PASS = True
+    def test_client_wpa_5g(self, test_cases, lf_test):
+        print("**********  test_client_wpa_5g  ***********")
+        profile_data = setup_params_general["ssid_modes"]["wpa"][1]
+        print(profile_data)
+
+        create_station = CreateStation(_host=lf_test.lanforge_ip,
+                                       _port=lf_test.lanforge_port,
+                                       _ssid=profile_data["ssid_name"],
+                                       _password=profile_data["security_key"],
+                                       _security="wpa",
+                                       _sta_list=['1.1.sta0000', '1.1.sta0001'],
+                                       _radio=lf_test.fiveg_radios)
+        create_station.build()
+        time.sleep(20)
+        PASS = lf_test.wifi_capacity(self, ssid=profile_data["ssid_name"], passkey=profile_data["security_key"],
+                                     security="wpa", station_name=[], mode="NAT", band="fiveg",
+                                     instance_name="wct_instance", )
         assert PASS
+
 
     @pytest.mark.wpa2_personal
     @pytest.mark.twog
-    def test_client_wpa2_personal_2g(self, get_lanforge_data, setup_profile_data):
-        profile_data = setup_profile_data["NAT"]["WPA2_P"]["2G"]
-        lanforge_ip = get_lanforge_data["lanforge_ip"]
-        lanforge_port = int(get_lanforge_data["lanforge-port-number"])
-        ssid = profile_data["ssid_name"]
-        security_key = profile_data["security_key"]
-        security = "wpa2"
-        upstream = get_lanforge_data["lanforge_bridge_port"]
-        radio = get_lanforge_data["lanforge_2dot4g"]
-        # Write Your test case Here
-        PASS = False
-        WFC_Test = WiFiCapacityTest(lf_host=lanforge_ip,
-                                    lf_port=lanforge_port,
-                                    lf_user="lanforge",
-                                    lf_password="lanforge",
-                                    instance_name="wct_instance",
-                                    config_name="wifi_config",
-                                    upstream=f"1.1.{upstream}",
-                                    batch_size="1",
-                                    loop_iter="1",
-                                    protocol="UDP-IPv4",
-                                    duration="3000",
-                                    pull_report=True,
-                                    load_old_cfg=False,
-                                    upload_rate="10Mbps",
-                                    download_rate="1Gbps",
-                                    sort="interleave",
-                                    stations="1.1.sta0000,1.1.sta0001",
-                                    create_stations=True,
-                                    radio=radio,
-                                    security=security,
-                                    paswd=security_key,
-                                    ssid=ssid,
-                                    enables=[],
-                                    disables=[],
-                                    raw_lines=[],
-                                    raw_lines_file="",
-                                    sets=[],
-                                    )
-        WFC_Test.setup()
-        WFC_Test.run()
-        PASS = True
+    def test_client_wpa2_personal_2g(self, test_cases,lf_test):
+        print("**********  test_client_wpa2_personal_2g  ***********")
+        profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
+        print(profile_data)
+
+        create_station = CreateStation(_host = lf_test.lanforge_ip,
+                                       _port = lf_test.lanforge_port,
+                                       _ssid = profile_data["ssid_name"],
+                                       _password = profile_data["security_key"],
+                                       _security = "wpa2",
+                                       _sta_list = ['1.1.sta0000','1.1.sta0001'],
+                                       _radio = lf_test.twog_radios)
+        create_station.build()
+        time.sleep(20)
+        PASS = lf_test.wifi_capacity(self, ssid = profile_data["ssid_name"], passkey = profile_data["security_key"],
+                              security = "wpa2", station_name = [], mode = "NAT", band = "twog",
+                              instance_name = "wct_instance", )
         assert PASS
 
     @pytest.mark.wpa2_personal
     @pytest.mark.fiveg
-    def test_client_wpa2_personal_5g(self, get_lanforge_data, setup_profile_data):
-        profile_data = setup_profile_data["NAT"]["WPA2_P"]["5G"]
-        lanforge_ip = get_lanforge_data["lanforge_ip"]
-        lanforge_port = int(get_lanforge_data["lanforge-port-number"])
-        ssid = profile_data["ssid_name"]
-        security_key = profile_data["security_key"]
-        security = "wpa2"
-        upstream = get_lanforge_data["lanforge_bridge_port"]
-        radio = get_lanforge_data["lanforge_5g"]
+    def test_client_wpa2_personal_2g(self, test_cases, lf_test):
+        print("**********  test_client_wpa2_personal_5g  ***********")
+        profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][1]
+        print(profile_data)
 
-        # Write Your test case Here
-        PASS = False
-        WFC_Test = WiFiCapacityTest(lf_host=lanforge_ip,
-                                    lf_port=lanforge_port,
-                                    lf_user="lanforge",
-                                    lf_password="lanforge",
-                                    instance_name="wct_instance",
-                                    config_name="wifi_config",
-                                    upstream=f"1.1.{upstream}",
-                                    batch_size="1",
-                                    loop_iter="1",
-                                    protocol="UDP-IPv4",
-                                    duration="3000",
-                                    pull_report=True,
-                                    load_old_cfg=False,
-                                    upload_rate="10Mbps",
-                                    download_rate="1Gbps",
-                                    sort="interleave",
-                                    stations="1.1.sta0000,1.1.sta0001",
-                                    create_stations=True,
-                                    radio=radio,
-                                    security=security,
-                                    paswd=security_key,
-                                    ssid=ssid,
-                                    enables=[],
-                                    disables=[],
-                                    raw_lines=[],
-                                    raw_lines_file="",
-                                    sets=[],
-                                    )
-        WFC_Test.setup()
-        WFC_Test.run()
-        PASS = True
+        create_station = CreateStation(_host=lf_test.lanforge_ip,
+                                       _port=lf_test.lanforge_port,
+                                       _ssid=profile_data["ssid_name"],
+                                       _password=profile_data["security_key"],
+                                       _security="wpa2",
+                                       _sta_list=['1.1.sta0000', '1.1.sta0001'],
+                                       _radio=lf_test.fiveg_radios)
+        create_station.build()
+        time.sleep(20)
+        PASS = lf_test.wifi_capacity(self, ssid=profile_data["ssid_name"], passkey=profile_data["security_key"],
+                                     security="wpa2", station_name=[], mode="NAT", band="fiveg",
+                                     instance_name="wct_instance", )
         assert PASS
 
 
