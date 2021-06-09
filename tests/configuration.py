3895bc426d32e674c6b2a0af68cf829c85182c51
tushar.metkar <tushar.metkar@netexperience.com>
2021-06-09 01:52:50 -0400

    Profile download URLs for android and ios devices

--- tests/configuration.py
+++ tests/configuration.py
@@ -211,7 +211,8 @@ PASSPOINT_VENUE_INFO = {
 }
 
 PASSPOINT_PROFILE_INFO = {
-    "profile_download_url": "https://onboard.almondlabs.net/ios.html",
+    "profile_download_url_ios": "https://onboard.almondlabs.net/ios.html",
+    "profile_download_url_android": "https://onboard.almondlabs.net/android.html",
     "profile_name_on_device": "AmeriBand",
     "radius_configuration": {
         "user_defined_nas_id": "FB001AP001",
