f3251f7fb0ba8275f729818c2c73f24463187dc8
shivamcandela <shivam.thakur@candelatech.com>
2021-06-24 18:27:00 +0530

    perf test fix
    Signed-off-by: shivamcandela <shivam.thakur@candelatech.com>

--- libs/lanforge/lf_tests.py
+++ libs/lanforge/lf_tests.py
@@ -282,7 +282,7 @@ class RunTest:
                                            duration=duration,
                                            dut=dut_name,
                                            station="1.1." + station_name[0],
-                                           raw_lines=[['pkts: Custom;60;142;256;512;1024;MTU'],
+                                           raw_lines=[['pkts: MTU'],
                                                       ['directions: DUT Transmit;DUT Receive'],
                                                       ['traffic_types: UDP;TCP'], ["show_3s: 1"],
                                                       ["show_ll_graphs: 1"], ["show_log: 1"]],
