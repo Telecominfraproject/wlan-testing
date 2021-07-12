7006ff10c20e6992ab9818b6ec99e2a84054bc27
Nikita Yadav <nikita.yadav@candelatech.com>
2021-07-12 10:56:37 +0530

    vlan  mode test cases

--- tests/e2e/advanced/rate_vs_range/wpa2_personal/test_vlan_mode.py
+++ tests/e2e/advanced/rate_vs_range/wpa2_personal/test_vlan_mode.py
@@ -1,7 +1,7 @@
 """
 
-    Advanced  Test: Rate v/s Range test under various combinations: NAT Mode
-    pytest -m "throughput_benchmark_test and nat" -s -vvv --skip-testrail --testbed=basic-01
+    Advanced  Test: Rate v/s Range test under various combinations: VLAN Mode
+    pytest -m "throughput_benchmark_test and vlan" -s -vvv --skip-testrail --testbed=basic-01
 
 
 """
@@ -11,11 +11,11 @@ import time
 import pytest
 import allure
 
-pytestmark = [pytest.mark.performance, pytest.mark.ratevsrange, pytest.mark.vlan ,pytest.mark.usefixtures("setup_test_run")]
+pytestmark = [pytest.mark.performance_advanced, pytest.mark.ratevsrange, pytest.mark.vlan ,pytest.mark.usefixtures("setup_test_run")]
 
 
 setup_params_general = {
-    "mode": "NAT",
+    "mode": "VLAN",
     "ssid_modes": {
         "open": [{"ssid_name": "ssid_open_2g", "appliedRadios": ["is2dot4GHz"]},
                  {"ssid_name": "ssid_open_5g", "appliedRadios": ["is5GHzU", "is5GHz", "is5GHzL"]}],
@@ -53,7 +53,7 @@ class TestRatevsRangeVlan(object):
                                                  get_configuration, lf_tools):
         """
 
-        pytest -m "ratevsrange and nat and 11bclient" -s -vvv --skip-testrail --testbed=advanced-02
+        pytest -m "ratevsrange and vlan and 11bclient" -s -vvv --skip-testrail --testbed=advanced-02
         jira- wifi-2495
         """
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
@@ -72,12 +72,13 @@ class TestRatevsRangeVlan(object):
                                          station_name=station_names_twog, vlan_id=vlan)
         print("station", station)
 
-        val = [['modes: 802.11b'], ['pkts: MTU'], ['directions: DUT Transmit;DUT Receive'], ['traffic_types: TCP'],
-               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuations: 0..+50']]
+        val = [['modes: 802.11b'], ['pkts: MTU'], ['directions: DUT Transmit'], ['traffic_types:TCP'],
+               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuator2: 1.1.3059'],
+               ['attenuations: 0..+50..950'], ['attenuations2: 0..+50..950']]
         if station:
             time.sleep(3)
             rvr_o = lf_test.ratevsrange(station_name=station_names_twog, mode=mode,
-                                       instance_name="WPa2_2G_vlan_rvr_11b",
+                                       instance_name="VLAN_RVR_11B",
                                        vlan_id=vlan, dut_name=dut_name, raw_lines=val)
             report_name = rvr_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
             entries = os.listdir("../reports/" + report_name + '/')
@@ -90,7 +91,7 @@ class TestRatevsRangeVlan(object):
                                    name=get_configuration["access_point"][0]["model"] + "ratevsrange")
             print("Test Completed... Cleaning up Stations")
             lf_test.Client_disconnect(station_name=station_names_twog)
-            kpi_val = lf_tools.read_kpi_file(column_name='numeric-score', dir_name=report_name)
+            kpi_val = lf_tools.read_kpi_file(column_name=None, dir_name=report_name)
             print(str(kpi_val))
             allure.attach(name="Kpi Data", body=str(kpi_val))
             assert station
@@ -105,7 +106,7 @@ class TestRatevsRangeVlan(object):
                                          get_configuration, lf_tools):
         """
 
-        pytest -m "ratevsrange and nat and client11g" -s -vvv --skip-testrail --testbed=advanced-02
+        pytest -m "ratevsrange and vlan and client11g" -s -vvv --skip-testrail --testbed=advanced-02
         jira- wifi-2496
         """
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
@@ -124,13 +125,14 @@ class TestRatevsRangeVlan(object):
                                          station_name=station_names_twog, vlan_id=vlan)
         print("station", station)
 
-        val = [['modes: 802.11g'], ['pkts: MTU'], ['directions: DUT Transmit;DUT Receive'], ['traffic_types: TCP'],
-               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuations: 0..+50']]
+        val = [['modes: 802.11g'], ['pkts: MTU'], ['directions: DUT Transmit'], ['traffic_types:TCP'],
+               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuator2: 1.1.3059'],
+               ['attenuations: 0..+50..950'], ['attenuations2: 0..+50..950']]
 
         if station:
             time.sleep(3)
             rvr_o = lf_test.ratevsrange(station_name=station_names_twog, mode=mode,
-                                        instance_name="WPA2_2G_vlan_rvr_11g",
+                                        instance_name="VLAN_RVR_11G",
                                         vlan_id=vlan, dut_name=dut_name, raw_lines=val)
             report_name = rvr_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
             entries = os.listdir("../reports/" + report_name + '/')
@@ -143,7 +145,7 @@ class TestRatevsRangeVlan(object):
                                    name=get_configuration["access_point"][0]["model"] + "ratevsrange")
             print("Test Completed... Cleaning up Stations")
             lf_test.Client_disconnect(station_name=station_names_twog)
-            kpi_val = lf_tools.read_kpi_file(column_name='numeric-score', dir_name=report_name)
+            kpi_val = lf_tools.read_kpi_file(column_name=None, dir_name=report_name)
             print(str(kpi_val))
             allure.attach(name="Kpi Data", body=str(kpi_val))
             assert station
@@ -158,7 +160,7 @@ class TestRatevsRangeVlan(object):
                                          get_configuration, lf_tools):
         """
 
-        pytest -m "ratevsrange and nat  and client11a" -s -vvv --skip-testrail --testbed=advanced-02
+        pytest -m "ratevsrange and vlan  and client11a" -s -vvv --skip-testrail --testbed=advanced-02
         jira- wifi-2497
         """
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][1]
@@ -177,13 +179,13 @@ class TestRatevsRangeVlan(object):
                                          station_name=station_names_fiveg, vlan_id=vlan)
         print("station", station)
 
-        val = [['modes: 802.11a'], ['pkts: MTU'], ['directions: DUT Transmit;DUT Receive'], ['traffic_types: TCP'],
-               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuations: 0..+50']]
-
+        val = [['modes: 802.11a'], ['pkts: MTU'], ['directions: DUT Transmit'], ['traffic_types:TCP'],
+               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuator2: 1.1.3059'],
+               ['attenuations: 0..+50..950'], ['attenuations2: 0..+50..950']]
         if station:
             time.sleep(3)
             rvr_o = lf_test.ratevsrange(station_name=station_names_fiveg, mode=mode,
-                                        instance_name="WPA2_2G_vlan_rvr_11a",
+                                        instance_name="VLAN_RVR_11A",
                                         vlan_id=vlan, dut_name=dut_name, raw_lines=val)
             report_name = rvr_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
             entries = os.listdir("../reports/" + report_name + '/')
@@ -196,7 +198,7 @@ class TestRatevsRangeVlan(object):
                                    name=get_configuration["access_point"][0]["model"] + "ratevsrange")
             print("Test Completed... Cleaning up Stations")
             lf_test.Client_disconnect(station_name=station_names_fiveg)
-            kpi_val = lf_tools.read_kpi_file(column_name='numeric-score', dir_name=report_name)
+            kpi_val = lf_tools.read_kpi_file(column_name=None, dir_name=report_name)
             print(str(kpi_val))
             allure.attach(name="Kpi Data", body=str(kpi_val))
             assert station
@@ -211,7 +213,7 @@ class TestRatevsRangeVlan(object):
                                          get_configuration,lf_tools):
         """
 
-        pytest -m "ratevsrange and nat and client11an" -s -vvv --skip-testrail --testbed=advanced-02
+        pytest -m "ratevsrange and vlan and client11an" -s -vvv --skip-testrail --testbed=advanced-02
         jira- wifi-2498
         """
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][1]
@@ -230,13 +232,13 @@ class TestRatevsRangeVlan(object):
                                          station_name=station_names_fiveg, vlan_id=vlan)
         print("station", station)
 
-        val = [['modes: 802.11an'], ['pkts: MTU'], ['directions: DUT Transmit;DUT Receive'], ['traffic_types: TCP'],
-               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuations: 0..+50']]
-
+        val = [['modes: 802.11an'], ['pkts: MTU'], ['directions: DUT Transmit'], ['traffic_types:TCP'],
+               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuator2: 1.1.3059'],
+               ['attenuations: 0..+50..950'], ['attenuations2: 0..+50..950']]
         if station:
             time.sleep(3)
             rvr_o = lf_test.ratevsrange(station_name=station_names_fiveg, mode=mode,
-                                        instance_name="WPA2_2G_vlan_rvr_11an",
+                                        instance_name="VLAN_RVR_11AN",
                                         vlan_id=vlan, dut_name=dut_name, raw_lines=val)
             report_name = rvr_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
             entries = os.listdir("../reports/" + report_name + '/')
@@ -249,7 +251,7 @@ class TestRatevsRangeVlan(object):
                                    name=get_configuration["access_point"][0]["model"] + "ratevsrange")
             print("Test Completed... Cleaning up Stations")
             lf_test.Client_disconnect(station_name=station_names_fiveg)
-            kpi_val = lf_tools.read_kpi_file(column_name='numeric-score', dir_name=report_name)
+            kpi_val = lf_tools.read_kpi_file(column_name=None, dir_name=report_name)
             print(str(kpi_val))
             allure.attach(name="Kpi Data", body=str(kpi_val))
             assert station
@@ -264,7 +266,7 @@ class TestRatevsRangeVlan(object):
                                           get_configuration, lf_tools):
         """
 
-        pytest -m "ratevsrange and nat and client11ac" -s -vvv --skip-testrail --testbed=advanced-02
+        pytest -m "ratevsrange and vlan and client11ac" -s -vvv --skip-testrail --testbed=advanced-02
         jira- wifi-2499
         """
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][1]
@@ -273,7 +275,7 @@ class TestRatevsRangeVlan(object):
         security = "wpa2"
         mode = "VLAN"
         band = "fiveg"
-        vlan = 100
+        vlan = 1
         dut_name = create_lanforge_chamberview_dut
         if ssid_name not in get_vif_state:
             allure.attach(name="retest,vif state ssid not available:", body=str(get_vif_state))
@@ -283,13 +285,14 @@ class TestRatevsRangeVlan(object):
                                          station_name=station_names_fiveg, vlan_id=vlan)
         print("station", station)
 
-        val = [['modes: 802.11an-AC'],['pkts: MTU'], ['directions: DUT Transmit;DUT Receive'], ['traffic_types: TCP'],
-               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'] , ['attenuations: 0..+50']]
+        val = [['modes: 802.11an-AC'], ['pkts: MTU'], ['directions: DUT Transmit'], ['traffic_types:TCP'],
+               ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: 1.1.3034'], ['attenuator2: 1.1.3059'],
+               ['attenuations: 0..+50..950'], ['attenuations2: 0..+50..950']]
 
         if station:
             time.sleep(3)
             rvr_o = lf_test.ratevsrange(station_name=station_names_fiveg, mode=mode,
-                                        instance_name="WPA2_2G_vlan_rvr_11an_ac",
+                                        instance_name="VLAN_RVR_11AC",
                                         vlan_id=vlan, dut_name=dut_name, raw_lines=val)
             report_name = rvr_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
             entries = os.listdir("../reports/" + report_name + '/')
@@ -302,7 +305,7 @@ class TestRatevsRangeVlan(object):
                                    name=get_configuration["access_point"][0]["model"] + "ratevsrange")
             print("Test Completed... Cleaning up Stations")
             lf_test.Client_disconnect(station_name=station_names_fiveg)
-            kpi_val = lf_tools.read_kpi_file(column_name='numeric-score', dir_name=report_name)
+            kpi_val = lf_tools.read_kpi_file(column_name=None, dir_name=report_name)
             print(str(kpi_val))
             allure.attach(name="Kpi Data", body=str(kpi_val))
             assert station
