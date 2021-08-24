9ba843e5e03235bbf1ab2f7bf4370452698f68fc
Nikita Yadav <nikita.yadav@candelatech.com>
2021-08-24 23:33:09 +0530

    adding test automation for wifi-3492

--- tests/e2e/basic/validation_of_operating_modes/bridge_mode/multi_psk/test_multipsk_basic.py
+++ tests/e2e/basic/validation_of_operating_modes/bridge_mode/multi_psk/test_multipsk_basic.py
@@ -1,17 +1,34 @@
+"""
+
+    Performance Test: Multi-psk Test: Bridge Mode
+     pytest -m "multipsk and wpa2_personal and twog" -s -vvv --skip-testrail --testbed=basic-03 --alluredir=../allure_reports
+        wifi-3492
+"""
+import os
+import time
+
+import pytest
+import allure
+
+pytestmark = [pytest.mark.multipsk, pytest.mark.bridge]
+# pytest.mark.usefixtures("setup_test_run")]
+
+
 setup_params_general = {
     "mode": "BRIDGE",
     "ssid_modes": {
         "wpa2_personal": [
             {"ssid_name": "ssid_wpa2_2g_br",
              "appliedRadios": ["2G"],
+             "security": "psk2",
              "security_key": "something",
              "multi-psk": [
                  {
-                     "key": "aaaaaaaa",
+                     "key": "password@123",
                      "vlan-id": 100
                  },
                  {
-                     "key": "bbbbbbbb"
+                     "key": "lanforge@123"
                  }
              ],
              },
@@ -26,3 +43,88 @@ setup_params_general = {
     "rf": {},
     "radius": False
 }
+
+
+@allure.feature("BRIDGE MODE CLIENT CONNECTIVITY")
+@pytest.mark.parametrize(
+    'setup_profiles',
+    [setup_params_general],
+    indirect=True,
+    scope="class"
+)
+@pytest.mark.usefixtures("setup_profiles")
+class TestMultipskBridge(object):
+
+    @pytest.mark.multipsk
+    @pytest.mark.wpa
+    def test_client_wpa(self, lf_test, lf_tools):
+
+        profile_data = setup_params_general["ssid_modes"]["wpa"][0]
+        print(profile_data)
+        ssid_name = profile_data["ssid_name"]
+        print(ssid_name)
+        security_key = profile_data["security_key"]
+        key1 = profile_data["multi-psk"][0]["key"]
+        key2 = profile_data["multi-psk"][1]["key"]
+        vlan_id = profile_data["multi-psk"][0]['vlan-id']
+        security = "wpa"
+        mode = "BRIDGE"
+        band = "twog"
+        vlan = 1
+        # create vlan
+        lf_tools.add_vlan(vlan_ids=[int(vlan_id)])
+
+        # input data
+        input_data = [{
+            "password": key1,
+            "upstream": "eth2." + str(vlan_id),
+            "mac": "",
+            "num_station": 1,
+            "radio": "wiphy4"
+        },
+            {
+                "password": key2,
+                "upstream": "eth2",
+                "mac": "",
+                "num_station": 1,
+                "radio": "wiphy4"
+            },
+
+        ]
+        multipsk_obj = lf_test.multipsk(ssid=ssid_name, input_dataset=input_data, security="wpa", mode="BRIDGE", vlan_id=int(vlan_id))
+
+        if multipsk_obj == True:
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.multipsk
+    @pytest.mark.wpa2_personal
+    @pytest.mark.twog
+    def test_client_wpa2_2g(self, lf_test, lf_tools):
+
+        profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
+        print(profile_data)
+        ssid_name = profile_data["ssid_name"]
+        print(ssid_name)
+        security_key = profile_data["security_key"]
+        key1 = profile_data["multi-psk"][0]["key"]
+        key2 = profile_data["multi-psk"][1]["key"]
+        vlan_id = profile_data["multi-psk"][0]['vlan-id']
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "twog"
+        vlan = 1
+        # create vlan
+        lf_tools.add_vlan(vlan_ids=[int(vlan_id)])
+        time.sleep(10)
+        station_name = []
+        station_name.append("sta" + str(vlan_id))
+        station_name.append("sta00")
+        print(station_name)
+        multipsk_obj = lf_test.multipsk(ssid=ssid_name,  security="wpa2", mode="BRIDGE",
+                                        vlan_id=int(vlan_id), key1=key1, key2=key2, band=band, station_name=station_name)
+        if multipsk_obj == True:
+            assert True
+        else:
+            assert False
