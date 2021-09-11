7dbdf300f3c71c7f30b4d7f4236d6d293fb5d922
saurabh <saurabh.goyal@candelatech.com>
2021-09-11 14:59:33 +0530

    Remove duplicated directory

--- tests/configuration.py
+++ tests/configuration.py
@@ -202,10 +202,10 @@ CONFIGURATION = {
                 'mode': 'wifi6',
                 'serial': '0000c1018812',
                 'jumphost': True,
-                'ip': "10.28.3.103",
+                'ip': "localhost",
                 'username': "lanforge",
                 'password': "pumpkin77",
-                'port': 22,
+                'port': 8999,
                 'jumphost_tty': '/dev/ttyAP1',
                 'version': "https://tip.jfrog.io/artifactory/tip-wlan-ap-firmware/uCentral/cig_wf188/20210729-cig_wf188-v2.0.0-rc2-ec3662e-upgrade.bin"
             }
@@ -213,9 +213,9 @@ CONFIGURATION = {
         "traffic_generator": {
             "name": "lanforge",
             "details": {
-                "ip": "10.28.3.28",
-                "port": 8080,
-                "ssh_port": 22,
+                "ip": "localhost",
+                "port": 8998,
+                "ssh_port": 8990,
                 "2.4G-Radio": ["1.1.wiphy0", "1.1.wiphy2"],
                 "5G-Radio": ["1.1.wiphy1", "1.1.wiphy3"],
                 "AX-Radio": ["1.1.wiphy4", "1.1.wiphy5", "1.1.wiphy6", "1.1.wiphy7"],
