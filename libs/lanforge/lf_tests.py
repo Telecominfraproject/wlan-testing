3e50bb23f5732c507a18211086aa3aee14ade6d5
KVSaiMahesh <mahesh.karnati@candelatech.com>
2021-09-22 14:21:40 +0530

    added Multi_Stat_fun

--- libs/lanforge/lf_tests.py
+++ libs/lanforge/lf_tests.py
@@ -639,6 +639,71 @@ class RunTest:
         else:
             return False
 
+    def Multi_Sta_Thpt(self, ssid_5G="[BLANK]", ssid_2G="[BLANK]", mode="BRIDGE", vlan_id=100, dut_name="TIP",
+                       raw_line=[], instance_name="test_demo", dut_5g="", dut_2g=""):
+
+        inst_name = instance_name.split('_')[0]
+        instance_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=S))
+
+        if mode == "BRIDGE":
+            self.upstream_port = self.upstream_port
+        elif mode == "NAT":
+            self.upstream_port = self.upstream_port
+        else:
+            self.upstream_port = self.upstream_port + "." + str(vlan_id)
+
+        sets = [['Basic Client Connectivity', '0'], ['Multi Band Performance', '0'],
+                ['Throughput vs Pkt Size', '0'], ['Capacity', '0'],
+                ['Stability', '0'],
+                ['Band-Steering', '0'], ['Multi-Station Throughput vs Pkt Size', '1'],
+                ['Long-Term', '0']]
+
+        if len(self.twog_radios) == 1:
+            twog_radios = [[self.twog_radios[0]]]
+
+        elif len(self.twog_radios) > 1:
+            twog_radio = []
+            for i in range(0, len(self.twog_radios)):
+                twog_radio.append([self.twog_radios[i]])
+            twog_radios = twog_radio
+
+        if len(self.fiveg_radios) == 1:
+            fiveg_radios = [[self.fiveg_radios[0]]]
+
+        elif len(self.fiveg_radios) > 1:
+            fiveg_radio = []
+            for i in range(0, len(self.fiveg_radios)):
+                fiveg_radio.append([self.fiveg_radios[i]])
+            fiveg_radios = fiveg_radio
+
+        self.msthpt_obj = ApAutoTest(lf_host=self.lanforge_ip,
+                                     lf_port=self.lanforge_port,
+                                     ssh_port=self.lf_ssh_port,
+                                     lf_user="lanforge",
+                                     lf_password="lanforge",
+                                     instance_name=instance_name,
+                                     config_name="dbp_config",
+                                     upstream="1.1." + self.upstream_port,
+                                     pull_report=True,
+                                     dut5_0=dut_5g,
+                                     dut2_0=dut_2g,
+                                     load_old_cfg=False,
+                                     local_lf_report_dir=self.local_report_path,
+                                     radio2=twog_radios,
+                                     radio5=fiveg_radios,
+                                     sets=sets,
+                                     raw_lines=raw_line
+                                     )
+        self.msthpt_obj.setup()
+        self.msthpt_obj.run()
+        report_name = self.msthpt_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+        # influx = CSVtoInflux(influxdb=self.influxdb,
+        #                      _influx_tag=self.influx_params["influx_tag"],
+        #                      target_csv=self.local_report_path + report_name + "/kpi.csv")
+        # influx.post_to_influx()
+        return self.msthpt_obj
+
+
 if __name__ == '__main__':
     lanforge_data = {
         "ip": "localhost",
