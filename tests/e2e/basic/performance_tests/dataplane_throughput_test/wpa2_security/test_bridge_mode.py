48cd9101d3d065f735013d1a6ac3af5db4404d43
Nikita Yadav <nikita.yadav@candelatech.com>
2021-10-04 12:36:08 +0530

    added two more test case on ax radio

--- tests/e2e/basic/performance_tests/dataplane_throughput_test/wpa2_security/test_bridge_mode.py
+++ tests/e2e/basic/performance_tests/dataplane_throughput_test/wpa2_security/test_bridge_mode.py
@@ -54,9 +54,7 @@ class TestDataplaneThroughputBRIDGE(object):
         band = "twog"
         vlan = 1
         dut_name = create_lanforge_chamberview_dut
-        if ssid_name not in get_vif_state:
-            allure.attach(name="retest,vif state ssid not available:", body=str(get_vif_state))
-            pytest.xfail("SSID NOT AVAILABLE IN VIF STATE")
+
         station = lf_test.Client_Connect(ssid=ssid_name, security=security,
                                          passkey=security_key, mode=mode, band=band,
                                          station_name=station_names_twog, vlan_id=vlan)
@@ -89,9 +87,7 @@ class TestDataplaneThroughputBRIDGE(object):
         band = "fiveg"
         vlan = 1
         dut_name = create_lanforge_chamberview_dut
-        if ssid_name not in get_vif_state:
-            allure.attach(name="retest,vif state ssid not available:", body=str(get_vif_state))
-            pytest.xfail("SSID NOT AVAILABLE IN VIF STATE")
+
         station = lf_test.Client_Connect(ssid=ssid_name, security=security,
                                          passkey=security_key, mode=mode, band=band,
                                          station_name=station_names_fiveg, vlan_id=vlan)
@@ -107,3 +103,73 @@ class TestDataplaneThroughputBRIDGE(object):
             assert station
         else:
             assert False
+
+
+    @pytest.mark.wpa2_personal
+    @pytest.mark.fiveg
+    def test_tcp_upd_wpa2_personal_bridge_5g_band_ax(self, get_vif_state, lf_tools,
+                                                  lf_test, station_names_ax, create_lanforge_chamberview_dut,
+                                                  get_configuration):
+        """Dataplane THroughput BRIDGE Mode
+           pytest -m "dataplane_throughput_test and BRIDGE and wpa2_personal and fiveg"
+        """
+        profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][1]
+        ssid_name = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "ax"
+        vlan = 1
+        dut_name = create_lanforge_chamberview_dut
+
+        station = lf_test.Client_Connect(ssid=ssid_name, security=security,
+                                         passkey=security_key, mode=mode, band=band,
+                                         station_name=station_names_ax, vlan_id=vlan)
+
+        if station:
+            dp_obj = lf_test.dataplane(station_name=station_names_ax, mode=mode,
+                                       instance_name="TIP_DPT_DPT_WPA2_5G_BRIDGE_AX",
+                                       vlan_id=vlan, dut_name=dut_name)
+            report_name = dp_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+            lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Dataplane Throughput Test - TCP-UDP 5G")
+            print("Test Completed... Cleaning up Stations")
+            lf_test.Client_disconnect(station_name=station_names_ax)
+            assert station
+        else:
+            assert False
+
+
+    @pytest.mark.wpa2_personal
+    @pytest.mark.twog
+    def test_tcp_upd_wpa2_personal_bridge_2g_band_ax(self, get_vif_state, lf_tools,
+                                                  lf_test, station_names_ax, create_lanforge_chamberview_dut,
+                                                  get_configuration):
+        """Dataplane THroughput BRIDGE Mode
+           pytest -m "dataplane_throughput_test and BRIDGE and wpa2_personal and twog"
+        """
+        profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
+        ssid_name = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "ax"
+        vlan = 1
+        dut_name = create_lanforge_chamberview_dut
+
+        station = lf_test.Client_Connect(ssid=ssid_name, security=security,
+                                         passkey=security_key, mode=mode, band=band,
+                                         station_name=station_names_ax, vlan_id=vlan)
+
+        if station:
+            dp_obj = lf_test.dataplane(station_name=station_names_ax, mode=mode,
+                                       instance_name="TIP_DPT_DPT_WPA2_2G_BRIDGE_AX",
+                                       vlan_id=vlan, dut_name=dut_name)
+            report_name = dp_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+            lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Dataplane Throughput Test - TCP-UDP 2.4G")
+            lf_test.Client_disconnect(station_name=station_names_ax)
+            assert station
+        else:
+            assert False
+
+
+
