b72f97871d063acef55a5e2fffd5cf8ad01a4109
haricharan-jaka <haricharan.jaka@candelatech.com>
2021-10-05 23:17:19 +0530

    Added Enterprise marker

--- tests/e2e/interOp/validation_of_operating_modes/bridge_mode/client_connectivity_test/android/test_enterprise_ttls.py
+++ tests/e2e/interOp/validation_of_operating_modes/bridge_mode/client_connectivity_test/android/test_enterprise_ttls.py
@@ -17,7 +17,7 @@ if 'perfecto_libs' not in sys.path:
     sys.path.append(f'../libs/perfecto_libs')
 
 pytestmark = [pytest.mark.sanity, pytest.mark.interop, pytest.mark.android, pytest.mark.interop_and, pytest.mark.client_connect
-              ,pytest.mark.interop_uc_sanity, pytest.mark.bridge]
+              ,pytest.mark.interop_uc_sanity, pytest.mark.bridge, pytest.mark.enterprise]
 
 from android_lib import closeApp, set_APconnMobileDevice_android, Toggle_AirplaneMode_android, ForgetWifiConnection, openApp, get_ip_address_eap_and
 
