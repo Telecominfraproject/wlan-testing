555e7bf6287fa1f3fdf5a031f680a3e1c5b8362d
saurabh <saurabh.goyal@candelatech.com>
2021-10-06 17:49:58 +0530

    Marker changes
    Signed-off-by: saurabh <saurabh.goyal@candelatech.com>

--- tests/e2e/interOp/validation_of_operating_modes/bridge_mode/client_connect_test/iOS/test_enterprise_ttls.py
+++ tests/e2e/interOp/validation_of_operating_modes/bridge_mode/client_connect_test/iOS/test_enterprise_ttls.py
@@ -21,9 +21,9 @@ from iOS_lib import closeApp, openApp, get_WifiIPAddress_iOS, ForgetWifiConnecti
     Toggle_AirplaneMode_iOS, set_APconnMobileDevice_iOS, verify_APconnMobileDevice_iOS, Toggle_WifiMode_iOS, tearDown,\
     verifyUploadDownloadSpeediOS, get_ip_address_eap_ios
 
-pytestmark = [pytest.mark.client_connectivity, pytest.mark.bridge, pytest.mark.enterprise, pytest.mark.ttls,
+pytestmark = [pytest.mark.client_connect, pytest.mark.bridge, pytest.mark.enterprise, pytest.mark.ttls,
               pytest.mark.ucentral, pytest.mark.interop, pytest.mark.ios]
-
+    #ios and ucentral and interop and ttls and enterprise and bridge and client_connect
 setup_params_enterprise = {
     "mode": "BRIDGE",
     "ssid_modes": {
