ca2559fd7e197eb654b3cdc1f9fc3ff4bc05da36
saurabh <saurabh.goyal@candelatech.com>
2021-10-12 16:39:40 +0530

    Added Wpa Enterprise ClientConnect mode in Bridge ios
    Signed-off-by: saurabh <saurabh.goyal@candelatech.com>

--- tests/e2e/interOp/validation_of_operating_modes/bridge_mode/client_connect_test/iOS/test_enterprise_ttls.py
+++ tests/e2e/interOp/validation_of_operating_modes/bridge_mode/client_connect_test/iOS/test_enterprise_ttls.py
@@ -32,6 +32,9 @@ setup_params_enterprise = {
             {"ssid_name": "ssid_wpa2_eap_5g", "appliedRadios": ["5G"]}],
         "wpa3_enterprise": [
             {"ssid_name": "ssid_wpa3_eap_2g", "appliedRadios": ["2G"]},
+            {"ssid_name": "ssid_wpa3_eap_5g", "appliedRadios": ["5G"]}],
+        "wpa_enterprise": [
+            {"ssid_name": "ssid_wpa3_eap_2g", "appliedRadios": ["2G"]},
             {"ssid_name": "ssid_wpa3_eap_5g", "appliedRadios": ["5G"]}]},
 
     "rf": {},
@@ -178,6 +181,83 @@ class TestBridgeModeEnterpriseTTLSSuiteA(object):
                                        , setup_perfectoMobile_iOS, radius_info, get_ap_logs):
 
         profile_data = setup_params_enterprise["ssid_modes"]["wpa3_enterprise"][0]
+        ssidName = profile_data["ssid_name"]
+        # ssidPassword = profile_data["security_key"]
+        print("SSID_NAME: " + ssidName)
+        # print ("SSID_PASS: " + ssidPassword)
+        ttls_passwd = radius_info["password"]
+        identity = radius_info['user']
+        get_vif_state.append(ssidName)
+        if ssidName not in get_vif_state:
+            allure.attach(name="retest,vif state ssid not available:", body=str(get_vif_state))
+            pytest.xfail("SSID NOT AVAILABLE IN VIF STATE")
+
+        report = setup_perfectoMobile_iOS[1]
+        driver = setup_perfectoMobile_iOS[0]
+        connData = get_ToggleAirplaneMode_data
+
+        # Set Wifi/AP Mode
+        ip, is_internet = get_ip_address_eap_ios(request, ssidName, identity, ttls_passwd, setup_perfectoMobile_iOS, connData)
+
+        if ip:
+            if is_internet:
+                text_body = ("connected to " + ssidName + " (" + ip + ") " + "with internet")
+            else:
+                text_body = ("connected to " + ssidName + " (" + ip + ") " + "without internet")
+            print(text_body)
+            allure.attach(name="Connection Status: ", body=str(text_body))
+            assert True
+        else:
+            allure.attach(name="Connection Status: ", body=str("Device is Unable to connect"))
+            assert False
+
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-4693", name="WIFI-4693")
+    @pytest.mark.wpa_enterprise
+    @pytest.mark.fiveg
+    def test_ClientConnect_5g_WPA_Eap_Bridge(self, request, get_vif_state, get_ToggleAirplaneMode_data
+                                       , setup_perfectoMobile_iOS, radius_info, get_ap_logs):
+        """ wpa enterprise 5g
+            pytest -m "client_connect and bridge and enterprise and ttls and wpa_enterprise and fiveg"
+        """
+        profile_data = setup_params_enterprise["ssid_modes"]["wpa_enterprise"][1]
+        ssidName = profile_data["ssid_name"]
+        # ssidPassword = profile_data["security_key"]
+        print("SSID_NAME: " + ssidName)
+        # print ("SSID_PASS: " + ssidPassword)
+        ttls_passwd = radius_info["password"]
+        identity = radius_info['user']
+        get_vif_state.append(ssidName)
+        if ssidName not in get_vif_state:
+            allure.attach(name="retest,vif state ssid not available:", body=str(get_vif_state))
+            pytest.xfail("SSID NOT AVAILABLE IN VIF STATE")
+
+        report = setup_perfectoMobile_iOS[1]
+        driver = setup_perfectoMobile_iOS[0]
+        connData = get_ToggleAirplaneMode_data
+
+        # Set Wifi/AP Mode
+        ip, is_internet = get_ip_address_eap_ios(request, ssidName, identity, ttls_passwd, setup_perfectoMobile_iOS,
+                                                 connData)
+
+        if ip:
+            if is_internet:
+                text_body = ("connected to " + ssidName + " (" + ip + ") " + "with internet")
+            else:
+                text_body = ("connected to " + ssidName + " (" + ip + ") " + "without internet")
+            print(text_body)
+            allure.attach(name="Connection Status: ", body=str(text_body))
+            assert True
+        else:
+            allure.attach(name="Connection Status: ", body=str("Device is Unable to connect"))
+            assert False
+
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-4692", name="WIFI-4692")
+    @pytest.mark.twog
+    @pytest.mark.wpa_enterprise
+    def test_ClientConnect_2g_WPA_Eap_Bridge(self, request, get_vif_state, get_ToggleAirplaneMode_data
+                                       , setup_perfectoMobile_iOS, radius_info, get_ap_logs):
+
+        profile_data = setup_params_enterprise["ssid_modes"]["wpa_enterprise"][0]
         ssidName = profile_data["ssid_name"]
         # ssidPassword = profile_data["security_key"]
         print("SSID_NAME: " + ssidName)
