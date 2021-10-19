d1a0c8ec9429b82aee8cd8780ead9befd140fe0c
sushant <sushant.bawiskar@candelatech.com>
2021-10-19 23:22:11 +0530

    - e2e for expresswifi IOS
    Signed-off-by: Sushant Bawiskar <sushant.bawiskar@candelatech.com>

--- tests/e2e/interOp/Express_WiFi/iOS/test_ExpressWifi_ios.py
+++ tests/e2e/interOp/Express_WiFi/iOS/test_ExpressWifi_ios.py
@@ -16,32 +16,50 @@ import allure
 if 'perfecto_libs' not in sys.path:
     sys.path.append(f'../libs/perfecto_libs')
 
-pytestmark = [pytest.mark.sanity, pytest.mark.interop, pytest.mark.ios, pytest.mark.interop_ios, pytest.mark.expressWifiConnection]
+pytestmark = [pytest.mark.sanity, pytest.mark.interop, pytest.mark.ios, pytest.mark.interop_ios,
+              pytest.mark.expressWifiConnection]
 
-from iOS_lib import closeApp, ForgetWifiConnection, set_APconnMobileDevice_iOS, Toggle_WifiMode_iOS, verifyUploadDownloadSpeediOS, expressWifi
+from iOS_lib import closeApp, ForgetWifiConnection, set_APconnMobileDevice_iOS, Toggle_WifiMode_iOS, \
+    verifyUploadDownloadSpeediOS, expressWifi
+
+setup_params_general = {
+    "mode": "NAT",
+    "ssid_modes": {
+        "open": [{"ssid_name": "XWF-OWF_DSx", "appliedRadios": ["2G"]}]
+    },
+    "rf": {},
+    "radius": False,
+    "express-wifi": True
+}
 
 
+@pytest.mark.parametrize(
+    'setup_profiles',
+    [setup_params_general],
+    indirect=True,
+    scope="class"
+)
+@pytest.mark.usefixtures("setup_profiles")
 class TestExpressWifi(object):
 
+    @pytest.mark.twog
+    @pytest.mark.open
     def test_ExpressWifi(self, request, get_ToggleAirplaneMode_data, setup_perfectoMobile_iOS):
-
-        ssidName = "XWF-OWF_DSx"
+        profile_data = setup_params_general["ssid_modes"]["open"][0]
+        ssidName = profile_data["ssid_name"]
         ssidPassword = ""
-        print ("SSID_NAME: " + ssidName)
-        print ("SSID_PASS: " + ssidPassword)
+        print("SSID_NAME: " + ssidName)
+        print("SSID_PASS: " + ssidPassword)
 
         report = setup_perfectoMobile_iOS[1]
         driver = setup_perfectoMobile_iOS[0]
         connData = get_ToggleAirplaneMode_data
 
-        #Set Wifi/AP Mode
+        # Set Wifi/AP Mode
         set_APconnMobileDevice_iOS(request, ssidName, ssidPassword, setup_perfectoMobile_iOS, connData)
 
-        #Express Wifi
+        # Express Wifi
         expressWifi(request, setup_perfectoMobile_iOS, connData)
 
-        #ForgetWifi
+        # ForgetWifi
         ForgetWifiConnection(request, setup_perfectoMobile_iOS, ssidName, connData)
-
-  
-    
--- tests/fixtures_2x.py
+++ tests/fixtures_2x.py
@@ -508,6 +508,12 @@ class Fixtures_2x:
                             print(e)
                             test_cases["wpa_eap"] = False
 
+        try:
+            if parameter['express-wifi']:
+                instantiate_profile_obj.set_express_wifi()
+        except:
+            pass
+
         ap_ssh = get_apnos(get_configuration['access_point'][0], pwd="../libs/apnos/", sdk="2.x")
 
         # Get ucentral status
