3e6a71f7d3b7f5df5464d069abdef8fb49f30e17
DYeag <37943859+DYeag@users.noreply.github.com>
2021-11-22 08:00:03 -0800

    Update start_reservation.py
    update start reservation with sandbox api

--- .quali/start_reservation.py
+++ .quali/start_reservation.py
@@ -1,31 +1,100 @@
+import requests
+import json
 import os
 
 from cloudshell.api.cloudshell_api import UpdateTopologyGlobalInputsRequest, UpdateTopologyRequirementsInputsRequest
+
+import argparse
 
 from common import wait_for_provisioning_status, get_session
 
 run_id = os.environ.get('GITHUB_RUN_NUMBER', 1)
 
 def main():
-    session = get_session()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--openwifi-version', default='main')
+    parser.add_argument('--openwifi-gw-version', default='master')
+    parser.add_argument('--openwifi-sec-version', default='main')
+    parser.add_argument('--openwifi-fms-version', default='main')
+    parser.add_argument('--openwifi-ui-version', default='main')
+    parser.add_argument('--ap-model', default='EC420')
+    parser.add_argument('--wifi-type', default='Wifi5')
+    args = parser.parse_args()
 
-    reservation = session.CreateImmediateTopologyReservation(
-        reservationName=f'sanity-{run_id}',
-        owner=session.username,
-        durationInMinutes=60,
-        topologyFullPath='Basic Lab',
-        globalInputs=[
-            UpdateTopologyGlobalInputsRequest('Cloud Controller Version', '1.1.0-RC1'),
+
+    # Login to Sandbox API and get back Authorization token to use for later calls, auth token should timeout within 10 minutes by default
+    #TODO handle Quali Server host + gihtub user credentials
+    url = 'http://localhost:82/api/login'
+    headers = {'content-type': 'application/json'}
+    data = {
+        "username": "admin",
+        "password": "admin",
+        "domain": "Global"
+    }
+    response = requests.put(url, data=json.dumps(data), headers=headers)
+    auth_token = response.text.replace('"', '')
+    print('Basic {}'.format(auth_token))
+
+    #TODO Handle basic lab blueprint id + quali server host + arguments for sandbox
+    # Start Sandbox from Blueprint Basic Lab
+    basic_lab_id = '12345678'
+    url = 'http://localhost:82/api/v2/blueprints/{}/start'.format(basic_lab_id)
+    headers = {'content-type': 'application/json', 'Authorization': 'Basic {}'.format(auth_token)}
+    data = {
+        "name": f'sanity-{run_id}',
+        "duration": "PT6H5M",
+        "params": [
+            {
+                "name": "Chart Version",
+                "value": args.openwifi_version
+            },
+            {
+                "name": "owgw Version",
+                "value": args.openwifi_gw_version
+            },
+            {
+                "name": "owsec Version",
+                "value": args.openwifi_sec_version
+            },
+            {
+                "name": "owfms Version",
+                "value": args.openwifi_fms_version
+            },
+            {
+                "name": "owgwui Version",
+                "value": args.openwifi_gwui_version
+            },
+            {
+                "name": "owprov Version",
+                "value": "main"
+            },
+            {
+                "name": "owprovui Version",
+                "value": "main"
+            },
+            {
+                "name": "Wifi type",
+                "value": "[Any]"
+            },
+            {
+                "name": "AP Model",
+                "value": args.ap_model
+            }
         ],
-        requirementsInputs=[
-            UpdateTopologyRequirementsInputsRequest('Access Point', 'Ap.Wifi type', 'Wifi5', 'Attributes'),
-            UpdateTopologyRequirementsInputsRequest('Access Point', 'Ap.AP Model', 'ECW5410', 'Attributes')
+        "permitted_users": [
+            "owfgithub",
+            "admin"
         ]
-    ).Reservation
+    }
 
-    print(reservation.Id)
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    response_dict = json.loads(response.text)
+    reservation_id = response_dict['id']
 
-    wait_for_provisioning_status(session, reservation.Id, 'Ready')
+    print(reservation_id)
+
+    session = get_session()
+    wait_for_provisioning_status(session, reservation_id, 'Ready')
 
 if __name__ == '__main__':
     main()
