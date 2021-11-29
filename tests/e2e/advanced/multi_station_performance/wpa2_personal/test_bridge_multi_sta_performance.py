56c63b5da60774d3336a2da2945b0a690944bd47
jitendracandela <jitendra.kushavah@candelatech.com>
2021-11-29 14:33:52 +0530

    Added proper shelf and resource
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/e2e/advanced/multi_station_performance/wpa2_personal/test_bridge_multi_sta_performance.py
+++ tests/e2e/advanced/multi_station_performance/wpa2_personal/test_bridge_multi_sta_performance.py
@@ -29,17 +29,29 @@ class TestMultiStaPerfBridge(object):
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-5733", name="WIFI-5733")
     @pytest.mark.wpa2_personal
     @pytest.mark.twog
-    def test_multi_station_tcp_upload_short_dis_nss1_2g(self, lf_test, lf_tools):
+    def test_multi_station_tcp_upload_short_dis_nss1_2g(self, lf_test, lf_tools, station_names_twog):
         # run wifi capacity test here
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
         ssid_name = profile_data["ssid_name"]
         mode = "BRIDGE"
         vlan = 1
-        sta = ["1.1.sta0000", "1.1.sta0001", "1.1.sta0003"]
+        station_name = station_names_twog
+        print(station_name)
+        values = station_name.split()
+        shelf = int(values[0])
+        resource = int(values[1])
+        print(shelf, resource)
+        sta = []
+        for i in range(3):
+            sta.append(station_name + str(i))
+        print(sta)
+        #sta = ["1.1.sta0000", "1.1.sta0001", "1.1.sta0003"]
         atten_value = 100
-        lf_tools.set_radio_antenna("cli-json/set_wifi_radio", 1, 1, lf_tools.two_radios[0], 1)
+        lf_tools.set_radio_antenna("cli-json/set_wifi_radio", shelf, resource, lf_tools.two_radios[0], 1)
         lf_test.Client_Connect_Using_Radio(ssid=ssid_name, passkey=profile_data["security_key"], radio=lf_tools.two_radios[0], station_name=sta)
-        lf_tests.attenuator_modify("all", "all", atten_value)
+        atten_sr = lf_test.attenuator_serial()
+        print(atten_sr)
+        lf_test.attenuator_modify("all", "all", atten_value)
         #lf_tools.Chamber_View()
         wct_obj = lf_test.wifi_capacity(instance_name="tcp_upload_short_dis_nss1_2g", mode=mode, vlan_id=vlan,
                                         download_rate="0Gbps", batch_size="3",
