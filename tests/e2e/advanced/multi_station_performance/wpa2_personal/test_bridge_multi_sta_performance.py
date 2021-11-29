a62d9b083b3ff2f81f6b3259e4e5f6313167cab9
jitendracandela <jitendra.kushavah@candelatech.com>
2021-11-29 23:08:14 +0530

    testing
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/e2e/advanced/multi_station_performance/wpa2_personal/test_bridge_multi_sta_performance.py
+++ tests/e2e/advanced/multi_station_performance/wpa2_personal/test_bridge_multi_sta_performance.py
@@ -30,14 +30,16 @@ class TestMultiStaPerfBridge(object):
     @pytest.mark.wpa2_personal
     @pytest.mark.twog
     def test_multi_station_tcp_upload_short_dis_nss1_2g(self, lf_test, lf_tools, station_names_twog):
-        # run wifi capacity test here
+        lf_tools.reset_scenario()
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
         ssid_name = profile_data["ssid_name"]
         mode = "BRIDGE"
         vlan = 1
-        station_name = station_names_twog
+        station_name = station_names_twog[0]
+        radio_name = lf_tools.twog_radios[0]
+        print(radio_name)
         print(station_name)
-        values = station_name.split()
+        values = radio_name.split(".")
         shelf = int(values[0])
         resource = int(values[1])
         print(shelf, resource)
@@ -49,8 +51,8 @@ class TestMultiStaPerfBridge(object):
         print(sta)
         #sta = ["1.1.sta0000", "1.1.sta0001", "1.1.sta0003"]
         atten_value = 100
-        lf_tools.set_radio_antenna("cli-json/set_wifi_radio", shelf, resource, lf_tools.two_radios[0], 1)
-        sta_ip = lf_test.Client_Connect_Using_Radio(ssid=ssid_name, passkey=profile_data["security_key"], radio=lf_tools.two_radios[0], station_name=sta)
+        lf_tools.set_radio_antenna("cli-json/set_wifi_radio", shelf, resource, values[2], 1)
+        sta_ip = lf_test.Client_Connect_Using_Radio(ssid=ssid_name, passkey=profile_data["security_key"], radio=radio_name, station_name=sta)
         if not sta_ip:
             print("test failed due to no station ip")
             assert False
@@ -68,52 +70,110 @@ class TestMultiStaPerfBridge(object):
         if sta_ip:
             assert True
 
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-5733", name="WIFI-5733")
-    @pytest.mark.wpa2_personal
-    @pytest.mark.twog
-    def test_multi_station_tcp_upload_short_med_dis_nss1_2g(self, lf_test, lf_tools, station_names_twog):
-        # run wifi capacity test here
-        profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
-        ssid_name = profile_data["ssid_name"]
-        mode = "BRIDGE"
-        vlan = 1
-        station_name = station_names_twog
-        print(station_name)
-        values = station_name.split()
-        shelf = int(values[0])
-        resource = int(values[1])
-        print(shelf, resource)
-        atten_sr = lf_test.attenuator_serial()
-        print(atten_sr)
-        sta = []
-        for i in range(6):
-            sta.append(station_name + str(i))
-        print(sta)
-        #sta = [["1.1.sta0000", "1.1.sta0001", "1.1.sta0003"], ["1.1.sta0004", "1.1.sta0005", "1.1.sta0006"]]
+    # @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-5844", name="WIFI-5844")
+    # @pytest.mark.wpa2_personal
+    # @pytest.mark.twog
+    # def test_multi_station_tcp_upload_short_med_dis_nss1_2g(self, lf_test, lf_tools, station_names_twog):
+    #     lf_tools.reset_scenario()
+    #     profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
+    #     ssid_name = profile_data["ssid_name"]
+    #     mode = "BRIDGE"
+    #     vlan = 1
+    #     station_name = station_names_twog[0]
+    #     print(station_name)
+    #     values = station_name.split()
+    #     shelf = int(values[0])
+    #     resource = int(values[1])
+    #     print(shelf, resource)
+    #     atten_sr = lf_test.attenuator_serial()
+    #     print(atten_sr)
+    #     sta = []
+    #     for i in range(6):
+    #         sta.append(station_name + str(i))
+    #     print(sta)
+    #     #sta = [["1.1.sta0000", "1.1.sta0001", "1.1.sta0003"], ["1.1.sta0004", "1.1.sta0005", "1.1.sta0006"]]
+    #
+    #     for i in range(2):
+    #         lf_tools.set_radio_antenna("cli-json/set_wifi_radio", shelf, resource, lf_tools.twog_radios[i], 1)
+    #         time.sleep(0.5)
+    #         sta_ip = lf_test.Client_Connect_Using_Radio(ssid=ssid_name, passkey=profile_data["security_key"], radio=lf_tools.twog_radios[i], station_name=sta[i])
+    #         if not sta_ip:
+    #             print("test failed due to no station ip")
+    #             assert False
+    #         time.sleep(0.5)
+    #     for i in range(4):
+    #         lf_test.attenuator_modify(atten_sr[0], i, 100)
+    #         time.sleep(0.5)
+    #     for i in range(2):
+    #         lf_test.attenuator_modify(atten_sr[1], i, 400)
+    #         time.sleep(0.5)
+    #
+    #     #lf_tools.Chamber_View()
+    #     wct_obj = lf_test.wifi_capacity(instance_name="tcp_upload_short_med_dis_nss1_2g", mode=mode, vlan_id=vlan,
+    #                                     download_rate="0Gbps", batch_size="3,6",
+    #                                     upload_rate="4Mbps", protocol="TCP-IPv4", duration="120000", create_stations=False)
+    #
+    #
+    #     report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+    #
+    #     lf_tools.attach_report_graphs(report_name=report_name)
+    #     print("Test Completed... Cleaning up Stations")
+    #     if sta_ip:
+    #         assert True
+    #
+    # @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-5877", name="WIFI-5877")
+    # @pytest.mark.wpa2_personal
+    # @pytest.mark.twog
+    # def test_multi_station_tcp_upload_short_med_far_dis_nss1_2g(self, lf_test, lf_tools, station_names_twog):
+    #     lf_tools.reset_scenario()
+    #     profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
+    #     ssid_name = profile_data["ssid_name"]
+    #     mode = "BRIDGE"
+    #     vlan = 1
+    #     station_name = station_names_twog[0]
+    #     print(station_name)
+    #     values = station_name.split()
+    #     shelf = int(values[0])
+    #     resource = int(values[1])
+    #     print(shelf, resource)
+    #     atten_sr = lf_test.attenuator_serial()
+    #     print(atten_sr)
+    #     sta = []
+    #     for i in range(6):
+    #         sta.append(station_name + str(i))
+    #     print(sta)
+    #     # sta = [["1.1.sta0000", "1.1.sta0001", "1.1.sta0003"], ["1.1.sta0004", "1.1.sta0005", "1.1.sta0006"]]
+    #
+    #     for i in range(2):
+    #         lf_tools.set_radio_antenna("cli-json/set_wifi_radio", shelf, resource, lf_tools.twog_radios[i], 1)
+    #         time.sleep(0.5)
+    #         sta_ip = lf_test.Client_Connect_Using_Radio(ssid=ssid_name, passkey=profile_data["security_key"],
+    #                                                     radio=lf_tools.twog_radios[i], station_name=sta[i])
+    #         if not sta_ip:
+    #             print("test failed due to no station ip")
+    #             assert False
+    #         time.sleep(0.5)
+    #     for i in range(4):
+    #         lf_test.attenuator_modify(atten_sr[0], i, 100)
+    #         time.sleep(0.5)
+    #     for i in range(4):
+    #         lf_test.attenuator_modify(atten_sr[1], i, 400)
+    #         time.sleep(0.5)
+    #         if i >= 2:
+    #             lf_test.attenuator_modify(atten_sr[1], i, 500)
+    #             time.sleep(0.5)
+    #
+    #
+    #     # lf_tools.Chamber_View()
+    #     wct_obj = lf_test.wifi_capacity(instance_name="tcp_upload_short_med_far_dis_nss1_2g", mode=mode, vlan_id=vlan,
+    #                                     download_rate="0Gbps", batch_size="3,6,9",
+    #                                     upload_rate="4Mbps", protocol="TCP-IPv4", duration="120000",
+    #                                     create_stations=False)
+    #
+    #     report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+    #
+    #     lf_tools.attach_report_graphs(report_name=report_name)
+    #     print("Test Completed... Cleaning up Stations")
+    #     if sta_ip:
+    #         assert True
 
-        for i in range(2):
-            lf_tools.set_radio_antenna("cli-json/set_wifi_radio", shelf, resource, lf_tools.two_radios[i], 1)
-            time.sleep(0.5)
-            sta_ip = lf_test.Client_Connect_Using_Radio(ssid=ssid_name, passkey=profile_data["security_key"], radio=lf_tools.two_radios[i], station_name=sta[i])
-            if not sta_ip:
-                print("test failed due to no station ip")
-                assert False
-            time.sleep(0.5)
-        for i in range(4):
-            lf_tests.attenuator_modify(atten_sr[0], i, 100)
-            time.sleep(0.5)
-        for i in range(2):
-            lf_tests.attenuator_modify(atten_sr[1], i, 400)
-            time.sleep(0.5)
-
-        #lf_tools.Chamber_View()
-        wct_obj = lf_test.wifi_capacity(instance_name="tcp_upload_short_med_dis_nss1_2g", mode=mode, vlan_id=vlan,
-                                        download_rate="0Gbps", batch_size="3,6",
-                                        upload_rate="4Mbps", protocol="TCP-IPv4", duration="120000", create_stations=False)
-
-
-        report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-
-        lf_tools.attach_report_graphs(report_name=report_name)
-        print("Test Completed... Cleaning up Stations")
-        assert True
