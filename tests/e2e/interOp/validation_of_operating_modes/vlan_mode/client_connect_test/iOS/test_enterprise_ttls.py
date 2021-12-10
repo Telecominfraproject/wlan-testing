b1e52d9b706e09fde8f5423b9394605f6327a7c5
sushant <sushant.bawiskar@candelatech.com>
2021-12-10 11:54:35 +0530

    - added random text to ssid for enterprise client_connect/vlan/ios for uniques ssid
    Signed-off-by: Sushant Bawiskar <sushant.bawiskar@candelatech.com>

--- tests/e2e/interOp/validation_of_operating_modes/vlan_mode/client_connect_test/iOS/test_enterprise_ttls.py
+++ tests/e2e/interOp/validation_of_operating_modes/vlan_mode/client_connect_test/iOS/test_enterprise_ttls.py
@@ -10,7 +10,8 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from appium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-
+import random
+import string
 import sys
 import allure
 
@@ -40,7 +41,12 @@ setup_params_enterprise = {
     "rf": {},
     "radius": True
 }
-
+for sec_modes in setup_params_enterprise['ssid_modes'].keys():
+    for i in range(len(setup_params_enterprise['ssid_modes'][sec_modes])):
+        N = 6
+        rand_string = ''.join(random.choices(string.ascii_uppercase +
+                                     string.digits, k=N))
+        setup_params_enterprise['ssid_modes'][sec_modes][i]['ssid_name'] = setup_params_enterprise['ssid_modes'][sec_modes][i]['ssid_name'] + "_"+ rand_string
 
 @allure.suite(suite_name="interop sanity")
 @allure.sub_suite(sub_suite_name="Vlan Mode EAP Client Connect : Suite-A")
