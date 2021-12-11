14654e23257f04db84ee2b3b528795ed492cbd27
KvSaiMahesh <mahesh.karnati@candelatech.com>
2021-12-11 15:21:46 +0530

    added dynamic vlan twog and fiveg testcases
    Signed-off-by: KvSaiMahesh <mahesh.karnati@candelatech.com>

---
+++ tests/e2e/basic/validation_of_operating_modes/vlan_mode/dynamic_vlan/wpa2_enterprise/fiveg/test_dynamic_over_ssid_fiveg.py
@@ -0,0 +1,103 @@
+"""
+
+   Dynamic_Vlan: VLAN Mode
+    pytest -m "dynamic_vlan and wpa2_enterprise and vlan"
+
+"""
+
+import os
+import allure
+import pytest
+from configuration import DYNAMIC_VLAN_RADIUS_SERVER_DATA
+from configuration import DYNAMIC_VLAN_RADIUS_ACCOUNTING_DATA
+
+pytestmark = [pytest.mark.regression, pytest.mark.dynamic_vlan, pytest.mark.wpa2_enterprise, pytest.mark.vlan,pytest.mark.fiveg]
+
+setup_params_general = {
+    "mode": "VLAN",
+    "ssid_modes": {
+        "wpa2_enterprise": [
+            {"ssid_name": "ssid_wpa2e_5g", "appliedRadios": ["5G"],
+             "security_key": "something",
+             "radius_auth_data": DYNAMIC_VLAN_RADIUS_SERVER_DATA,
+             "radius_acc_data": DYNAMIC_VLAN_RADIUS_ACCOUNTING_DATA,
+             "vlan": 100
+             }]},
+    "rf": {},
+    "radius": True
+}
+
+
+@allure.suite("regression")
+@allure.feature("VLAN MODE wpa2_enterprise Dynamic Vlan")
+@pytest.mark.parametrize(
+    'setup_profiles',
+    [setup_params_general],
+    indirect=True,
+    scope="class"
+)
+@pytest.mark.usefixtures("setup_profiles")
+class TestDynamicVlan(object):
+
+    @pytest.mark.dynamic_precedence_over_ssid
+    @pytest.mark.wpa2_enterprise
+    @pytest.mark.fiveg
+    @allure.testcase(name="test_dynamic_precedence_over_ssid_vlan",
+                     url="https://telecominfraproject.atlassian.net/browse/WIFI-5705")
+    def test_dynamic_precedence_over_ssid_vlan(self, get_vif_state, lf_tools,get_ap_logs,get_lf_logs,
+                                                    create_lanforge_chamberview_dut, lf_test, get_configuration,
+                                                    station_names_fiveg):
+        """
+                pytest -m "dynamic_precedence_over_ssid and wpa2_enterprise and vlan"
+        """
+
+        profile_data = setup_params_general["ssid_modes"]["wpa2_enterprise"]
+        ssid_5G = profile_data[0]["ssid_name"]
+        mode = "VLAN"
+        vlan = [100,200]
+        upstream_port = lf_tools.upstream_port
+        print(upstream_port)
+        port_resources = upstream_port.split(".")
+        print(lf_tools.dut_idx_mapping)
+        lf_tools.reset_scenario()
+        lf_tools.add_vlan(vlan_ids=vlan)
+
+        lf_test.EAP_Connect(ssid=ssid_5G, passkey="[BLANK]", security="wpa2", extra_securities=[],
+                            mode=mode, band="fiveg", vlan_id=vlan[0],
+                            station_name=station_names_fiveg, key_mgmt="WPA-EAP",
+                            pairwise="NA", group="NA", wpa_psk="DEFAULT",
+                            ttls_passwd="passwordB", ieee80211w=0,
+                            wep_key="NA", ca_cert="NA", eap="TTLS", identity="userB", d_vlan=True)
+
+        eth_ssid_vlan_ip = lf_tools.json_get("/port/" + port_resources[0] + "/" + port_resources[1] +
+                                               "/" + port_resources[2] + "." + str(vlan[0]))["interface"]["ip"]
+
+        eth_radius_vlan_ip = lf_tools.json_get("/port/" + port_resources[0] + "/" + port_resources[1] +
+                                        "/" + port_resources[2] + "." + str(vlan[1]))["interface"]["ip"]
+        eth_ip = lf_tools.json_get("/port/" + port_resources[0] + "/" + port_resources[1] +
+                                   "/" + port_resources[2])["interface"]["ip"]
+
+        sta_ip_1 = lf_test.station_ip[station_names_fiveg[0]].split('.')
+        eth_vlan_ip_1 = eth_radius_vlan_ip.split('.')
+        print("station ip...", lf_test.station_ip[station_names_fiveg[0]])
+        print("vlan ip...", eth_radius_vlan_ip)
+        print("eth_bridge_ip..", eth_ip)
+        for i, j in zip(sta_ip_1[0:2], eth_vlan_ip_1[0:2]):
+            if i != j:
+                allure.attach(name="station ip....", body=str(lf_test.station_ip[station_names_fiveg[0]]))
+                allure.attach(name="ssid configured vlan..",body=str(port_resources[2] + "." + str(vlan[0])))
+                allure.attach(name="ssid configured vlan ip....", body=str(eth_ssid_vlan_ip))
+                allure.attach(name="radius configured vlan..", body=str(port_resources[2] + "." + str(vlan[1])))
+                allure.attach(name="radius configured vlan ip....", body=str(eth_radius_vlan_ip))
+                allure.attach(name="bridge ip....", body=str(eth_ip))
+                print("Station ip not assigned as per vlan")
+                assert False
+            else:
+                assert True
+                allure.attach(name="station ip....", body=str(lf_test.station_ip[station_names_fiveg[0]]))
+                allure.attach(name="ssid configured vlan..", body=str(port_resources[2] + "." + str(vlan[0])))
+                allure.attach(name="ssid configured vlan ip....", body=str(eth_ssid_vlan_ip))
+                allure.attach(name="ssid configured vlan..", body=str(port_resources[2] + "." + str(vlan[1])))
+                allure.attach(name="radius configured vlan ip....", body=str(eth_radius_vlan_ip))
+                allure.attach(name="bridge ip....", body=str(eth_ip))
+                print("Station ip assigned as per dynamic vlan")
