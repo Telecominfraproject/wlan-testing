285a92db35b0c36778a709fa3463ff0f52a3ff7e
Nikita Yadav <nikita.yadav@candelatech.com>
2021-12-25 14:22:29 +0530

    added mesh option in set mode

--- libs/controller/controller_2x/controller.py
+++ libs/controller/controller_2x/controller.py
@@ -451,11 +451,15 @@ class UProfileUtility:
         self.vlan_section["ssids"] = []
         self.vlan_ids = []
 
-    def set_mode(self, mode):
+    def set_mode(self, mode, mesh=False):
         self.mode = mode
         if mode == "NAT":
             self.base_profile_config['interfaces'][1]['ssids'] = []
         elif mode == "BRIDGE":
+            if mesh :
+                self.base_profile_config['interfaces'][0]['tunnel'] = {
+                    "proto" : "mesh"
+                }
             self.base_profile_config['interfaces'][0]['ssids'] = []
         elif mode == "VLAN":
             del self.base_profile_config['interfaces'][1]
