0288667687624ae50c11cb6c6a707b82d8cc4b8d
Nikita Yadav <nikita.yadav@candelatech.com>
2021-12-29 14:31:36 +0530

    added dhcp disable mesh scenario
    Signed-off-by: Nikita Yadav <nikita.yadav@candelatech.com>

--- libs/lanforge/lf_tools.py
+++ libs/lanforge/lf_tools.py
@@ -389,6 +389,30 @@ class ChamberView:
         mesh = self.Chamber_View()
         return mesh
 
+    def create_mesh_scenario_dhcp_disable(self):
+        upstream_list = []
+        for data in range(0,len(self.access_point_data)):
+            self.CreateDut = DUT(lfmgr=self.lanforge_ip,
+                                 port=self.lanforge_port,
+                                 dut_name="upstream" + str(data))
+            self.CreateDut.lan_port = "10.28.2.1/24"
+            name = "upstream" + str(data)
+            upstream_list.append(name)
+
+            self.CreateDut.setup()
+            data = data + 1
+        self.raw_line = [
+            ["profile_link " + self.upstream_resource_2 + " upstream-dhcp 1 NA NA " + self.upstream_port_2.split(".")[2] + ",AUTO -1 NA"],
+            ["profile_link " + self.uplink_resource_2 + " uplink-nat 1 'DUT: " + str(upstream_list[0]) + " LAN " + self.upstream_subnet + "' NA " + self.uplink_port_2.split(".")[2] + "," + self.upstream_port_2.split(".")[2] + " -1 NA"],
+            ["profile_link " + self.upstream_resource_3 + " upstream 1 NA NA " + self.upstream_port_3.split(".")[2] + ",AUTO -1 NA"],
+            ["profile_link " + self.uplink_resource_3 + " uplink-nat 1 'DUT: " + str(upstream_list[1]) + " LAN " + self.upstream_subnet + "' NA " + self.uplink_port_3.split(".")[2] + "," + self.upstream_port_3.split(".")[2] + " -1 NA"],
+            ["profile_link " + self.upstream_resource_4 + " upstream 1 NA NA " + self.upstream_port_4.split(".")[2] + ",AUTO -1 NA"],
+            ["profile_link " + self.uplink_resource_4 + " uplink-nat 1 'DUT: " + str(upstream_list[2]) + " LAN " + self.upstream_subnet + "' NA " + self.uplink_port_4.split(".")[2] + "," + self.upstream_port_4.split(".")[2] + " -1 NA"]
+        ]
+        print(self.raw_line)
+        mesh = self.Chamber_View()
+        return mesh
+
     def create_mesh_dut(self, ssid_data=None):
         print("hi ssid data", ssid_data)
         for ap, ssid in zip(self.access_point_data, range(len(ssid_data))):
