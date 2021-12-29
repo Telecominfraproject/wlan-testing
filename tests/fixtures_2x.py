e5e6aa9f0dc28c6f59aa25495989e2b621231a0a
jitendracandela <jitendra.kushavah@candelatech.com>
2021-12-29 15:38:25 +0530

    Added set_radio_config method call for dfs
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/fixtures_2x.py
+++ tests/fixtures_2x.py
@@ -368,12 +368,18 @@ class Fixtures_2x:
         test_cases = {}
         profile_data = {}
         var = ""
+        list_key = list(parameter.keys())
 
+        if "dfs" in list_key:
+            print("IN dfs")
+            instantiate_profile_obj.set_radio_config(DFS=True, channel=parameter["dfs"]["channel"], bw=parameter["dfs"]["channel_bandwidth"])
+        else:
+            instantiate_profile_obj.set_radio_config()
         if parameter['mode'] not in ["BRIDGE", "NAT", "VLAN"]:
             print("Invalid Mode: ", parameter['mode'])
             return test_cases
 
-        instantiate_profile_obj.set_radio_config()
+
 
         if parameter['mode'] == "NAT":
             mode = "NAT"
