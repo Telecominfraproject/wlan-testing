cbd18dd2ba67033a02b155f99e6aa84e6a4edcc1
Nikita Yadav <nikita.yadav@candelatech.com>
2022-01-12 23:46:49 +0530

    First mesh test case
    Signed-off-by: Nikita Yadav <nikita.yadav@candelatech.com>

--- tests/e2e/mesh/Mesh/test_mesh_bridge.py
+++ tests/e2e/mesh/Mesh/test_mesh_bridge.py
@@ -1,8 +1,8 @@
 import pytest
 import allure
+import os
 
 pytestmark = [pytest.mark.mesh, pytest.mark.bridge]
-
 
 setup_params_general = {
     "mode": "BRIDGE",
@@ -16,6 +16,8 @@ setup_params_general = {
     "rf": {},
     "radius": False
 }
+
+
 @allure.feature("MESH BASIC")
 @pytest.mark.parametrize(
     'setup_mesh_profile_fix',
@@ -27,5 +29,16 @@ setup_params_general = {
 class TestMesh(object):
 
     @pytest.mark.wpa2_personal
-    def testmesh(self, setup_mesh_profile_fix):
-        pass
+    def testmesh_2g(self, setup_mesh_profile_fix, lf_test, lf_tools):
+        raw_lines = [['selected_dut2: tip-node2 OpenWifi123 90:3c>b3:9d:69:36(1)'], ['selected_dut5: tip-node2 OpenWifi 90:3c:b3:9d:69:2f(2)'], ['sta_amount-2: 2'], ['radios-2-0: 1.4.6 wiphy0'],
+               ['radios-2-3: 1.4.7 wiphy1'], ['ap_arrangements: Current Position'], ['sta_position: Current Position'],
+               ['path: Orbit Current'], ['traffic_types: UDP;TCP'], ['direction: Both'], ['tests: Throughput'], ['traf_combo: N2']]
+
+        mesh_o = lf_test.mesh_test(instance_name="MESH-01-INSTANCE", raw_lines=raw_lines)
+        report_name = mesh_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+        print("report name ", report_name)
+        entries = os.listdir("../reports/" + report_name + '/')
+        print("entries", entries)
+        lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Basic Mesh Test")
+
+        assert True
