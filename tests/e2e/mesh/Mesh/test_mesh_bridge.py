80fd97629fb326131ea5c68deb1ee15046447c32
KvSaiMahesh <76985775+KvSaiMahesh@users.noreply.github.com>
2022-01-24 15:30:36 +0530

    added chamber2,speed,speed2 parameters

--- tests/e2e/mesh/Mesh/test_mesh_bridge.py
+++ tests/e2e/mesh/Mesh/test_mesh_bridge.py
@@ -31,7 +31,7 @@ class TestMesh(object):
     @pytest.mark.wpa2_personal
     def testmesh_2g(self, setup_mesh_profile_fix, lf_test, lf_tools):
         raw_lines = [['selected_dut2: tip-node-2 ssid_wpa2_2g 90:3c:b3:9d:69:36(2)'], ['selected_dut5: tip-node-2 ssid_wpa2_5g 90:3c:b3:9d:69:2e(1)'], ['sta_amount-2: 5'], ['radios-2-0: 1.4.6 wiphy0'],
-               ['ap_arrangements: Current Position'], ['sta_position: Current Position'],
+               ['ap_arrangements: Current Position'], ['sta_position: Current Position'],['chamber-2: Node2'],['speed: 85%'],['speed2: 85%'],
                ['path: Orbit Current'], ['traffic_types: TCP'], ['direction: Both'], ['tests: Throughput'], ['traf_combo: N2'],
                      ["skip_dhcp: 1"], ["skip_5: 1"], ]
 
