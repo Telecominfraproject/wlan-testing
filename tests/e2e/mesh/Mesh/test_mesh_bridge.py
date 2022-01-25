45d41fdff8cf949ee4b20de41d688cc7d7e6ed41
KvSaiMahesh <76985775+KvSaiMahesh@users.noreply.github.com>
2022-01-25 09:16:46 +0530

    added changes to ssid name,instance name,rawlines

--- tests/e2e/mesh/Mesh/test_mesh_bridge.py
+++ tests/e2e/mesh/Mesh/test_mesh_bridge.py
@@ -8,8 +8,8 @@ setup_params_general = {
     "mode": "BRIDGE",
     "ssid_modes": {
         "wpa2_personal": [
-            {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"},
-            {"ssid_name": "ssid_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"}
+            {"ssid_name": "ssid_wpa2_5g_1", "appliedRadios": ["5G"], "security_key": "something"},
+            {"ssid_name": "ssid_wpa2_2g_1", "appliedRadios": ["2G"], "security_key": "something"}
         ]
     },
     "mesh": "yes",
@@ -30,12 +30,12 @@ class TestMesh(object):
 
     @pytest.mark.wpa2_personal
     def testmesh_2g(self, setup_mesh_profile_fix, lf_test, lf_tools):
-        raw_lines = [['selected_dut2: tip-node-2 ssid_wpa2_2g 90:3c:b3:9d:69:36(2)'], ['selected_dut5: tip-node-2 ssid_wpa2_5g 90:3c:b3:9d:69:2e(1)'], ['sta_amount-2: 5'], ['radios-2-0: 1.4.6 wiphy0'],
-               ['ap_arrangements: Current Position'], ['sta_position: Current Position'],['chamber-2: Node2'],['speed: 85%'],['speed2: 85%'],
+        raw_lines = [['selected_dut2: tip-node-2 ssid_wpa2_2g_1 90:3c:b3:9d:69:36(2)'], ['selected_dut5: tip-node-2 ssid_wpa2_5g_1 90:3c:b3:9d:69:2e(1)'], ['sta_amount-2: 2'], ['radios-2-0: 1.4.6 wiphy0'],
+               ['ap_arrangements: Current Position'], ['sta_position: Current Position'],['chamber-2: Node2'],
                ['path: Orbit Current'], ['traffic_types: TCP'], ['direction: Both'], ['tests: Throughput'], ['traf_combo: N2'],
-                     ["skip_dhcp: 1"], ["skip_5: 1"], ]
+                     ["skip_dhcp: 1"], ["skip_5: 1"] ]
 
-        mesh_o = lf_test.mesh_test(instance_name="test_mesh1", raw_lines=raw_lines,duration="5m")
+        mesh_o = lf_test.mesh_test(instance_name="test_mesh1_1", raw_lines=raw_lines,duration="5m")
         report_name = mesh_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
         print("report name ", report_name)
         entries = os.listdir("../reports/" + report_name + '/')
