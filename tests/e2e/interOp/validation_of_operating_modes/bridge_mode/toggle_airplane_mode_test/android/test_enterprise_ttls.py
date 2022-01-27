083b46d214440af10b049304f0ba116bf4f86524
haricharan-jaka <haricharan.jaka@candelatech.com>
2022-01-27 19:55:59 +0530

    Changed sanity marker to regression

--- tests/e2e/interOp/validation_of_operating_modes/bridge_mode/toggle_airplane_mode_test/android/test_enterprise_ttls.py
+++ tests/e2e/interOp/validation_of_operating_modes/bridge_mode/toggle_airplane_mode_test/android/test_enterprise_ttls.py
@@ -17,7 +17,7 @@ import allure
 if 'perfecto_libs' not in sys.path:
     sys.path.append(f'../libs/perfecto_libs')
 
-pytestmark = [pytest.mark.sanity, pytest.mark.interop, pytest.mark.android, pytest.mark.interop_and, pytest.mark.ToggleAirplaneMode,
+pytestmark = [pytest.mark.regression, pytest.mark.interop, pytest.mark.android, pytest.mark.interop_and, pytest.mark.ToggleAirplaneMode,
               pytest.mark.client_reconnect, pytest.mark.enterprise]
 
 from android_lib import closeApp, set_APconnMobileDevice_android, Toggle_AirplaneMode_android, ForgetWifiConnection, openApp,\
@@ -46,7 +46,7 @@ for sec_modes in setup_params_enterprise['ssid_modes'].keys():
                                      string.digits, k=N)))+str(int(time.time_ns())%10000)
         setup_params_enterprise['ssid_modes'][sec_modes][i]['ssid_name'] = setup_params_enterprise['ssid_modes'][sec_modes][i]['ssid_name'] + "_"+ rand_string
 
-@allure.suite(suite_name="interop sanity")
+@allure.suite(suite_name="interop regression")
 @allure.sub_suite(sub_suite_name="Bridge Mode EAP Client ReConnect : Suite-A")
 @pytest.mark.suiteA
 @pytest.mark.parametrize(
