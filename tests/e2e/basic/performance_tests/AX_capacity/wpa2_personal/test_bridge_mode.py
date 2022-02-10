8974e28cf6aaa1735b2013b3c7c67f61fa19fd1f
jitendracandela <jitendra.kushavah@candelatech.com>
2022-02-10 20:46:37 +0530

    Added module level marker and jira number to test cases
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/e2e/basic/performance_tests/AX_capacity/wpa2_personal/test_bridge_mode.py
+++ tests/e2e/basic/performance_tests/AX_capacity/wpa2_personal/test_bridge_mode.py
@@ -8,7 +8,7 @@ import os
 import pytest
 import allure
 
-pytestmark = [pytest.mark.performance, pytest.mark.bridge]
+pytestmark = [pytest.mark.performance, pytest.mark.bridge, pytest.mark.wifi_capacity_ax]
 setup_params_general_5G = {
     "mode": "BRIDGE",
     "ssid_modes": {
@@ -32,13 +32,12 @@ setup_params_general_5G = {
 @pytest.mark.wpa2_personal
 @pytest.mark.fiveg
 @pytest.mark.fiveg_band
-@pytest.mark.ax
 class TestWifiCapacityBRIDGEModeAX5G(object):
     """ Wifi Capacity Test BRIDGE mode
            pytest -m "wifi_capacity_test and BRIDGE"
     """
 
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-3929", name="WIFI-3929")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6934", name="WIFI-6934")
     @pytest.mark.tcp_download
     def test_client_wpa2_BRIDGE_tcp_dl(self, lf_tools, setup_profiles,
                                        lf_test, station_names_twog, create_lanforge_chamberview_dut,
@@ -62,7 +61,7 @@ class TestWifiCapacityBRIDGEModeAX5G(object):
         print("Test Completed... Cleaning up Stations")
         assert True
 
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-3931", name="WIFI-3931")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6944", name="WIFI-6944")
     @pytest.mark.udp_download
     def test_client_wpa2_BRIDGE_udp_dl(self, lf_tools,
                                        lf_test, station_names_twog, create_lanforge_chamberview_dut,
@@ -86,7 +85,7 @@ class TestWifiCapacityBRIDGEModeAX5G(object):
         print("Test Completed... Cleaning up Stations")
         assert True
 
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-3936", name="WIFI-3936")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6943", name="WIFI-6943")
     @pytest.mark.tcp_bidirectional
     def test_client_wpa2_BRIDGE_tcp_bidirectional(self, lf_tools,
                                                   lf_test, station_names_twog, create_lanforge_chamberview_dut,
@@ -110,7 +109,7 @@ class TestWifiCapacityBRIDGEModeAX5G(object):
         print("Test Completed... Cleaning up Stations")
         assert True
 
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-3937", name="WIFI-3937")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6946", name="WIFI-6946")
     @pytest.mark.udp_bidirectional
     def test_client_wpa2_BRIDGE_udp_bidirectional(self, lf_tools,
                                                   lf_test, station_names_twog, create_lanforge_chamberview_dut,
@@ -134,7 +133,7 @@ class TestWifiCapacityBRIDGEModeAX5G(object):
         print("Test Completed... Cleaning up Stations")
         assert True
 
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-4000", name="WIFI-4000")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6942", name="WIFI-6942")
     @pytest.mark.tcp_upload
     def test_client_wpa2_BRIDGE_tcp_ul(self, lf_tools, setup_profiles,
                                        lf_test, station_names_twog, create_lanforge_chamberview_dut,
@@ -158,7 +157,7 @@ class TestWifiCapacityBRIDGEModeAX5G(object):
         print("Test Completed... Cleaning up Stations")
         assert True
 
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-40001", name="WIFI-4001")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6945", name="WIFI-6945")
     @pytest.mark.udp_upload
     def test_client_wpa2_BRIDGE_udp_ul(self, lf_tools,
                                        lf_test, station_names_twog, create_lanforge_chamberview_dut,
