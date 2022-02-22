0d7d2c0aa584cad882385802260ea9dc55eef186
karthikaeyetea <karthika.subramani@candelatech.com>
2022-02-22 15:54:15 +0530

    checking from setup_params_general 'rf' to set radio config

--- tests/fixtures_2x.py
+++ tests/fixtures_2x.py
@@ -385,6 +385,9 @@ class Fixtures_2x:
         if "dfs" in list_key:
             print("In dfs")
             instantiate_profile_obj.set_radio_config(DFS=True, channel=parameter["dfs"]["channel"], bw=parameter["dfs"]["channel_bandwidth"])
+        if len(parameter['rf']) > 0:
+            print("In country code channel division")
+            instantiate_profile_obj.set_radio_config(radio_config=parameter['rf'])
         else:
             instantiate_profile_obj.set_radio_config()
         if parameter['mode'] not in ["BRIDGE", "NAT", "VLAN"]:
