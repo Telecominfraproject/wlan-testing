5386a47ea1301fa19eec795cc0ab7696732a448c
Nikita Yadav <nikita.yadav@candelatech.com>
2022-02-24 15:44:00 +0530

    sync lanforge-scripts
    Signed-off-by: Nikita Yadav <nikita.yadav@candelatech.com>

--- dependencies.bash
+++ dependencies.bash
@@ -3,7 +3,7 @@
 
 if [ -d ../lanforge-scripts ]
 then
-    rm -fr wlan-testing/libs/controller/controller_3x/wifi_ctl_9800_3504.py
+    rm -fr lanforge/lanforge-scripts
 
-    cp -a ../lanforge-scripts/wifi_ctl_9800_3504.py ../wlan-testing/libs/controller/controller_3x/wifi_ctl_9800_3504.py
+    cp -a ../lanforge-scripts lanforge/lanforge-scripts
 fi
