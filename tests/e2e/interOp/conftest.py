931d28111026a2b91bea498349f87adc4c00fbf3
haricharan-jaka <haricharan.jaka@candelatech.com>
2022-03-08 16:22:09 +0530

    Added run-lf argument in return function of setup_profiles
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>

--- tests/e2e/interOp/conftest.py
+++ tests/e2e/interOp/conftest.py
@@ -232,7 +232,7 @@ def setup_profiles(request, setup_controller, testbed, get_equipment_ref, fixtur
                                              instantiate_profile,
                                              get_markers, create_lanforge_chamberview_dut, lf_tools,
                                              get_security_flags, get_configuration, radius_info, get_apnos,
-                                             radius_accounting_info, skip_lf=skip_lf, open_flow=get_openflow)
+                                             radius_accounting_info, skip_lf=skip_lf, open_flow=get_openflow, run_lf=run_lf)
     print("sleeping for 120 sec.")
     time.sleep(120)
     print("Done sleeping")
