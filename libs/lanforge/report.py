0177cc568496d6129ef8babe4bfc1d4eceee924e
Nikita Yadav <nikita.yadav@candelatech.com>
2022-03-10 13:53:06 +0530

    added report library
    Signed-off-by: Nikita Yadav <nikita.yadav@candelatech.com>

---
+++ libs/lanforge/report.py
@@ -0,0 +1,22 @@
+from tabulate import tabulate
+
+class Report:
+    def __init__(self, key1=None,
+                 key2=None,
+                 val1=None,
+                 val2=None):
+        self.key1 = key1
+        self.key2 = key2
+        self.val1 = val1
+        self.val2 = val2
+
+    def table1(self):
+        table ={str(self.key1): self.val1,  str(self.key2):self.val2}
+        x = tabulate(table, headers="keys", tablefmt="fancy_grid")
+        return x
+
+    def table2(self, table):
+        self.table = table
+        x = tabulate(self.table, headers='firstrow', tablefmt='fancy_grid')
+        return x
+
