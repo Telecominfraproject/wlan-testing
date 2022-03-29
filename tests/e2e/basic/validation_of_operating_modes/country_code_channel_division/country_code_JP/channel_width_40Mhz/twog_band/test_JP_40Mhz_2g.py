623469a000642a9134bf6981b62e94f181ca3bff
karthikaeyetea <karthika.subramani@candelatech.com>
2022-03-29 11:46:28 +0530

    Added testcases - 2G(20Mhz+40Mhz) channel-14
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/basic/validation_of_operating_modes/country_code_channel_division/country_code_JP/channel_width_40Mhz/twog_band/test_JP_40Mhz_2g.py
+++ tests/e2e/basic/validation_of_operating_modes/country_code_channel_division/country_code_JP/channel_width_40Mhz/twog_band/test_JP_40Mhz_2g.py
@@ -845,3 +845,67 @@ class TestCountryJP40Mhz2GChannel13(object):
             assert True
         else:
             assert False
+
+
+setup_params_general14 = {
+    "mode": "BRIDGE",
+    "ssid_modes": {
+        "wpa2_personal": [{"ssid_name": "ssid_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"},
+                          {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}
+                          ]},
+    "rf": {
+        "5G":
+        {'band': '5G',
+        'country': 'JP',
+        "channel-mode": "VHT",
+        'channel-width': 40,
+        "channel": 36},
+        "2G":
+        {'band': '2G',
+        'country': 'JP',
+        'channel-mode': 'VHT',
+        'channel-width': 40,
+        "channel": 14}
+           },
+    "radius": False
+}
+
+@allure.feature("BRIDGE MODE CLIENT CONNECTIVITY")
+@pytest.mark.parametrize(
+    'setup_profiles',
+    [setup_params_general14],
+    indirect=True,
+    scope="class"
+)
+@pytest.mark.usefixtures("setup_profiles")
+class TestCountryJP40Mhz2GChannel14(object):
+    """Country code along with Channel and Channel-width Test Bridge mode
+       pytest -m "country_code and Bridge"
+    """
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2546", name="WIFI-6938")
+    @pytest.mark.wpa2_personal
+    @pytest.mark.fourtyMhz
+    @pytest.mark.twog
+    @pytest.mark.channel14
+    def test_client_bridge_wpa2_chn14_40Mhz_JP_2g(self, lf_test, station_names_twog, get_configuration):
+        """Country code Bridge Mode
+           pytest -m "country_code and fourtyMhz and wpa2 and twog and channel14"
+        """
+        profile_data = setup_params_general14["ssid_modes"]["wpa2_personal"][0]
+        ssid = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "twog"
+        vlan = 1
+        channel = setup_params_general14['rf']['2G']['channel']
+        channel_width = setup_params_general14['rf']['2G']['channel-width']
+
+        result = lf_test.country_code_channel_division(ssid=ssid, security=security, passkey=security_key, mode=mode,
+                                                       band=band, station_name=station_names_twog, vlan_id=vlan,
+                                                       channel=channel,channel_width=channel_width,country_num=392,
+                                                       country='Japan(JP)')
+        if result:
+            assert True
+        else:
+            assert False
