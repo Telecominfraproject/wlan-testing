7db9b9e9b3cb27aa0aaecfb28458b60f0dfb0303
Nikita Yadav <nikita.yadav@candelatech.com>
2022-04-04 22:19:06 +0530

    hard roam 2g method
    Signed-off-by: Nikita Yadav <nikita.yadav@candelatech.com>

--- tests/e2e/advanced/roam_test/hard_roam/OTA/test_roam_2g.py
+++ tests/e2e/advanced/roam_test/hard_roam/OTA/test_roam_2g.py
@@ -1,5 +1,6 @@
 import pytest
 import allure
+import time
 
 pytestmark = [pytest.mark.roam_test, pytest.mark.bridge]
 
@@ -17,7 +18,7 @@ setup_params_general = {
     "rf": {},
     "radius": False
 }
-@allure.suite("Roam Test with attenuator")
+@allure.suite("Hard Roam 2g")
 @allure.feature("Roam Test")
 # @pytest.mark.parametrize(
 #     'setup_profiles',
@@ -32,9 +33,10 @@ class TestRoamOTATwog(object):
 
     @pytest.mark.hard_roam_2g_ota
     @pytest.mark.wpa2_personal
-    def test_multi_hard_roam_2g_to_2g(self, get_configuration, lf_test, lf_reports, station_names_twog, lf_tools,
+    def test_multi_hard_roam_2g_to_2g(self, get_configuration, lf_test, lf_reports, lf_tools,
                                       run_lf, add_env_properties,
-                                      instantiate_profile, get_controller_logs, get_ap_config_slots, get_lf_logs):
+                                      instantiate_profile, get_controller_logs, get_ap_config_slots, get_lf_logs,
+                                      roaming_delay, iteration, client):
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
         ssid_name = profile_data["ssid_name"]
         security_key = profile_data["security_key"]
@@ -42,9 +44,31 @@ class TestRoamOTATwog(object):
         mode = "BRIDGE"
         band = "twog"
         vlan = 1
+        # check channel
+
+        lf_test.create_n_clients(sta_prefix="wlan1", num_sta=1, dut_ssid=ssid_name,
+                                 dut_security=security, dut_passwd=security_key, band="twog",
+                                 lf_tools=lf_tools, type="11r")
+        sta_list = lf_tools.get_station_list()
+        print(sta_list)
+        val = lf_test.wait_for_ip(station=sta_list)
+        ch = ""
+        if val:
+            for sta_name in sta_list:
+                sta = sta_name.split(".")[2]
+                time.sleep(5)
+                ch = lf_tools.station_data_query(station_name=str(sta), query="channel")
+            print(ch)
+            lf_test.Client_disconnect(station_name=sta_list)
+
+        else:
+            pytest.exit("station failed to get ip")
+            assert False
+
         lf_test.multi_hard_roam(run_lf=run_lf, get_configuration=get_configuration, lf_tools=lf_tools,
                                 lf_reports=lf_reports,
                                 instantiate_profile=instantiate_profile,
                                 ssid_name=ssid_name, security=security, security_key=security_key,
-                                mode=mode, band=band, station_name=station_names_twog, vlan=vlan, test="2g",
-                                iteration=1, num_sta=5)
+                                band=band, test="2g",
+                                iteration=int(iteration), num_sta=int(client), roaming_delay=roaming_delay,
+                                option="ota", channel=ch)
