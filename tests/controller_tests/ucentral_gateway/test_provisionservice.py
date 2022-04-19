93dd188041c8c63cfcfb0164866e1afaa896ea52
haricharan-jaka <haricharan.jaka@candelatech.com>
2022-04-19 12:49:44 +0530

    Added Provision service file for Provision API tetcases
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>

---
+++ tests/controller_tests/ucentral_gateway/test_provisionservice.py
@@ -0,0 +1,31 @@
+"""
+
+    Provision Services Rest API Tests
+
+"""
+
+import string
+import random
+
+import pytest
+import json
+import allure
+
+
+@pytest.mark.uc_sanity
+@allure.feature("SDK PROV REST API")
+class TestUcentralProvisionService(object):
+
+    @pytest.mark.sdk_restapi
+    def test_provservice_inventorylist(self, setup_controller):
+        """
+            Test the list devices endpoint
+            WIFI-3452
+        """
+        resp = setup_controller.request("prov", "inventory", "GET", None, None)
+        body = resp.url + "," + str(resp.status_code) + ',' + resp.text
+        allure.attach(name="prov inventory list", body=body)
+        if resp.status_code != 200:
+            assert False
+        inven = json.loads(resp.text)
+        print(inven)
