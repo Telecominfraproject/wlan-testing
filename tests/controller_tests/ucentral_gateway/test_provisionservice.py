1a154d68ec948d82e482bd2fd24c4708eb187fef
haricharan-jaka <haricharan.jaka@candelatech.com>
2022-04-20 19:19:00 +0530

    Added Prov UI test cases for creating device in Inventory
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>

--- tests/controller_tests/ucentral_gateway/test_provisionservice.py
+++ tests/controller_tests/ucentral_gateway/test_provisionservice.py
@@ -30,6 +30,46 @@ class TestUcentralProvisionService(object):
         inven = json.loads(resp.text)
         print(inven)
 
+    @pytest.mark.prov_api_test
+    def test_gwservice_create_inventory_device(self, setup_controller, testbed):
+        """
+            Test the create device in provision Inventory
+        """
+        device_mac = "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255),
+                                     random.randint(0, 255),
+                                     random.randint(0, 255))
+        device_name = device_mac.replace(":", "")
+        # device_name = "deadbeef0011" + testbed.replace("-","")
+        payload = {'serialNumber': device_name,
+                   'UUID': '123456',
+                   'configuration': self.configuration,
+                   'deviceType': 'AP',
+                   'location': '',
+                   'macAddress': device_mac,
+                   'manufacturer': 'Testing',
+                   'owner': ''}
+        print(json.dumps(payload))
+        resp = setup_controller.request("prov", "inventory/" + device_name, "POST", None, json.dumps(payload))
+        allure.attach(name="response: ", body=str(resp.json()))
+        body = resp.url + "," + str(resp.status_code) + ',' + resp.text
+        allure.attach(name="Prov create devices", body=body)
+        if resp.status_code != 200:
+            assert False
+        devices = json.loads(resp.text)
+        print(devices)
+
+        resp = setup_controller.request("prov", "inventory/" + device_name, "GET", None, None)
+        body = resp.url + "," + str(resp.status_code) + ',' + resp.text
+        allure.attach(name="Prov create device verify", body=body)
+        if resp.status_code != 200:
+            assert False
+
+        resp = setup_controller.request("prov", "inventory/" + device_name, "DELETE", None, None)
+        body = resp.url + "," + str(resp.status_code) + ',' + resp.text
+        allure.attach(name="Prov create device delete", body=body)
+        if resp.status_code != 200:
+            assert False
+
     @pytest.mark.system_info_prov
     def test_system_info_prov(self, setup_controller):
         system_info = setup_controller.get_system_prov()
