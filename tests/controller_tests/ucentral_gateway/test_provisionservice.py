400ac2f8d3fca1aa43c67cebc421a7ef7cdb63f4
haricharan-jaka <haricharan.jaka@candelatech.com>
2022-04-20 18:52:04 +0530

    Added system info test case of Provision UI
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>

--- tests/controller_tests/ucentral_gateway/test_provisionservice.py
+++ tests/controller_tests/ucentral_gateway/test_provisionservice.py
@@ -20,8 +20,7 @@ class TestUcentralProvisionService(object):
     @pytest.mark.prov_api
     def test_provservice_inventorylist(self, setup_controller):
         """
-            Test the list devices endpoint
-            WIFI-3452
+            Test the list of devices present in Provisioning UI
         """
         resp = setup_controller.request("prov", "inventory", "GET", None, None)
         body = resp.url + "," + str(resp.status_code) + ',' + resp.text
@@ -30,3 +29,10 @@ class TestUcentralProvisionService(object):
             assert False
         inven = json.loads(resp.text)
         print(inven)
+
+    @pytest.mark.system_info_prov
+    def test_system_info_prov(self, setup_controller):
+        system_info = setup_controller.get_system_prov()
+        print(system_info.json())
+        allure.attach(name="system info", body=str(system_info.json()), attachment_type=allure.attachment_type.JSON)
+        assert system_info.status_code == 200
