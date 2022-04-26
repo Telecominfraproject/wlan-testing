ccd5850a800133ec2ad2ae8e352978fe5ce326d8
haricharan-jaka <haricharan.jaka@candelatech.com>
2022-04-26 14:49:39 +0530

    Added Entity Related testcases
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>

--- tests/controller_tests/ucentral_gateway/test_provisionservice.py
+++ tests/controller_tests/ucentral_gateway/test_provisionservice.py
@@ -74,7 +74,7 @@ class TestUcentralProvisionService(object):
         if resp.status_code != 200:
             assert False
 
-        resp = setup_prov_controller.request("prov", "inventory/" + device_name, "DELETE", None, None)
+        resp = setup_prov_controller.delete_device_from_inventory(device_name)
         body = resp.url + "," + str(resp.status_code) + ',' + resp.text
         allure.attach(name="Prov created device-delete", body=body)
         if resp.status_code != 200:
@@ -87,51 +87,35 @@ class TestUcentralProvisionService(object):
         allure.attach(name="system info", body=str(system_info.json()), attachment_type=allure.attachment_type.JSON)
         assert system_info.status_code == 200
 
-    @pytest.mark.prov_api_test
+    @pytest.mark.prov_api_entity_test
     def test_prov_service_create_entity(self, setup_prov_controller, testbed):
         """
-            Test the create device in provision Inventory
+            Test the create Entity in provision Inventory
         """
-        device_mac = "02:00:00:%02x:%02x:%02x" % (random.randint(0, 255),
-                                                  random.randint(0, 255),
-                                                  random.randint(0, 255))
-        device_name = device_mac.replace(":", "")
-        # device_name = "deadbeef0011" + testbed.replace("-","")
-        payload = {"serialNumber": device_name,
-                   "name": "Testing_to_add_device_through_automation",
-                   "rrm": "inherit",
-                   "deviceType": "edgecore_eap101",
-                   "devClass": "any",
-                   "description": "For testing API through automation",
-                   "entity": "",
-                   "venue": "",
-                   "subscriber": "",
-                   "__newConfig":
-                       {"rrm": "inherit",
-                        "firmwareUpgrade": "no",
-                        "configuration": [],
-                        "name": "Device added through automation",
-                        "description": "Created from the Edit Tag menu",
-                        "deviceTypes": ["edgecore_eap101"]
-                        }
+        payload = {"name": "Testing_prov",
+                    "rrm": "inherit",
+                    "description": "For testing Purposes through Automation",
+                    "notes": [{"note": "For testing Purposes through Automation"}],
+                    "parent": "0000-0000-0000"
                    }
         print(json.dumps(payload))
-        resp = setup_prov_controller.add_device_to_inventory(device_name, payload)
+        resp = setup_prov_controller.add_entity(payload)
         allure.attach(name="response: ", body=str(resp.json()))
         body = resp.url + "," + str(resp.status_code) + ',' + resp.text
-        allure.attach(name="Prov create device", body=body)
+        allure.attach(name="Prov create entity", body=body)
         if resp.status_code != 200:
             assert False
-        devices = json.loads(resp.text)
-        print(devices)
+        entitiy = json.loads(resp.text)
+        print(entitiy)
+        entity_id = entitiy['id']
 
-        resp = setup_prov_controller.get_inventory_by_device(device_name)
+        resp = setup_prov_controller.get_entity_by_id(entity_id)
         body = resp.url + "," + str(resp.status_code) + ',' + resp.text
         allure.attach(name="Prov create device-verify", body=body)
         if resp.status_code != 200:
             assert False
 
-        resp = setup_prov_controller.request("prov", "inventory/" + device_name, "DELETE", None, None)
+        resp = setup_prov_controller.delete_entity(entity_id)
         body = resp.url + "," + str(resp.status_code) + ',' + resp.text
         allure.attach(name="Prov created device-delete", body=body)
         if resp.status_code != 200:
