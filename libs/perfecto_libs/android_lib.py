5aecccec72dc9051aa43b25ab70cb808adc22c47
AjaydeepGrewal <grewal19in@gmail.com>
2022-04-27 03:02:56 -0400

    WIFI-7218: Scrolling for s20 on additional details page (#445)
    * Scrolling for s20
    
    Signed-off-by: Ajaydeep Grewal <grewal19in@gmail.com>
    
    * Added scrlloing of S20 in Enterprise function also
    
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>
    
    Co-authored-by: haricharan-jaka <haricharan.jaka@candelatech.com>

--- libs/perfecto_libs/android_lib.py
+++ libs/perfecto_libs/android_lib.py
@@ -1498,11 +1498,25 @@ def get_ip_address_and(request, WifiName, WifiPass, setup_perfectoMobile, connDa
                                 closeApp(connData["appPackage-android"], setup_perfectoMobile)
 
                             report.step_start("Scrolling for ip address - 1")
-                            scrollDown(setup_perfectoMobile)
+                            if deviceModelName == "Galaxy S20":
+                                print("Scrolling for S20")
+                                driver.swipe(470, 1400, 470, 1000, 400)
+                            else:
+                                scrollDown(setup_perfectoMobile)
+
                             report.step_start("Scrolling for ip address - 2")
-                            scrollDown(setup_perfectoMobile)
+                            if deviceModelName == "Galaxy S20":
+                                print("Scrolling for S20")
+                                driver.swipe(470, 1400, 470, 1000, 400)
+                            else:
+                                scrollDown(setup_perfectoMobile)
+
                             report.step_start("Scrolling for ip address - 3")
-                            scrollDown(setup_perfectoMobile)
+                            if deviceModelName == "Galaxy S20":
+                                print("Scrolling for S20")
+                                driver.swipe(470, 1400, 470, 1000, 400)
+                            else:
+                                scrollDown(setup_perfectoMobile)
                             report.step_start("looking for ip address")
 
                             try:
@@ -1511,6 +1525,7 @@ def get_ip_address_and(request, WifiName, WifiPass, setup_perfectoMobile, connDa
                                 ip_address_element_text = ip_address_element_text.text
                                 ssid_with_internet = True
                             except:
+                                print("Unable to get IP address")
                                 pass
 
                             report.step_start("Forget SSID")
@@ -3352,7 +3367,26 @@ def get_ip_address_eap_and(request, WifiName, User, ttls_passwd, setup_perfectoM
                                 closeApp(connData["appPackage-android"], setup_perfectoMobile)
 
                             report.step_start("Scrolling for ip address - 1")
-                            scrollDown(setup_perfectoMobile)
+                            if deviceModelName == "Galaxy S20":
+                                print("Scrolling for S20")
+                                driver.swipe(470, 1400, 470, 1000, 400)
+                            else:
+                                scrollDown(setup_perfectoMobile)
+
+                            report.step_start("Scrolling for ip address - 2")
+                            if deviceModelName == "Galaxy S20":
+                                print("Scrolling for S20")
+                                driver.swipe(470, 1400, 470, 1000, 400)
+                            else:
+                                scrollDown(setup_perfectoMobile)
+
+                            report.step_start("Scrolling for ip address - 3")
+                            if deviceModelName == "Galaxy S20":
+                                print("Scrolling for S20")
+                                driver.swipe(470, 1400, 470, 1000, 400)
+                            else:
+                                scrollDown(setup_perfectoMobile)
+                            report.step_start("looking for ip address")
 
                             try:
                                 ip_address_element_text = driver.find_element_by_xpath(
