2cb414b878aef820c4d5e199d5869ea59041e6ca
Haricharan Jaka <87761091+haricharan-jaka@users.noreply.github.com>
2022-05-10 22:28:38 +0530

    Wifi 7858 (#518)
    * Crated a new file for FMS related testcases
    
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>
    
    * Removed Fms system ifo test case from gateway service file
    
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>
    
    * Added Location related test cases of Prov UI
    
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>
    
    * Added Location related functions for support
    
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>
    
    * Changed the function name
    
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>
    
    * Changed the allure attachment names on edit functions
    
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>

--- tests/controller_tests/ucentral_gateway/test_gatewayservice.py
+++ tests/controller_tests/ucentral_gateway/test_gatewayservice.py
@@ -252,9 +252,3 @@ class TestUcentralGatewayService(object):
         allure.attach(name="system info", body=str(system_info.json()),attachment_type=allure.attachment_type.JSON)
         assert system_info.status_code == 200
 
-    @pytest.mark.system_info_gw
-    def test_system_info_fms(self, setup_controller):
-        system_info = setup_controller.get_system_fms()
-        print(system_info.json())
-        allure.attach(name="system info", body=str(system_info.json()), attachment_type=allure.attachment_type.JSON)
-        assert system_info.status_code == 200
