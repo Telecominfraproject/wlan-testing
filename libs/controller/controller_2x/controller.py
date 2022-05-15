0ccb47ea18214fe642e989aa5c8df94b7bde7a24
Haricharan Jaka <87761091+haricharan-jaka@users.noreply.github.com>
2022-05-15 10:29:36 +0530

    WIFI-7950 Added config test load script (#528)

--- libs/controller/controller_2x/controller.py
+++ libs/controller/controller_2x/controller.py
@@ -931,9 +931,10 @@ class UProfileUtility:
         allure.attach(name="/configure response: " + str(resp.status_code), body=str(resp.json()),
                       attachment_type=allure.attachment_type.JSON)
         self.sdk_client.check_response("POST", resp, self.sdk_client.make_headers(), basic_cfg_str, uri)
-        # print(resp.url)
+        print(resp.url)
         resp.close()
         print(resp)
+        return resp
 
 
 if __name__ == '__main__':
