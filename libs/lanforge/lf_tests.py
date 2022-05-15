d78edaa1a8917dbb95f2de7a37d2a7520b95f14d
anil-tegala <anil.tegala@candelatech.com>
2022-05-16 00:52:44 +0530

    test information data table added to allure
    Signed-off-by: anil-tegala <anil.tegala@candelatech.com>

--- libs/lanforge/lf_tests.py
+++ libs/lanforge/lf_tests.py
@@ -938,7 +938,42 @@ class RunTest:
         # station_name =  ['sta100', 'sta200', 'sta00']
         cli_base = LFCliBase(_lfjson_host=self.lanforge_ip, _lfjson_port=self.lanforge_port)
         res_data = cli_base.json_get(_req_url='port?fields=alias,port+type,ip,mac',)['interfaces']
-        print(res_data)
+        table_data = {"station name": [], "configured vlan-id": [], "expected IP Range": [], "allocated IP": [],"mac address":[]}
+        temp = {'sta100':'', 'sta200': '', 'sta00': ''}
+        for i in res_data:
+            for item in i:
+                if i[item]['port type'] == 'Ethernet':
+                    if i[item]['alias'] == self.upstream_port:
+                        temp.update({'sta00': i[item]['ip']})
+                if i[item]['port type'] == '802.1Q VLAN':
+                    if i[item]['alias'] == str(self.upstream_port+".100"):
+                        temp.update({'sta100': i[item]['ip']})
+                    elif i[item]['alias'] == str(self.upstream_port+".200"):
+                        temp.update({'sta200': i[item]['ip']})
+                if i[item]['port type'] == 'WIFI-STA':
+                    table_data["station name"].append(i[item]['alias'])
+                    if i[item]['alias'] == "sta100":
+                        exp1 = temp['sta100'].split('.')
+                        table_data["configured vlan-id"].append('100')
+                        table_data['expected IP Range'].append(f'{exp1[0]}.{exp1[1]}.X.X')
+                        table_data['allocated IP'].append(i[item]['ip'])
+                        table_data['mac address'].append(i[item]['mac'])
+                    elif i[item]['alias'] == 'sta200':
+                        exp2 = temp['sta200'].split('.')
+                        table_data["configured vlan-id"].append('200')
+                        table_data['expected IP Range'].append(f'{exp2[0]}.{exp2[1]}.X.X')
+                        table_data['allocated IP'].append(i[item]['ip'])
+                        table_data['mac address'].append(i[item]['mac'])
+                    elif i[item]['alias'] == 'sta00':
+                        exp3 = temp['sta00'].split('.')
+                        table_data["configured vlan-id"].append('-')
+                        table_data['expected IP Range'].append(f'{exp3[0]}.{exp3[1]}.X.X')
+                        table_data['allocated IP'].append(i[item]['ip'])
+                        table_data['mac address'].append(i[item]['mac'])
+        print(table_data)
+        # attach test data in a table to allure
+        report_obj = Report()
+        report_obj.table2(table=[table_data["station name"],table_data["configured vlan-id"],table_data["expected IP Range"],table_data["allocated IP"],table_data["mac address"]], headers=table_data.keys())
         # attach station data to allure
         self.attach_stationdata_to_allure(name="", station_name=station_name)
         if result1 == "Pass":
