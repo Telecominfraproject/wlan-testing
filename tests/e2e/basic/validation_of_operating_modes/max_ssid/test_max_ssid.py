7a4e45d6a4d4c0008df26482fd9b7cff856720d1
karthikaeyetea <karthika.subramani@candelatech.com>
2022-05-18 20:43:49 +0530

    Created Testcases for max_ssid
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/basic/validation_of_operating_modes/max_ssid/test_max_ssid.py
+++ tests/e2e/basic/validation_of_operating_modes/max_ssid/test_max_ssid.py
@@ -22,13 +22,13 @@ setup_params_general = {
             {"ssid_name": "ssid1_wpa_2g", "appliedRadios": ["2G"], "security_key": "something"},
             {"ssid_name": "ssid2_wpa_2g", "appliedRadios": ["2G"], "security_key": "something"}],
 
-        # "wpa2_personal": [
-        #     {"ssid_name": "ssid1_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"},
-        #     {"ssid_name": "ssid2_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"}],
-        #
-        # "wpa3_personal": [
-        #     {"ssid_name": "ssid1_wpa3_2g", "appliedRadios": ["2G"], "security_key": "something"},
-        #     {"ssid_name": "ssid2_wpa3_2g", "appliedRadios": ["2G"], "security_key": "something"}],
+        "wpa2_personal": [
+            {"ssid_name": "ssid1_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"},
+            {"ssid_name": "ssid2_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"}],
+
+        "wpa3_personal": [
+            {"ssid_name": "ssid1_wpa3_2g", "appliedRadios": ["2G"], "security_key": "something"},
+            {"ssid_name": "ssid2_wpa3_2g", "appliedRadios": ["2G"], "security_key": "something"}],
     },
     "rf": {},
     "radius": False
@@ -49,8 +49,8 @@ class TestMaxEightSsid2G(object):
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-7678", name="WIFI-7678")
     @pytest.mark.open
     @pytest.mark.wpa
-    # @pytest.mark.wpa2_personal
-    # @pytest.mark.wpa3_personal
+    @pytest.mark.wpa2_personal
+    @pytest.mark.wpa3_personal
     @pytest.mark.eight_ssid_2g
     @pytest.mark.twog
     def test_max_eight_ssid_2g(self, lf_test, station_names_twog, get_configuration):
@@ -98,13 +98,13 @@ setup_params_general1 = {
             {"ssid_name": "ssid1_wpa_5g", "appliedRadios": ["5G"], "security_key": "something"},
             {"ssid_name": "ssid2_wpa_5g", "appliedRadios": ["5G"], "security_key": "something"}],
 
-        # "wpa2_personal": [
-        #     {"ssid_name": "ssid1_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"},
-        #     {"ssid_name": "ssid2_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}],
-        #
-        # "wpa3_personal": [
-        #     {"ssid_name": "ssid1_wpa3_5g", "appliedRadios": ["5G"], "security_key": "something"},
-        #     {"ssid_name": "ssid2_wpa3_5g", "appliedRadios": ["5G"], "security_key": "something"}],
+        "wpa2_personal": [
+            {"ssid_name": "ssid1_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"},
+            {"ssid_name": "ssid2_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}],
+
+        "wpa3_personal": [
+            {"ssid_name": "ssid1_wpa3_5g", "appliedRadios": ["5G"], "security_key": "something"},
+            {"ssid_name": "ssid2_wpa3_5g", "appliedRadios": ["5G"], "security_key": "something"}],
     },
     "rf": {},
     "radius": False
@@ -125,8 +125,8 @@ class TestMaxEightSsid5G(object):
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-7678", name="WIFI-7678")
     @pytest.mark.open
     @pytest.mark.wpa
-    # @pytest.mark.wpa2_personal
-    # @pytest.mark.wpa3_personal
+    @pytest.mark.wpa2_personal
+    @pytest.mark.wpa3_personal
     @pytest.mark.eight_ssid_5g
     @pytest.mark.fiveg
     def test_max_eight_ssid_5g(self, lf_test, station_names_fiveg, get_configuration):
@@ -142,9 +142,9 @@ class TestMaxEightSsid5G(object):
         security_key = "something"
         security_list = ["open", "wpa", "wpa2", "wpa3"]
         sta_list = []
-        for i in setup_params_general["ssid_modes"]:
-            for j in range(len(setup_params_general["ssid_modes"][i])):
-                profile_data = setup_params_general["ssid_modes"][i][j]
+        for i in setup_params_general1["ssid_modes"]:
+            for j in range(len(setup_params_general1["ssid_modes"][i])):
+                profile_data = setup_params_general1["ssid_modes"][i][j]
                 ssid_name = profile_data["ssid_name"]
                 security = security_list[count_security]
                 station_names = station_names_fiveg[0] + str(int(station_names_fiveg[0][-1]) + sta_count)
@@ -157,7 +157,7 @@ class TestMaxEightSsid5G(object):
             count_security += 1
         fail_list = list(filter(lambda x : x == False, pass_list))
         if len(fail_list) == 0:
-            lf_test.layer3_traffic(ssid_num=len(pass_list), band="2.4 Ghz", station_name=sta_list)
+            lf_test.layer3_traffic(ssid_num=len(pass_list), band="5 Ghz", station_name=sta_list)
             assert True
         else:
             assert False
@@ -206,7 +206,7 @@ class TestMoreThanEightSsid2G(object):
     @pytest.mark.wpa3_personal
     @pytest.mark.more_than_eight_ssid_2g
     @pytest.mark.twog
-    def test_max_eight_ssid_2g(self, lf_test, station_names_twog, get_configuration):
+    def test_more_than_eight_ssid_2g(self, lf_test, station_names_twog, get_configuration):
         """Max-SSID Bridge Mode
            pytest -m "max_ssid and twog"
         """
@@ -219,9 +219,9 @@ class TestMoreThanEightSsid2G(object):
         security_key = "something"
         security_list = ["open", "wpa", "wpa2", "wpa3"]
         sta_list = []
-        for i in setup_params_general["ssid_modes"]:
-            for j in range(len(setup_params_general["ssid_modes"][i])):
-                profile_data = setup_params_general["ssid_modes"][i][j]
+        for i in setup_params_general2["ssid_modes"]:
+            for j in range(len(setup_params_general2["ssid_modes"][i])):
+                profile_data = setup_params_general2["ssid_modes"][i][j]
                 ssid_name = profile_data["ssid_name"]
                 security = security_list[count_security]
                 station_names = station_names_twog[0] + str(int(station_names_twog[0][-1]) + sta_count)
@@ -283,7 +283,7 @@ class TestMoreThanEightSsid5G(object):
     @pytest.mark.wpa3_personal
     @pytest.mark.more_than_eight_ssid_5g
     @pytest.mark.fiveg
-    def test_max_eight_ssid_5g(self, lf_test, station_names_fiveg, get_configuration):
+    def test_more_than_eight_ssid_5g(self, lf_test, station_names_fiveg, get_configuration):
         """Max-SSID Bridge Mode
            pytest -m "max_ssid and fiveg"
         """
@@ -311,7 +311,7 @@ class TestMoreThanEightSsid5G(object):
             count_security += 1
         fail_list = list(filter(lambda x : x == False, pass_list))
         if len(fail_list) == 0:
-            lf_test.layer3_traffic(ssid_num=len(pass_list), band="2.4 Ghz", station_name=sta_list)
+            lf_test.layer3_traffic(ssid_num=len(pass_list), band="5 Ghz", station_name=sta_list)
             assert True
         else:
             assert False
@@ -368,11 +368,10 @@ class TestMaxSixteenSsid(object):
     @pytest.mark.fiveg
     def test_max_sixteen_2g_5g(self, lf_test, station_names_twog, station_names_fiveg, get_configuration):
         """Max-SSID Bridge Mode
-           pytest -m "max_ssid and twog"
+           pytest -m "max_ssid and twog and fiveg"
         """
         scan_ssid = True
         mode = "BRIDGE"
-        # bands = ["twog", "fiveg"]
         band_name = (lambda bnd: '2G' if bnd == 'twog' else '5G')
         vlan = 1
         count_security, sta_count = 0, 0
@@ -380,7 +379,6 @@ class TestMaxSixteenSsid(object):
         security_key = "something"
         security_list = ["open", "wpa", "wpa2", "wpa3"]
         sta_list = []
-        # for band in bands:
         for i in setup_params_general4["ssid_modes"]:
             for j in range(len(setup_params_general4["ssid_modes"][i])):
                 profile_data = setup_params_general4["ssid_modes"][i][j]
@@ -431,8 +429,10 @@ setup_params_general5 = {
         "wpa3_personal": [
             {"ssid_name": "ssid1_wpa3_2g", "appliedRadios": ["2G"], "security_key": "something"},
             {"ssid_name": "ssid2_wpa3_2g", "appliedRadios": ["2G"], "security_key": "something"},
+            {"ssid_name": "ssid3_wpa3_2g", "appliedRadios": ["2G"], "security_key": "something"},
             {"ssid_name": "ssid1_wpa3_5g", "appliedRadios": ["5G"], "security_key": "something"},
-            {"ssid_name": "ssid2_wpa3_5g", "appliedRadios": ["5G"], "security_key": "something"}],
+            {"ssid_name": "ssid2_wpa3_5g", "appliedRadios": ["5G"], "security_key": "something"},
+            {"ssid_name": "ssid3_wpa3_5g", "appliedRadios": ["5G"], "security_key": "something"}],
         },
     "rf": {},
     "radius": False
@@ -460,7 +460,7 @@ class TestMoreThanSixteenSsid(object):
     @pytest.mark.fiveg
     def test_more_than_sixteen_2g_5g(self, lf_test, station_names_twog, station_names_fiveg, get_configuration):
         """Max-SSID Bridge Mode
-           pytest -m "max_ssid and twog"
+           pytest -m "max_ssid and twog and fiveg"
         """
         scan_ssid = True
         mode = "BRIDGE"
@@ -471,9 +471,9 @@ class TestMoreThanSixteenSsid(object):
         security_key = "something"
         security_list = ["open", "wpa", "wpa2", "wpa3"]
         sta_list = []
-        for i in setup_params_general4["ssid_modes"]:
-            for j in range(len(setup_params_general4["ssid_modes"][i])):
-                profile_data = setup_params_general4["ssid_modes"][i][j]
+        for i in setup_params_general5["ssid_modes"]:
+            for j in range(len(setup_params_general5["ssid_modes"][i])):
+                profile_data = setup_params_general5["ssid_modes"][i][j]
                 ssid_name = profile_data["ssid_name"]
                 security = security_list[count_security]
                 if profile_data["appliedRadios"] == ['2G']:
