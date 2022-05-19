190bacccb18973610841fea29f028120f90c3d79
karthikaeyetea <karthika.subramani@candelatech.com>
2022-05-19 10:34:06 +0530

    changed csv file name from Mbps to bps
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- libs/lanforge/lf_tools.py
+++ libs/lanforge/lf_tools.py
@@ -574,6 +574,7 @@ class ChamberView:
         try:
             df = pd.read_csv("../reports/" + str(dir_name) + file_name,
                             sep=r'\t', engine='python')
+
             print("csv file opened")
         except FileNotFoundError:
             print("csv file does not exist")
