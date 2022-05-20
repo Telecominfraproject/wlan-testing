52203faae96bec86471dab1d333b979ecd989bbf
Shivam Thakur <70829776+shivamcandela@users.noreply.github.com>
2022-05-20 10:43:33 +0530

    WIFI-7868 :  Added test_resources in ow_sanity_lf (#532)
    * WIFI-7868 :  Added test_resources in ow_sanity_lf
    
    Signed-off-by: shivam <shivam.thakur@candelatech.com>
    
    * WIFI-7868 Updated the markers for features
    
    Signed-off-by: shivam <shivam.thakur@candelatech.com>

--- tests/e2e/basic/validation_of_operating_modes/vlan_mode/dynamic_vlan/wpa2_enterprise/fiveg/test_dynamic_vlan_fiveg_wpa2.py
+++ tests/e2e/basic/validation_of_operating_modes/vlan_mode/dynamic_vlan/wpa2_enterprise/fiveg/test_dynamic_vlan_fiveg_wpa2.py
@@ -13,7 +13,7 @@ from configuration import DYNAMIC_VLAN_RADIUS_SERVER_DATA
 from configuration import DYNAMIC_VLAN_RADIUS_ACCOUNTING_DATA
 
 pytestmark = [pytest.mark.regression,
-              pytest.mark.dynamic_vlan,
+              pytest.mark.ow_dvlan_tests_lf,
               pytest.mark.wpa2_enterprise,
               pytest.mark.fiveg]
 
