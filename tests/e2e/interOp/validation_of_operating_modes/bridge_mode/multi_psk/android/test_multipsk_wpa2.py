8f0f55a4844567e91fa02d9c77fc520ba6d97c17
sushant <sushant.bawiskar@candelatech.com>
2022-05-20 11:57:09 +0530

    - removing multipsk from interop sanity because of dependency on lanforge
    Signed-off-by: Sushant Bawiskar <sushant.bawiskar@candelatech.com>

--- tests/e2e/interOp/validation_of_operating_modes/bridge_mode/multi_psk/android/test_multipsk_wpa2.py
+++ tests/e2e/interOp/validation_of_operating_modes/bridge_mode/multi_psk/android/test_multipsk_wpa2.py
@@ -194,7 +194,6 @@ class TestMultipskBridge(object):
     @pytest.mark.wpa2_personal
     @pytest.mark.twog
     @pytest.mark.two_vlan
-    @pytest.mark.ow_sanity_interop
     def test_multi_psk_wpa2_2g_2vlans(self, request, get_vif_state, get_ap_logs, lf_tools, get_configuration,
                             get_ToggleAirplaneMode_data, setup_perfectoMobile_android):
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
@@ -264,7 +263,6 @@ class TestMultipskBridge(object):
     @pytest.mark.wpa2_personal
     @pytest.mark.fiveg
     @pytest.mark.two_vlan
-    @pytest.mark.ow_sanity_interop
     def test_multi_psk_wpa2_5g_2vlans(self, request, get_vif_state, get_ap_logs, lf_tools,
                             get_ToggleAirplaneMode_data, setup_perfectoMobile_android):
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][1]
