16a936c2699bfb9ec2309a6ed5b9bad5140c0b68
karthikaeyetea <karthika.subramani@candelatech.com>
2022-05-21 11:30:35 +0530

    Enabled markers
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/advanced/multi_station_performance/wpa2_personal/test_vlan_multi_sta_performance.py
+++ tests/e2e/advanced/multi_station_performance/wpa2_personal/test_vlan_multi_sta_performance.py
@@ -4,7 +4,7 @@ import os
 import time
 import pandas as pd
 
-#pytestmark = [pytest.mark.advance, pytest.mark.multistaperf, pytest.mark.vlan]
+pytestmark = [pytest.mark.advance, pytest.mark.multistaperf, pytest.mark.vlan]
 
 setup_params_general = {
     "mode": "VLAN",
