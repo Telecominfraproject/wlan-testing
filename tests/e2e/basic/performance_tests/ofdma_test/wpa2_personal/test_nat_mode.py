ac1b50f410110097428b7eefa90fafdf1686ad51
anil-tegala <anil.tegala@candelatech.com>
2022-05-23 13:38:53 +0530

    ofdma wpa2_personal nat and vlan test run changes
    Signed-off-by: anil-tegala <anil.tegala@candelatech.com>

--- tests/e2e/basic/performance_tests/ofdma_test/wpa2_personal/test_nat_mode.py
+++ tests/e2e/basic/performance_tests/ofdma_test/wpa2_personal/test_nat_mode.py
@@ -11,7 +11,7 @@ import allure
 pytestmark = [pytest.mark.wpa2_personal, pytest.mark.ofdma, pytest.mark.nat]
 
 setup_params_general = {
-    "mode": "BRIDGE",
+    "mode": "NAT",
     "ssid_modes": {
         "wpa2_personal": [
             {"ssid_name": "ofdma-5g", "appliedRadios": ["5G"], "security_key": "something"},
@@ -34,7 +34,7 @@ setup_params_general = {
 @pytest.mark.usefixtures("setup_profiles")
 @pytest.mark.wpa2_personal
 @pytest.mark.twog
-class TestOfdmabridgeMode(object):
+class TestOfdmaNatMode(object):
     """
         OFDMA Test nat mode
         pytest -m "ofdma_test and nat"
@@ -42,52 +42,94 @@ class TestOfdmabridgeMode(object):
 
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-7112", name="WIFI-7112")
     @pytest.mark.twog
-    def test_ofdma_he_capability_wpa2_bridge_twog(self, lf_tools, setup_profiles,
-                                                  lf_test, station_names_twog, create_lanforge_chamberview_dut,
-                                                  get_configuration):
+    def test_ofdma_he_capability_wpa2_nat_twog(self, lf_tools, lf_test, station_names_ax):
         """ Wifi Capacity Test nat mode
             pytest -m "ofdma_test and nat and wpa2_personal and twog"
         """
-        ssid_2g = setup_params_general["ssid_modes"]["wpa2_personal"]['ssid_name'][1]
-        mode = "nat"
-        vlan = 1
-        lf_tools.add_stations(band="2G", num_stations="4", dut=lf_tools.dut_name, ssid_name=ssid_2g)
-        lf_tools.Chamber_View()
-        influx_tags = ["ofdma", "download", "2G"]
-        wct_obj = lf_test.wifi_capacity(instance_name="ofdma_wpa2_bridge", mode=mode, vlan_id=vlan,
-                                        download_rate="300Mbps", batch_size='4',
-                                        upload_rate="0", protocol="TCP-IPv4", duration="60000")
-
-        report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-
-        report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-
-        lf_tools.attach_report_graphs(report_name=report_name)
-        print("Test Completed... Cleaning up Stations")
-        assert True
+        ssid_2g = setup_params_general["ssid_modes"]["wpa2_personal"][0]['ssid_name']
+        passkey = setup_params_general["ssid_modes"]["wpa2_personal"][0]['security_key']
+        mode = "NAT"
+        vlan = 100
+        if len(lf_test.ax_radios) >= 1:
+            radios_ax = lf_test.ax_radios
+            for i in range(len(radios_ax)):
+                if '1.1.' in radios_ax[i]:
+                    radios_ax[i] = str(radios_ax[i]).replace('1.1.', '')
+            sta_mode, sta = 13, 'sta000'
+            sta_list = []
+            sta_names = ''
+            for j in range(len(radios_ax) - 1):
+                sta_list.append(sta + str(j))
+                lf_test.Client_Connect_Using_Radio(ssid=ssid_2g, passkey=passkey, security="wpa2", mode="NAT",
+                                                   vlan_id=100, radio=radios_ax[j], sta_mode=sta_mode,
+                                                   station_name=[sta + str(j)])
+            print(sta_list)
+            sniffer_channel = int(lf_tools.station_data_query(station_name=sta_list[0]))
+            sniffer_radio = radios_ax[-1]
+            influx_tags = ["ofdma", "download", "2.4G"]
+            print("ax station names: ", station_names_ax)
+            res_data = lf_tools.json_get(_req_url='port?fields=port+type')['interfaces']
+            temp_sta = []
+            for i in res_data:
+                for item in i:
+                    if i[item]['port type'] == 'WIFI-STA':
+                        sta_names += item + ","
+            if sta_names.endswith(','):
+                sta_names.removesuffix(',')
+            ofdma_obj = lf_test.ofdma(mode=mode, vlan_id=vlan, inst_name="ofdma", batch_size='1', rawlines=None,
+                                      sniffer_channel=sniffer_channel, sniffer_radio=sniffer_radio,
+                                      wct_stations=sta_names)
+            report_name = ofdma_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+            lf_tools.attach_report_graphs(report_name=report_name)
+            print("Test Completed... Cleaning up Stations")
+            assert True
+        else:
+            print("This Feature needs AX radios to test")
+            assert False
 
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-7112", name="WIFI-7112")
     @pytest.mark.fiveg
-    def test_ofdma_he_capability_wpa2_bridge_fiveg(self, lf_tools,
-                                                   lf_test, station_names_twog, create_lanforge_chamberview_dut,
-                                                   get_configuration):
+    def test_ofdma_he_capability_wpa2_nat_fiveg(self, lf_tools, lf_test, station_names_ax):
         """ Wifi Capacity Test nat mode
             pytest -m "ofdma_test and nat and wpa2_personal and twog"
         """
-        ssid_5g = setup_params_general["ssid_modes"]["wpa2_personal"]['ssid_name'][1]
-        mode = "nat"
-        vlan = 1
-        lf_tools.add_stations(band="5G", num_stations="4", dut=lf_tools.dut_name, ssid_name=ssid_5g)
-        lf_tools.Chamber_View()
-        influx_tags = ["ofdma", "download", "5G"]
-        wct_obj = lf_test.wifi_capacity(instance_name="ofdma_wpa2_bridge", mode=mode, vlan_id=vlan,
-                                        download_rate="300Mbps", batch_size='4',
-                                        upload_rate="0", protocol="TCP-IPv4", duration="60000")
-
-        report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-
-        report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-
-        lf_tools.attach_report_graphs(report_name=report_name)
-        print("Test Completed... Cleaning up Stations")
-        assert True
+        ssid_5g = setup_params_general["ssid_modes"]["wpa2_personal"][1]['ssid_name']
+        passkey = setup_params_general["ssid_modes"]["wpa2_personal"][1]['security_key']
+        mode = "NAT"
+        vlan = 100
+        if len(lf_test.ax_radios) >= 1:
+            radios_ax = lf_test.ax_radios
+            for i in range(len(radios_ax)):
+                if '1.1.' in radios_ax[i]:
+                    radios_ax[i] = str(radios_ax[i]).replace('1.1.', '')
+            sta_mode, sta = 13, 'sta000'
+            sta_list = []
+            sta_names = ''
+            for j in range(len(radios_ax) - 1):
+                sta_list.append(sta + str(j))
+                lf_test.Client_Connect_Using_Radio(ssid=ssid_5g, passkey=passkey, security="wpa2", mode="NAT",
+                                                   vlan_id=100, radio=radios_ax[j], sta_mode=sta_mode,
+                                                   station_name=[sta + str(j)])
+            print(sta_list)
+            sniffer_channel = int(lf_tools.station_data_query(station_name=sta_list[0]))
+            sniffer_radio = radios_ax[-1]
+            influx_tags = ["ofdma", "download", "5G"]
+            print("ax station names: ", station_names_ax)
+            res_data = lf_tools.json_get(_req_url='port?fields=port+type')['interfaces']
+            temp_sta = []
+            for i in res_data:
+                for item in i:
+                    if i[item]['port type'] == 'WIFI-STA':
+                        sta_names += item + ","
+            if sta_names.endswith(','):
+                sta_names.removesuffix(',')
+            ofdma_obj = lf_test.ofdma(mode=mode, vlan_id=vlan, inst_name="ofdma", batch_size='1', rawlines=None,
+                                      sniffer_channel=sniffer_channel, sniffer_radio=sniffer_radio,
+                                      wct_stations=sta_names)
+            report_name = ofdma_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+            lf_tools.attach_report_graphs(report_name=report_name)
+            print("Test Completed... Cleaning up Stations")
+            assert True
+        else:
+            print("This Feature needs AX radios to test")
+            assert False
