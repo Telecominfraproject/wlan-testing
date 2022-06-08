a658a825738cbc3357cfe98c5f9f405181233789
jitendracandela <jitendra.kushavah@candelatech.com>
2022-06-08 13:55:52 +0530

    Changed position of ABC test case
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/e2e/mesh/Mesh/test_node_patterns.py
+++ tests/e2e/mesh/Mesh/test_node_patterns.py
@@ -30,26 +30,6 @@ class TestNodePatters(object):
 
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-3913", name="WIFI-3913")
     @pytest.mark.wpa2_personal
-    @pytest.mark.ABC
-    def test_throughput_latency_2g_5g_ap_chamber_pos_ABC(self, setup_mesh_profile_fix, lf_test, lf_tools):
-        #lf_tools.reset_scenario()
-        raw_lines = [['selected_dut2: tip-root ssid_wpa2_2g_1 34:ef:b6:af:4a:7d (2)'], ['selected_dut5: tip-root ssid_wpa2_5g_1 34:ef:b6:af:4a:7e (1)'],
-                    ['sta_amount-4: 1'], ['radios-0-0: 1.2.6 wiphy0'], ['radios-0-3: 1.2.7 wiphy1'], ['radios-1-0: 1.3.6 wiphy0'],
-                    ['radios-1-3: 1.3.7 wiphy1'], ['radios-2-0: 1.4.6 wiphy0'], ['radios-2-3: 1.4.7 wiphy1'],
-                    ['radios-4-0: 1.1.6 wiphy2'], ['radios-4-3: 1.1.7 wiphy3'], ['ap_arrangements: ABC'],
-                    ['sta_position: Current Position'], ['chamber-0: Root'], ['chamber-1: Node1'],
-                    ['chamber-2: Node2'], ['chamber-4: Mobile-Sta'],
-                    ['path: Orbit Current'], ['traffic_types: UDP'], ['direction: Both'], ['tests: Throughput'], ['traf_combo: STA']]
-
-        mesh_o = lf_test.mesh_test(instance_name="node_patterns_ABC", raw_lines=raw_lines)
-        report_name = mesh_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-        print("report name ", report_name)
-        lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Node patterns")
-
-        assert True
-
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-3913", name="WIFI-3913")
-    @pytest.mark.wpa2_personal
     @pytest.mark.A_BC
     def test_throughput_latency_2g_5g_ap_chamber_pos_A_BC(self, setup_mesh_profile_fix, lf_test, lf_tools):
         # lf_tools.reset_scenario()
@@ -69,6 +49,29 @@ class TestNodePatters(object):
         print("report name ", report_name)
         entries = os.listdir("../reports/" + report_name + '/')
         print("entries", entries)
+        lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Node patterns")
+
+        assert True
+
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-3913", name="WIFI-3913")
+    @pytest.mark.wpa2_personal
+    @pytest.mark.ABC
+    def test_throughput_latency_2g_5g_ap_chamber_pos_ABC(self, setup_mesh_profile_fix, lf_test, lf_tools):
+        # lf_tools.reset_scenario()
+        raw_lines = [['selected_dut2: tip-root ssid_wpa2_2g_1 34:ef:b6:af:4a:7d (2)'],
+                     ['selected_dut5: tip-root ssid_wpa2_5g_1 34:ef:b6:af:4a:7e (1)'],
+                     ['sta_amount-4: 1'], ['radios-0-0: 1.2.6 wiphy0'], ['radios-0-3: 1.2.7 wiphy1'],
+                     ['radios-1-0: 1.3.6 wiphy0'],
+                     ['radios-1-3: 1.3.7 wiphy1'], ['radios-2-0: 1.4.6 wiphy0'], ['radios-2-3: 1.4.7 wiphy1'],
+                     ['radios-4-0: 1.1.6 wiphy2'], ['radios-4-3: 1.1.7 wiphy3'], ['ap_arrangements: ABC'],
+                     ['sta_position: Current Position'], ['chamber-0: Root'], ['chamber-1: Node1'],
+                     ['chamber-2: Node2'], ['chamber-4: Mobile-Sta'],
+                     ['path: Orbit Current'], ['traffic_types: UDP'], ['direction: Both'], ['tests: Throughput'],
+                     ['traf_combo: STA']]
+
+        mesh_o = lf_test.mesh_test(instance_name="node_patterns_ABC", raw_lines=raw_lines)
+        report_name = mesh_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+        print("report name ", report_name)
         lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Node patterns")
 
         assert True
