c2ae6693a176d7e8abc2f58b62c0f7d4e3857842
jitendracandela <78074038+jitendracandela@users.noreply.github.com>
2022-06-21 13:52:05 +0530

    Wifi 9536 (#625)
    * Added radio_max_antenna method
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>
    
    * Added wifi capacity test
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>
    
    * Added wallys_dr40x9 AP
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>
    
    * Removed radio_max_antenna
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>
    
    * Fixed issues and added single client wct for 80Mhz
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>
    
    * Changed wifi capacity method name
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/e2e/basic/performance_tests/throughput_benchmark/throughput_across_channel_bw/test_bridge_mode.py
+++ tests/e2e/basic/performance_tests/throughput_benchmark/throughput_across_channel_bw/test_bridge_mode.py
@@ -23,9 +23,13 @@ setup_params_general_20Mhz = {
             {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"],
              "security_key": "something"}]},
     "rf": {
-        "is5GHz": {"channelBandwidth": "is20MHz"},
-        "is5GHzL": {"channelBandwidth": "is20MHz"},
-        "is5GHzU": {"channelBandwidth": "is20MHz"}},
+        "5G": {
+                "channel-width": 20,
+                },
+        "2G": {
+                "channel-width": 20,
+                }
+        },
     "radius": False
 }
 
@@ -92,7 +96,7 @@ class TestThroughputAcrossBw20MhzBRIDGE(object):
         """Throughput Across Bw Bridge Mode
            pytest -m "throughput_across_bw_test and Bridge and wpa2_personal and fiveg"
         """
-        profile_data = setup_params_general_40Mhz["ssid_modes"]["wpa2_personal"][1]
+        profile_data = setup_params_general_20Mhz["ssid_modes"]["wpa2_personal"][1]
         ssid_name = profile_data["ssid_name"]
         security_key = profile_data["security_key"]
         security = "wpa2"
@@ -123,7 +127,6 @@ class TestThroughputAcrossBw20MhzBRIDGE(object):
         else:
             assert False
 
-
 setup_params_general_40Mhz = {
     "mode": "BRIDGE",
     "ssid_modes": {
@@ -132,9 +135,13 @@ setup_params_general_40Mhz = {
             {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"],
              "security_key": "something"}]},
     "rf": {
-        "is5GHz": {"channelBandwidth": "is40MHz"},
-        "is5GHzL": {"channelBandwidth": "is40MHz"},
-        "is5GHzU": {"channelBandwidth": "is40MHz"}},
+            "5G": {
+                "channel-width": 40,
+                },
+            "2G": {
+                "channel-width": 40,
+                }
+    },
     "radius": False
 }
 
@@ -161,7 +168,7 @@ class TestThroughputAcrossBw40MhzBRIDGE(object):
         """Throughput Across Bw Bridge Mode
            pytest -m "throughput_across_bw_test and Bridge and wpa2_personal and twog"
         """
-        profile_data = setup_params_general_80Mhz["ssid_modes"]["wpa2_personal"][0]
+        profile_data = setup_params_general_40Mhz["ssid_modes"]["wpa2_personal"][0]
         ssid_name = profile_data["ssid_name"]
         security_key = profile_data["security_key"]
         security = "open"
@@ -201,7 +208,7 @@ class TestThroughputAcrossBw40MhzBRIDGE(object):
         """Throughput Across Bw Bridge Mode
            pytest -m "throughput_across_bw_test and Bridge and wpa2_personal and fiveg"
         """
-        profile_data = setup_params_general_80Mhz["ssid_modes"]["wpa2_personal"][1]
+        profile_data = setup_params_general_40Mhz["ssid_modes"]["wpa2_personal"][1]
         ssid_name = profile_data["ssid_name"]
         security_key = profile_data["security_key"]
         security = "wpa2"
@@ -236,13 +243,12 @@ setup_params_general_80Mhz = {
     "mode": "BRIDGE",
     "ssid_modes": {
         "wpa2_personal": [
-            {"ssid_name": "ssid_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"},
-            {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"],
-             "security_key": "something"}]},
+            {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}]},
     "rf": {
-        "is5GHz": {"channelBandwidth": "is80MHz"},
-        "is5GHzL": {"channelBandwidth": "is80MHz"},
-        "is5GHzU": {"channelBandwidth": "is80MHz"}},
+            "5G": {
+                "channel-width": 80,
+                }
+    },
     "radius": False
 }
 
@@ -259,47 +265,6 @@ class TestThroughputAcrossBw80MhzBRIDGE(object):
     """Throughput Across Bw Bridge Mode
        pytest -m "throughput_across_bw_test and Bridge"
     """
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2558", name="WIFI-2558")
-    @pytest.mark.bw80Mhz
-    @pytest.mark.wpa2_personal
-    @pytest.mark.twog
-    def test_client_wpa2_personal_2g(self, 
-                                     lf_test, station_names_twog, create_lanforge_chamberview_dut,
-                                     get_configuration):
-        """Throughput Across Bw Bridge Mode
-           pytest -m "throughput_across_bw_test and Bridge and wpa2_personal and twog"
-        """
-        profile_data = setup_params_general_80Mhz["ssid_modes"]["wpa2_personal"][0]
-        ssid_name = profile_data["ssid_name"]
-        security_key = profile_data["security_key"]
-        security = "open"
-        mode = "BRIDGE"
-        band = "twog"
-        vlan = 1
-        dut_name = create_lanforge_chamberview_dut
-        station = lf_test.Client_Connect(ssid=ssid_name, security=security,
-                                         passkey=security_key, mode=mode, band=band,
-                                         station_name=station_names_twog, vlan_id=vlan)
-
-        if station:
-            dp_obj = lf_test.dataplane(station_name=station_names_twog, mode=mode,
-                                       instance_name="TIP_PERF_DPT_WPA2_2G",
-                                       vlan_id=vlan, dut_name=dut_name, bw="80")
-            report_name = dp_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-            entries = os.listdir("../reports/" + report_name + '/')
-            pdf = False
-            for i in entries:
-                if ".pdf" in i:
-                    pdf = i
-            if pdf:
-                allure.attach.file(source="../reports/" + report_name + "/" + pdf,
-                                   name=get_configuration["access_point"][0]["model"] + "_dataplane")
-            print("Test Completed... Cleaning up Stations")
-            lf_test.Client_disconnect(station_name=station_names_twog)
-            assert station
-        else:
-            assert False
-
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2558", name="WIFI-2558")
     @pytest.mark.bw80Mhz
     @pytest.mark.wpa2_personal
@@ -340,18 +305,57 @@ class TestThroughputAcrossBw80MhzBRIDGE(object):
         else:
             assert False
 
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-9742", name="WIFI-9742")
+    @pytest.mark.ow_sanity_lf
+    @allure.parent_suite("Throughput Test")
+    @allure.suite("OpenWifi Throughput across channel bandwidth")
+    @allure.title("Single client wifi capacity 80Mhz Bw")
+    @allure.sub_suite("BRIDGE Mode")
+    @pytest.mark.bw80Mhz
+    @pytest.mark.wpa2_personal
+    @pytest.mark.mmm
+    @pytest.mark.tcp_download
+    def test_client_wpa2_BRIDGE_tcp_dl_80Mhz(self, lf_tools, get_apnos_max_clients,
+                                       lf_test, station_names_twog, create_lanforge_chamberview_dut,
+                                       get_configuration):
+        """ Wifi Capacity Test BRIDGE mode
+            pytest -m "wifi_capacity_test and BRIDGE and wpa2_personal and twog"
+        """
+        lf_tools.reset_scenario()
+        profile_data = setup_params_general_80Mhz["ssid_modes"]["wpa2_personal"][0]
+        ssid_name = profile_data["ssid_name"]
+        mode = "BRIDGE"
+        vlan = 1
+        max = int(get_apnos_max_clients[0])
+        sets = [["DUT_NAME", lf_tools.dut_name]]
+        print("sets", sets)
+        # lf_tools.add_stations(band="2G", num_stations=1, dut=lf_tools.dut_name, ssid_name=ssid_name)
+        # lf_tools.add_stations(band="5G", num_stations=1, dut=lf_tools.dut_name, ssid_name=ssid_name)
+        lf_tools.add_stations(band="ax", num_stations=1, dut=lf_tools.dut_name, ssid_name=ssid_name)
+        lf_tools.Chamber_View()
+        influx_tags = "wifi-capacity-tcp-download-bridge-wpa2-80Mhz-AX"
+        wct_obj = lf_test.wifi_capacity(instance_name="test_client_wpa2_BRIDGE_tcp_dl", mode=mode, vlan_id=vlan,
+                                        download_rate="1.5Gbps", batch_size="1",
+                                        influx_tags=influx_tags, sets=sets,
+                                        upload_rate="0", protocol="TCP-IPv4", duration="60000", move_to_influx=True)
+
+        report_name = wct_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
+
+        lf_tools.attach_report_graphs(report_name=report_name)
+        lf_tools.attach_report_kpi(report_name=report_name)
+
+        assert True
 
 setup_params_general_160Mhz = {
     "mode": "BRIDGE",
     "ssid_modes": {
         "wpa2_personal": [
-            {"ssid_name": "ssid_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"},
-            {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"],
-             "security_key": "something"}]},
+            {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}]},
     "rf": {
-        "is5GHz": {"channelBandwidth": "is160MHz"},
-        "is5GHzL": {"channelBandwidth": "is160MHz"},
-        "is5GHzU": {"channelBandwidth": "is160MHz"}},
+            "5G": {
+                "channel-width": 160,
+                }
+        },
     "radius": False
 }
 
@@ -368,47 +372,6 @@ class TestThroughputAcrossBw160MhzBRIDGE(object):
     """Throughput Across Bw Bridge Mode
        pytest -m "throughput_across_bw_test and Bridge"
     """
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2559", name="WIFI-2559")
-    @pytest.mark.bw160Mhz
-    @pytest.mark.wpa2_personal
-    @pytest.mark.twog
-    def test_client_wpa2_personal_2g(self, 
-                                     lf_test, station_names_twog, create_lanforge_chamberview_dut,
-                                     get_configuration):
-        """Throughput Across Bw Bridge Mode
-           pytest -m "throughput_across_bw_test and Bridge and wpa2_personal and twog"
-        """
-        profile_data = setup_params_general_160Mhz["ssid_modes"]["wpa2_personal"][0]
-        ssid_name = profile_data["ssid_name"]
-        security_key = profile_data["security_key"]
-        security = "open"
-        mode = "BRIDGE"
-        band = "twog"
-        vlan = 1
-        dut_name = create_lanforge_chamberview_dut
-        station = lf_test.Client_Connect(ssid=ssid_name, security=security,
-                                         passkey=security_key, mode=mode, band=band,
-                                         station_name=station_names_twog, vlan_id=vlan)
-
-        if station:
-            dp_obj = lf_test.dataplane(station_name=station_names_twog, mode=mode,
-                                       instance_name="TIP_PERF_DPT_WPA2_2G",
-                                       vlan_id=vlan, dut_name=dut_name, bw="160")
-            report_name = dp_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-            entries = os.listdir("../reports/" + report_name + '/')
-            pdf = False
-            for i in entries:
-                if ".pdf" in i:
-                    pdf = i
-            if pdf:
-                allure.attach.file(source="../reports/" + report_name + "/" + pdf,
-                                   name=get_configuration["access_point"][0]["model"] + "_dataplane")
-            print("Test Completed... Cleaning up Stations")
-            lf_test.Client_disconnect(station_name=station_names_twog)
-            assert station
-        else:
-            assert False
-
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2559", name="WIFI-2559")
     @pytest.mark.bw160Mhz
     @pytest.mark.wpa2_personal
