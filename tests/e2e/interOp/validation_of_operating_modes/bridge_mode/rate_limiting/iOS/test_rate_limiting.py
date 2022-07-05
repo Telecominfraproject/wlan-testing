4b1f880768a7861558d6fe8c4d9f6be894dcfd57
Jyothsna-setti <jyothsna.polamarasetty@candelatech.com>
2022-07-05 10:12:45 +0530

    Speedtest
    Signed-off-by: Jyothsna-setti <jyothsna.polamarasetty@candelatech.com>

--- tests/e2e/interOp/validation_of_operating_modes/bridge_mode/rate_limiting/iOS/test_rate_limiting.py
+++ tests/e2e/interOp/validation_of_operating_modes/bridge_mode/rate_limiting/iOS/test_rate_limiting.py
@@ -107,6 +107,7 @@ class TestRateLimitingBridge(object):
 
     @pytest.mark.wpa2_personal
     @pytest.mark.twog
+    @pytest.mark.speedtestbyookla
     def test_rate_limiting_wpa2_2g(self, request, get_vif_state, get_ap_logs,
                                    get_ToggleAirplaneMode_data, setup_perfectoMobile_iOS):
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][0]
