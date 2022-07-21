46621649c01f7d4244963c09b842ddedd8b9cbec
shivam <shivam.thakur@candelatech.com>
2022-07-21 14:09:18 +0530

    base pip module setup script and ap_lib imports
    Signed-off-by: shivam <shivam.thakur@candelatech.com>

--- libs/tip_2x/ap_lib.py
+++ libs/tip_2x/ap_lib.py
@@ -1,11 +1,26 @@
 import importlib
-import sys
 
-sys.path.append('..')
-SetupLibrary = importlib.import_module("SetupLibrary")
+setup_lib = importlib.import_module("SetupLibrary")
+SetupLibrary = setup_lib.SetupLibrary
 
 
 class APLIBS:
 
     def __init__(self):
         pass
+
+    def run_generic_command(self):
+        pass
+
+    def get(self):
+        pass
+
+
+if __name__ == '__main__':
+    obj = SetupLibrary(remote_ip="192.168.52.89",
+                       remote_ssh_port=22,
+                       pwd="")
+
+    obj.setup_serial_environment()
+    obj.check_serial_connection(tty="/dev/ttyUSB0")
+    obj.kill_all_minicom_process(tty="/dev/ttyUSB0")
