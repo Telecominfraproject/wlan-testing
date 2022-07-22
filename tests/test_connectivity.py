422d040055d7965188c080c434aafb0be0ccd53b
shivam <shivam.thakur@candelatech.com>
2022-07-22 12:18:55 +0530

    base structure for perfecto pip module
    Signed-off-by: shivam <shivam.thakur@candelatech.com>

--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -382,3 +382,8 @@ class TestFirmwareUpgrade(object):
             allure.attach(name=str(data['firmware']) + str(current_version_ap), body="")
             status.append(current_version_ap == data['firmware'].split())
         assert False not in status
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> base structure for perfecto pip module
