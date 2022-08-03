f317b8d7d4507b6049981874e9be9ed4b20160a7
karthikaeyetea <karthika.subramani@candelatech.com>
2022-08-03 12:12:05 +0530

    Added pass/fail validation
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/advanced/rate_vs_orientation/wpa2_personal/test_bridge_orientation.py
+++ tests/e2e/advanced/rate_vs_orientation/wpa2_personal/test_bridge_orientation.py
@@ -15,7 +15,12 @@ setup_params_general = {
             {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}
         ]
     },
-    "rf": {},
+    "rf": {
+        "5G":{
+            "channel-width": 80},
+        "2G":{
+            "channel-width": 20}
+    },
     "radius": False
 }
 @allure.feature("BRIDGE MODE RATE VS ORIENTATION")
@@ -27,12 +32,10 @@ setup_params_general = {
 )
 @pytest.mark.usefixtures("setup_profiles")
 class TestRatevsOrientationBridge(object):
-
-
     @pytest.mark.wpa2_personal
     @pytest.mark.fiveg
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-5431", name="WIFI-5431")
-    def test_client_wpa2_personal_5g_11ac(self, lf_test, station_names_fiveg, create_lanforge_chamberview_dut,
+    def test_client_tcp_dl_wpa2_personal_5g_11ac(self, lf_test, station_names_fiveg, create_lanforge_chamberview_dut,
                                           get_configuration, lf_tools):
 
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][1]
@@ -52,20 +55,53 @@ class TestRatevsOrientationBridge(object):
         print(ser_no)
         val = [['modes: 802.11an-AC'], ['pkts: MTU'], ['directions: DUT Transmit'], ['traffic_types:TCP'],
                ['bandw_options: 80'], ['spatial_streams: 2'], ['attenuator: ' + str(ser_no[0])],
-               ['attenuator2: ' + str(ser_no[1])],
-               ['attenuations: 0..+50..800'], ['attenuations2: 0..+50..800'],['chamber: DUT-Chamber'], ['tt_deg: 0..+30..359']]
+               ['attenuator2: ' + str(ser_no[1])], ['attenuations: 0 100 10..+30..540'],
+               ['attenuations2: 0 100 10..+30..540'], ['chamber: DUT-Chamber'], ['tt_deg: 0..+30..150']] #210..+30..540
 
         if station:
             time.sleep(3)
-            rvr_o = lf_test.ratevsrange(station_name=station_names_fiveg, mode=mode,
+            rvr_o = lf_test.ratevsrange(station_name=station_names_fiveg, mode=mode,duration="5000",
                                         instance_name="ORIENTATION_RVR_BRIDGE_11_AC",
                                         vlan_id=vlan, dut_name=dut_name, raw_lines=val)
             report_name = rvr_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
             entries = os.listdir("../reports/" + report_name + '/')
             lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Rate vs Range Test")
-            print("Test Completed... Cleaning up Stations")
-            lf_test.Client_disconnect(station_name=station_names_fiveg)
-            assert station
+            lf_test.Client_disconnect(clear_all_sta=True, clean_l3_traffic=True)
+            kpi = "kpi.csv"
+            pass_value = {"strong":500, "medium":400, "weak":200}
+            atn, deg = [0,10,21,24,27,30,33,36,39,42,45,48,51,54], [0,30,60,90,120,150,180,210,240,270,300,330]
+            if kpi in entries:
+                kpi_val = lf_tools.read_kpi_file(column_name=["numeric-score"], dir_name=report_name) #name="CSV Data", body="Throughput value : " + str(kpi_val)
+                print(kpi_val)
+                allure.attach.file(source="../reports/" + report_name + "/" + kpi, name="kpi.csv")
+                if str(kpi_val) == "empty":
+                    print("Throughput value from kpi.csv is empty, Test failed")
+                    allure.attach(name="CSV Data", body="Throughput value from kpi.csv is empty, Test failed")
+                    assert False, "Throughput value from kpi.csv is empty, Test failed"
+                else:
+                    allure.attach(name="CSV Data", body="Throughput value : " + str(kpi_val))
+                    start, thrpt_val, pass_fail = 0, {}, []
+                    for i in pass_value:
+                        count = 0
+                        for j in range(start, len(kpi_val), len(atn)):
+                            thrpt_val[f"{atn[start]}-{deg[count]}"] = kpi_val[j][0]
+                            if kpi_val[j][0] >= pass_value[i]:
+                                pass_fail.append("PASS")
+                            else:
+                                pass_fail.append("FAIL")
+                            count += 6
+                        start += 2
+                    if "FAIL" in pass_fail:
+                        print("Test failed due to lesser value")
+                        assert False, "Test failed due to lesser value"
+                    else:
+                        print("Test passed successfully")
+                        assert True
+            else:
+                print("csv file does not exist, Test failed")
+                allure.attach(name="CSV Data", body="csv file does not exist")
+                assert False, "csv file does not exist"
         else:
-            assert False
+            print("test failed due to no station ip")
+            assert False, "test failed due to no station ip"
 
