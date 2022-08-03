b17f2bae4bc8ed4fad6a89066a50eaaa6b532d3b
karthikaeyetea <karthika.subramani@candelatech.com>
2022-08-03 12:11:41 +0530

    Added pass/fail validation
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/advanced/rate_vs_range/wpa2_personal/test_bridge_mode.py
+++ tests/e2e/advanced/rate_vs_range/wpa2_personal/test_bridge_mode.py
@@ -70,7 +70,7 @@ class TestRatevsRangeBridge(object):
                ['bandw_options: AUTO'], ['spatial_streams: AUTO'], ['attenuator: ' + str(ser_no[0])], ['attenuator2: ' + str(ser_no[1])],
                ['attenuations: 0 100 210..+30..630'], ['attenuations2: 0 100 210..+30..630']]
         if station:
-            rvr_o = lf_test.ratevsrange(station_name=station_names_twog, mode=mode,duration="120000",
+            rvr_o = lf_test.ratevsrange(station_name=station_names_twog, mode=mode,duration="5000",
                                        instance_name="MODEBRIDGE_RVR_11B_TWOG_modified",
                                        vlan_id=vlan, dut_name=dut_name, raw_lines=val)
             report_name = rvr_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
@@ -80,20 +80,46 @@ class TestRatevsRangeBridge(object):
             lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Rate vs Range Test")
             print("Test Completed... Cleaning up Stations")
             lf_test.Client_disconnect(clear_all_sta=True, clean_l3_traffic=True)
-            assert station
+            kpi = "kpi.csv"
+            pass_value = {"strong": 100, "medium": 95, "weak": 14}
+            atn = [0, 10, 21, 24, 27,30,33,36,39,42,45,48,51,54,57,60,63]
+            if kpi in entries:
+                kpi_val = lf_tools.read_kpi_file(column_name=["numeric-score"],
+                                                 dir_name=report_name)  # name="CSV Data", body="Throughput value : " + str(kpi_val)
+                print(kpi_val)
+                allure.attach.file(source="../reports/" + report_name + "/" + kpi, name="kpi.csv")
+                if str(kpi_val) == "empty":
+                    print("Throughput value from kpi.csv is empty, Test failed")
+                    allure.attach(name="CSV Data", body="Throughput value from kpi.csv is empty, Test failed")
+                    assert False, "Throughput value from kpi.csv is empty, Test failed"
+                else:
+                    allure.attach(name="CSV Data", body="Throughput value : " + str(kpi_val))
+                    start, thrpt_val, pass_fail = 0, {}, []
+                    for i in pass_value:
+                        # count = 0
+                        dirction = "DUT-TX"
+                        for j in range(start, len(kpi_val), len(atn)):
+                            thrpt_val[f"{atn[start]}--{dirction}"] = kpi_val[j][0]
+                            if kpi_val[j][0] >= pass_value[i]:
+                                pass_fail.append("PASS")
+                            else:
+                                pass_fail.append("FAIL")
+                            # count += 1
+                            dirction = "DUT-RX"
+                        start += 7
+                    if "FAIL" in pass_fail:
+                        print("Test failed due to lesser value")
+                        assert False, "Test failed due to lesser value"
+                    else:
+                        print("Test passed successfully")
+                        assert True
+            else:
+                print("csv file does not exist, Test failed")
+                allure.attach(name="CSV Data", body="csv file does not exist")
+                assert False, "csv file does not exist"
         else:
             print("Test failed due to no station ip")
             assert False, "Test failed due to no station ip"
-    # kpi_val = lf_tools.read_kpi_file(column_name=None, dir_name=report_name)
-    # print(str(kpi_val))
-    # if str(kpi_val) == "empty":
-    #     print("kpi is empty, station did not got ip, Test failed")
-    #     allure.attach(name="Kpi Data", body="station did not got ip Test failed.")
-    #     print("Test failed due to no station ip")
-            assert False, "Test failed due to no station ip"
-    # else:
-    #     print("Test passed successfully")
-    #     allure.attach(name="Kpi Data", body=str(kpi_val))
 
     @pytest.mark.wpa2_personal
     @pytest.mark.twog
