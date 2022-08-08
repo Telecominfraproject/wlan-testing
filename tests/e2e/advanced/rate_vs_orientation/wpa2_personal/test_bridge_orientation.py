ed39a0b9b3377697fb80ae589fe0ae719d164c21
karthikaeyetea <karthika.subramani@candelatech.com>
2022-08-08 05:36:24 +0530

    Added pass/fail validation
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/advanced/rate_vs_orientation/wpa2_personal/test_bridge_orientation.py
+++ tests/e2e/advanced/rate_vs_orientation/wpa2_personal/test_bridge_orientation.py
@@ -35,7 +35,7 @@ class TestRatevsOrientationBridge(object):
     @pytest.mark.wpa2_personal
     @pytest.mark.fiveg
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-5431", name="WIFI-5431")
-    def test_client_tcp_dl_wpa2_personal_5g_11ac(self, lf_test, station_names_fiveg, create_lanforge_chamberview_dut,
+    def test_client_tcp_dl_nss2_wpa2_personal_5g_11ac(self, lf_test, station_names_fiveg, create_lanforge_chamberview_dut,
                                           get_configuration, lf_tools):
 
         profile_data = setup_params_general["ssid_modes"]["wpa2_personal"][1]
@@ -55,12 +55,12 @@ class TestRatevsOrientationBridge(object):
         print(ser_no)
         val = [['modes: 802.11an-AC'], ['pkts: MTU'], ['directions: DUT Transmit'], ['traffic_types:TCP'],
                ['bandw_options: 80'], ['spatial_streams: 2'], ['attenuator: ' + str(ser_no[0])],
-               ['attenuator2: ' + str(ser_no[1])], ['attenuations: 0 100 10..+30..540'],
-               ['attenuations2: 0 100 10..+30..540'], ['chamber: DUT-Chamber'], ['tt_deg: 0..+30..150']] #210..+30..540
+               ['attenuator2: ' + str(ser_no[1])], ['attenuations: 0 100 210..+30..540'],
+               ['attenuations2: 0 100 210..+30..540'], ['chamber: DUT-Chamber'], ['tt_deg: 0..+30..359']] #210..+30..540 #0..+30..359
 
         if station:
             time.sleep(3)
-            rvr_o = lf_test.ratevsrange(station_name=station_names_fiveg, mode=mode,duration="5000",
+            rvr_o = lf_test.ratevsrange(station_name=station_names_fiveg, mode=mode,
                                         instance_name="ORIENTATION_RVR_BRIDGE_11_AC",
                                         vlan_id=vlan, dut_name=dut_name, raw_lines=val)
             report_name = rvr_o.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
@@ -69,11 +69,11 @@ class TestRatevsOrientationBridge(object):
             lf_test.Client_disconnect(clear_all_sta=True, clean_l3_traffic=True)
             kpi = "kpi.csv"
             pass_value = {"strong":500, "medium":400, "weak":200}
-            atn, deg = [0,10,21,24,27,30,33,36,39,42,45,48,51,54], [0,30,60,90,120,150,180,210,240,270,300,330]
+            atn, deg = [0,10,21,24,27,30,33,36,39,42,45,48,51,54], [0,30,60,90,120,150,180,210,240,270,300,330] #
             if kpi in entries:
-                kpi_val = lf_tools.read_kpi_file(column_name=["numeric-score"], dir_name=report_name) #name="CSV Data", body="Throughput value : " + str(kpi_val)
+                kpi_val = lf_tools.read_kpi_file(column_name=["numeric-score"], dir_name=report_name)
                 print(kpi_val)
-                allure.attach.file(source="../reports/" + report_name + "/" + kpi, name="kpi.csv")
+                # allure.attach.file(source="../reports/" + report_name + "/" + kpi, name="kpi.csv")
                 if str(kpi_val) == "empty":
                     print("Throughput value from kpi.csv is empty, Test failed")
                     allure.attach(name="CSV Data", body="Throughput value from kpi.csv is empty, Test failed")
@@ -89,8 +89,9 @@ class TestRatevsOrientationBridge(object):
                                 pass_fail.append("PASS")
                             else:
                                 pass_fail.append("FAIL")
-                            count += 6
-                        start += 2
+                            count += 1
+                        start += 6
+                    print(thrpt_val,"\n",pass_fail)
                     if "FAIL" in pass_fail:
                         print("Test failed due to lesser value")
                         assert False, "Test failed due to lesser value"
