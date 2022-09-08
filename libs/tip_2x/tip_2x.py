3128996165aff54b6ea593c3dab857f3fdcab5c8
shivam <shivam.thakur@candelatech.com>
2022-09-08 18:31:37 +0530

    added changes from last commit in tip_2x
    Signed-off-by: shivam <shivam.thakur@candelatech.com>

--- libs/tip_2x/tip_2x.py
+++ libs/tip_2x/tip_2x.py
@@ -667,6 +667,7 @@ class tip_2x:
             else:
                 logging.info(radio + " is up and running")
 
+
     def setup_environment_properties(self, add_allure_environment_property=None):
         if add_allure_environment_property is None:
             return
