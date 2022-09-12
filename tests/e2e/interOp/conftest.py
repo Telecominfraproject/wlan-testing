6f13409ab97343e2fcd103c0bcf3a62c72434ff4
shivam <shivam.thakur@candelatech.com>
2022-09-12 16:12:46 +0530

    added interop-03 to lab_info.json
    Signed-off-by: shivam <shivam.thakur@candelatech.com>

---
+++ tests/e2e/interOp/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture(scope="class")
+def setup_configuration(request, get_markers, get_target_object, run_lf):
+    # Predefined selected markers and selected configuration
+
+    conf = dict(request.param)
+    configuration = conf.copy()
+    requested_combination = []
+    for key in get_markers:
+        if get_markers[key]:
+            requested_combination.append(get_markers[key])
+
+    # Method to setup the basic configuration
+    data = {}
+    if not run_lf:
+        data = get_target_object.setup_basic_configuration(configuration=configuration,
+                                                           requested_combination=requested_combination)
+
+    yield data
