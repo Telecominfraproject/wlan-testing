6c0e0cb701e0cbf4e03fb42d9178c963d7b19136
jitendracandela <jitendra.kushavah@candelatech.com>
2022-09-20 19:12:05 +0530

    Added ow_sanity_lf marker for channel 11
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/e2e/basic/performance_tests/peak_throughput_tests/bridge_mode/wpa2_personal_encryption/test_peak_throughput_wpa2_40Mhz_2g.py
+++ tests/e2e/basic/performance_tests/peak_throughput_tests/bridge_mode/wpa2_personal_encryption/test_peak_throughput_wpa2_40Mhz_2g.py
@@ -42,11 +42,6 @@ class Test20Mhz2GChannel1PeakThroughput(object):
 
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6934", name="WIFI-6934")
     @pytest.mark.tcp_download
-    @pytest.mark.ow_sanity_lf
-    @allure.parent_suite("Throughput Test")
-    @allure.suite("2.4 Ghz Band")
-    @allure.title("Single client TCP Download wifi capacity 40Mhz Bw")
-    @allure.sub_suite("BRIDGE Mode")
     def test_client_wpa2_personal_bridge_tcp_dl(self, get_test_library, get_dut_logs_per_test_case,
                                        get_test_device_logs, num_stations, setup_configuration):
         """ Wifi Capacity Test BRIDGE mode
@@ -1368,7 +1363,11 @@ class Test20Mhz2GChannel11PeakThroughput(object):
 
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6934", name="WIFI-6934")
     @pytest.mark.tcp_download
-    @pytest.mark.aaa
+    @pytest.mark.ow_sanity_lf
+    @allure.parent_suite("Throughput Test")
+    @allure.suite("2.4 Ghz Band")
+    @allure.title("Single client TCP Download wifi capacity 40Mhz Bw")
+    @allure.sub_suite("BRIDGE Mode")
     def test_client_wpa2_personal_bridge_tcp_dl(self, get_test_library, get_dut_logs_per_test_case,
                                        get_test_device_logs, num_stations, setup_configuration):
         """ Wifi Capacity Test BRIDGE mode
