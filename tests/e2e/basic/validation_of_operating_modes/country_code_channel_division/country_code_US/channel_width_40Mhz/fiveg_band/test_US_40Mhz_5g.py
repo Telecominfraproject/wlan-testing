591aa17af7db234d2262714f667d2fc5b8c66d95
karthikaeyetea <karthika.subramani@candelatech.com>
2022-11-21 20:03:10 +0530

    Added new test channels
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/basic/validation_of_operating_modes/country_code_channel_division/country_code_US/channel_width_40Mhz/fiveg_band/test_US_40Mhz_5g.py
+++ tests/e2e/basic/validation_of_operating_modes/country_code_channel_division/country_code_US/channel_width_40Mhz/fiveg_band/test_US_40Mhz_5g.py
@@ -421,7 +421,7 @@ setup_params_general7 = {
         'country': 'US',
         'channel-mode': 'VHT',
         'channel-width': 40,
-        "channel": 132},
+        "channel": 116},
         "2G":
         {'band': '2G',
         'country': 'US',
@@ -441,9 +441,9 @@ setup_params_general7 = {
 @allure.feature("Channel vs Country Code")
 @allure.parent_suite("Country Code Tests")
 @allure.suite("BRIDGE Mode(40 MHz)")
-@allure.sub_suite("US country code (Channel-132)")
+@allure.sub_suite("US country code (Channel-116)")
 @pytest.mark.usefixtures("setup_configuration")
-class TestCountryUS40Mhz5GChannel132(object):
+class TestCountryUS40Mhz5GChannel116(object):
     """Country code along with Channel and Channel-width Test Bridge mode
        pytest -m "country_code and Bridge"
     """
@@ -451,11 +451,11 @@ class TestCountryUS40Mhz5GChannel132(object):
     @pytest.mark.wpa2_personal
     @pytest.mark.fourtyMhz
     @pytest.mark.fiveg
-    @pytest.mark.channel132
-    @allure.title("Test for bandwidth 40 and channel 132")
-    def test_client_bridge_wpa2_chn132_40Mhz_US_5g(self, get_test_library, setup_configuration):
+    @pytest.mark.channel116
+    @allure.title("Test for bandwidth 40 and channel 116")
+    def test_client_bridge_wpa2_chn116_40Mhz_US_5g(self, get_test_library, setup_configuration):
         """Country code Bridge Mode
-           pytest -m "country_code and fourtyMhz and wpa2 and fiveg and channel132"
+           pytest -m "country_code and fourtyMhz and wpa2 and fiveg and channel116"
         """
         profile_data = setup_params_general7["ssid_modes"]["wpa2_personal"][0]
         ssid = profile_data["ssid_name"]
@@ -487,7 +487,7 @@ setup_params_general8 = {
         'country': 'US',
         'channel-mode': 'VHT',
         'channel-width': 40,
-        "channel": 140},
+        "channel": 108},
         "2G":
         {'band': '2G',
         'country': 'US',
@@ -501,6 +501,138 @@ setup_params_general8 = {
 @pytest.mark.parametrize(
     'setup_configuration',
     [setup_params_general8],
+    indirect=True,
+    scope="class"
+)
+@allure.feature("Channel vs Country Code")
+@allure.parent_suite("Country Code Tests")
+@allure.suite("BRIDGE Mode(40 MHz)")
+@allure.sub_suite("US country code (Channel-124)")
+@pytest.mark.usefixtures("setup_configuration")
+class TestCountryUS40Mhz5GChannel124(object):
+    """Country code along with Channel and Channel-width Test Bridge mode
+       pytest -m "country_code and Bridge"
+    """
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2546", name="WIFI-6938")
+    @pytest.mark.wpa2_personal
+    @pytest.mark.fourtyMhz
+    @pytest.mark.fiveg
+    @pytest.mark.channel124
+    @allure.title("Test for bandwidth 40 and channel 124")
+    def test_client_bridge_wpa2_chn124_40Mhz_US_5g(self, get_test_library, setup_configuration):
+        """Country code Bridge Mode
+           pytest -m "country_code and fourtyMhz and wpa2 and fiveg and channel124"
+        """
+        profile_data = setup_params_general8["ssid_modes"]["wpa2_personal"][0]
+        ssid = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "fiveg"
+        vlan = 1
+        channel = setup_params_general8['rf']['5G']['channel']
+        channel_width = setup_params_general8['rf']['5G']['channel-width']
+
+        result = get_test_library.country_code_channel_division(ssid=ssid, security=security, passkey=security_key,
+                                                       band=band,  vlan_id=vlan, channel=channel, mode=mode,
+                                                       channel_width=channel_width,country_num=840,
+                                                       country='United States(US)', dut_data=setup_configuration)
+        if result:
+            assert True
+        else:
+            assert False
+
+
+setup_params_general9 = {
+    "mode": "BRIDGE",
+    "ssid_modes": {
+        "wpa2_personal": [{"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}
+                          ]},
+    "rf": {"5G":
+        {'band': '5G',
+        'country': 'US',
+        'channel-mode': 'VHT',
+        'channel-width': 40,
+        "channel": 132},
+        "2G":
+        {'band': '2G',
+        'country': 'US',
+        "channel-mode": "VHT",
+        'channel-width': 40,
+        "channel": 1}
+           },
+    "radius": False
+}
+
+@pytest.mark.parametrize(
+    'setup_configuration',
+    [setup_params_general9],
+    indirect=True,
+    scope="class"
+)
+@allure.feature("Channel vs Country Code")
+@allure.parent_suite("Country Code Tests")
+@allure.suite("BRIDGE Mode(40 MHz)")
+@allure.sub_suite("US country code (Channel-132)")
+@pytest.mark.usefixtures("setup_configuration")
+class TestCountryUS40Mhz5GChannel132(object):
+    """Country code along with Channel and Channel-width Test Bridge mode
+       pytest -m "country_code and Bridge"
+    """
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2546", name="WIFI-6938")
+    @pytest.mark.wpa2_personal
+    @pytest.mark.fourtyMhz
+    @pytest.mark.fiveg
+    @pytest.mark.channel132
+    @allure.title("Test for bandwidth 40 and channel 132")
+    def test_client_bridge_wpa2_chn132_40Mhz_US_5g(self, get_test_library, setup_configuration):
+        """Country code Bridge Mode
+           pytest -m "country_code and fourtyMhz and wpa2 and fiveg and channel132"
+        """
+        profile_data = setup_params_general9["ssid_modes"]["wpa2_personal"][0]
+        ssid = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "fiveg"
+        vlan = 1
+        channel = setup_params_general9['rf']['5G']['channel']
+        channel_width = setup_params_general9['rf']['5G']['channel-width']
+
+        result = get_test_library.country_code_channel_division(ssid=ssid, security=security, passkey=security_key,
+                                                       band=band,  vlan_id=vlan, channel=channel, mode=mode,
+                                                       channel_width=channel_width,country_num=840,
+                                                       country='United States(US)', dut_data=setup_configuration)
+        if result:
+            assert True
+        else:
+            assert False
+
+
+setup_params_general10 = {
+    "mode": "BRIDGE",
+    "ssid_modes": {
+        "wpa2_personal": [{"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}
+                          ]},
+    "rf": {"5G":
+        {'band': '5G',
+        'country': 'US',
+        'channel-mode': 'VHT',
+        'channel-width': 40,
+        "channel": 140},
+        "2G":
+        {'band': '2G',
+        'country': 'US',
+        "channel-mode": "VHT",
+        'channel-width': 40,
+        "channel": 1}
+           },
+    "radius": False
+}
+
+@pytest.mark.parametrize(
+    'setup_configuration',
+    [setup_params_general10],
     indirect=True,
     scope="class"
 )
@@ -523,15 +655,15 @@ class TestCountryUS40Mhz5GChannel140(object):
         """Country code Bridge Mode
            pytest -m "country_code and fourtyMhz and wpa2 and fiveg and channel140"
         """
-        profile_data = setup_params_general8["ssid_modes"]["wpa2_personal"][0]
+        profile_data = setup_params_general10["ssid_modes"]["wpa2_personal"][0]
         ssid = profile_data["ssid_name"]
         security_key = profile_data["security_key"]
         security = "wpa2"
         mode = "BRIDGE"
         band = "fiveg"
         vlan = 1
-        channel = setup_params_general8['rf']['5G']['channel']
-        channel_width = setup_params_general8['rf']['5G']['channel-width']
+        channel = setup_params_general10['rf']['5G']['channel']
+        channel_width = setup_params_general10['rf']['5G']['channel-width']
 
         result = get_test_library.country_code_channel_division(ssid=ssid, security=security, passkey=security_key,
                                                        band=band,  vlan_id=vlan, channel=channel, mode=mode,
@@ -541,3 +673,137 @@ class TestCountryUS40Mhz5GChannel140(object):
             assert True
         else:
             assert False
+
+
+setup_params_general11 = {
+    "mode": "BRIDGE",
+    "ssid_modes": {
+        "wpa2_personal": [{"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}
+                          ]},
+    "rf": {"5G":
+        {'band': '5G',
+        'country': 'US',
+        'channel-mode': 'VHT',
+        'channel-width': 40,
+        "channel": 149},
+        "2G":
+        {'band': '2G',
+        'country': 'US',
+        "channel-mode": "VHT",
+        'channel-width': 40,
+        "channel": 1}
+           },
+    "radius": False
+}
+
+@pytest.mark.parametrize(
+    'setup_configuration',
+    [setup_params_general11],
+    indirect=True,
+    scope="class"
+)
+@allure.feature("Channel vs Country Code")
+@allure.parent_suite("Country Code Tests")
+@allure.suite("BRIDGE Mode(40 MHz)")
+@allure.sub_suite("US country code (Channel-149)")
+@pytest.mark.usefixtures("setup_configuration")
+class TestCountryUS40Mhz5GChannel149(object):
+    """Country code along with Channel and Channel-width Test Bridge mode
+       pytest -m "country_code and Bridge"
+    """
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2546", name="WIFI-6938")
+    @pytest.mark.wpa2_personal
+    @pytest.mark.fourtyMhz
+    @pytest.mark.fiveg
+    @pytest.mark.channel149
+    @allure.title("Test for bandwidth 40 and channel 149")
+    def test_client_bridge_wpa2_chn149_40Mhz_US_5g(self, get_test_library, setup_configuration):
+        """Country code Bridge Mode
+           pytest -m "country_code and fourtyMhz and wpa2 and fiveg and channel149"
+        """
+        profile_data = setup_params_general11["ssid_modes"]["wpa2_personal"][0]
+        ssid = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "fiveg"
+        vlan = 1
+        channel = setup_params_general11['rf']['5G']['channel']
+        channel_width = setup_params_general11['rf']['5G']['channel-width']
+
+        result = get_test_library.country_code_channel_division(ssid=ssid, security=security, passkey=security_key,
+                                                       band=band,  vlan_id=vlan, channel=channel, mode=mode,
+                                                       channel_width=channel_width,country_num=840,
+                                                       country='United States(US)', dut_data=setup_configuration)
+        if result:
+            assert True
+        else:
+            assert False
+
+
+setup_params_general12 = {
+    "mode": "BRIDGE",
+    "ssid_modes": {
+        "wpa2_personal": [{"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}
+                          ]},
+    "rf": {"5G":
+        {'band': '5G',
+        'country': 'US',
+        'channel-mode': 'VHT',
+        'channel-width': 40,
+        "channel": 157},
+        "2G":
+        {'band': '2G',
+        'country': 'US',
+        "channel-mode": "VHT",
+        'channel-width': 40,
+        "channel": 1}
+           },
+    "radius": False
+}
+
+@pytest.mark.parametrize(
+    'setup_configuration',
+    [setup_params_general12],
+    indirect=True,
+    scope="class"
+)
+@allure.feature("Channel vs Country Code")
+@allure.parent_suite("Country Code Tests")
+@allure.suite("BRIDGE Mode(40 MHz)")
+@allure.sub_suite("US country code (Channel-157)")
+@pytest.mark.usefixtures("setup_configuration")
+class TestCountryUS40Mhz5GChannel157(object):
+    """Country code along with Channel and Channel-width Test Bridge mode
+       pytest -m "country_code and Bridge"
+    """
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-2546", name="WIFI-6938")
+    @pytest.mark.wpa2_personal
+    @pytest.mark.fourtyMhz
+    @pytest.mark.fiveg
+    @pytest.mark.channel157
+    @allure.title("Test for bandwidth 40 and channel 157")
+    def test_client_bridge_wpa2_chn157_40Mhz_US_5g(self, get_test_library, setup_configuration):
+        """Country code Bridge Mode
+           pytest -m "country_code and fourtyMhz and wpa2 and fiveg and channel157"
+        """
+        profile_data = setup_params_general12["ssid_modes"]["wpa2_personal"][0]
+        ssid = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "fiveg"
+        vlan = 1
+        channel = setup_params_general12['rf']['5G']['channel']
+        channel_width = setup_params_general12['rf']['5G']['channel-width']
+
+        result = get_test_library.country_code_channel_division(ssid=ssid, security=security, passkey=security_key,
+                                                       band=band,  vlan_id=vlan, channel=channel, mode=mode,
+                                                       channel_width=channel_width,country_num=840,
+                                                       country='United States(US)', dut_data=setup_configuration)
+        if result:
+            assert True
+        else:
+            assert False
+
+
