57647065b151c87ea2be9cdb6dbde2a52c5f6f89
haricharan-jaka <haricharan.jaka@candelatech.com>
2022-12-06 13:33:10 +0530

    Added Open roaming configuration Fixture
    Signed-off-by: haricharan-jaka <haricharan.jaka@candelatech.com>

--- tests/e2e/interOp/conftest.py
+++ tests/e2e/interOp/conftest.py
@@ -1,5 +1,6 @@
+import allure
 import pytest
-
+import logging
 
 @pytest.fixture(scope="class")
 def setup_configuration(request, get_markers, get_target_object, run_lf):
@@ -17,5 +18,24 @@ def setup_configuration(request, get_markers, get_target_object, run_lf):
     if not run_lf:
         data = get_target_object.setup_basic_configuration(configuration=configuration,
                                                            requested_combination=requested_combination)
+    logging.info("dut_data after config applied: " + str(data))
+    yield data
 
+@pytest.fixture(scope="class")
+def setup_open_roaming_configuration(request, get_markers, get_target_object, run_lf):
+    # Predefined selected markers and selected configuration
+
+    conf = dict(request.param)
+    configuration = conf.copy()
+    requested_combination = []
+    for key in get_markers:
+        if get_markers[key]:
+            requested_combination.append(get_markers[key])
+
+    # Method to setup the basic configuration
+    data = {}
+    if not run_lf:
+        data = get_target_object.setup_basic_configuration(configuration=configuration, requested_combination=
+                                                           requested_combination, open_roaming=True)
+    logging.info("dut_data after config applied: " + str(data))
     yield data
