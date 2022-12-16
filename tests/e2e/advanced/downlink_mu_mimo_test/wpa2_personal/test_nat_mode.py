488aaa5e6642c44c50da4e4f852ceeb4d42b5d0c
karthikaeyetea <karthika.subramani@candelatech.com>
2022-12-16 07:49:49 +0530

    Refactored MU-MIMO
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/advanced/downlink_mu_mimo_test/wpa2_personal/test_nat_mode.py
+++ tests/e2e/advanced/downlink_mu_mimo_test/wpa2_personal/test_nat_mode.py
@@ -1,36 +1,37 @@
 """
 
     Performance Test: Downlink MU-MIMO Test: NAT Mode
-    pytest -m "downlink_mu_mimo and nat and wpa2_personal and fiveg"
+    pytest -m "downlink_mu_mimo and nat and wpa2_personal"
 
 """
 import os
 import pytest
 import allure
 
-pytestmark = [pytest.mark.downlink_mu_mimo, pytest.mark.nat, pytest.mark.wpa2_personal]
+pytestmark = [pytest.mark.mu_mimo_performance_tests, pytest.mark.nat, pytest.mark.wpa2_personal]
 
 setup_params_general = {
     "mode": "NAT",
     "ssid_modes": {
         "wpa2_personal": [
-            {"ssid_name": "mu-mimo-5g", "appliedRadios": ["5G"], "security_key": "something"},
-            {"ssid_name": "mu-mimo-2g", "appliedRadios": ["2G"], "security_key": "something"}
+            {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"},
+            {"ssid_name": "ssid_wpa2_2g", "appliedRadios": ["2G"], "security_key": "something"}
         ]
     },
     "rf": [],
     "radius": False
 }
 
-
-@allure.suite("performance")
-@allure.feature("NAT MODE wpa2_personal security and Downlink MU_MIMO Test")
 @pytest.mark.parametrize(
     'setup_configuration',
     [setup_params_general],
     indirect=True,
     scope="class"
 )
+@allure.parent_suite("Downlink MU_MIMO Tests")
+@allure.suite("WPA2 Personal Security")
+@allure.sub_suite("NAT Mode")
+@allure.feature("TR-398 Issue 2")
 @pytest.mark.usefixtures("setup_configuration")
 class TestMuMimoNat(object):
     """
@@ -42,68 +43,23 @@ class TestMuMimoNat(object):
                      name="WIFI-6849")
     @pytest.mark.wpa2_personal
     @pytest.mark.fiveg
-    def test_mu_mimo_wpa2_personal_nat_5g(self, lf_tools, lf_test, create_lanforge_chamberview_dut):
-        """
-            Downlink MU-MIMO Test: nat Mode
-            pytest -m downlink_mu_mimo and nat and wpa2_personal and fiveg
-            """
-        dut_name = create_lanforge_chamberview_dut
-        mode = "NAT"
-        vlan = 1
-        dut_5g, dut_2g = "", ""
-        radios_2g, radios_5g, radios_ax = [], [], []
-        data = lf_tools.json_get(_req_url="/port?fields=alias,port,mode")
-        data = data['interfaces']
-        port, port_data = "", []
-        for i in data:
-            for j in i:
-                if i[j]['mode'] != '':
-                    port_data.append(i)
-
-        for item in range(len(port_data)):
-            for p in port_data[item]:
-                temp = port_data[item][p]['port'].split('.')
-                temp = list(map(int, temp))
-                temp = list(map(str, temp))
-                port = ".".join(temp)
-                if port_data[item][p]['mode'] == '802.11bgn-AC':
-                    radios_2g.append(port + " " + port_data[item][p]['alias'])
-                if port_data[item][p]['mode'] == '802.11an-AC':
-                    radios_5g.append(port + " " + port_data[item][p]['alias'])
-                if port_data[item][p]['mode'] == '802.11abgn-AX':
-                    radios_ax.append(port + " " + port_data[item][p]['alias'])
-
-        print(lf_tools.dut_idx_mapping)
-        for i in lf_tools.dut_idx_mapping:
-            if lf_tools.dut_idx_mapping[i][3] == "5G":
-                dut_5g = dut_name + ' ' + lf_tools.dut_idx_mapping[i][0] + ' ' + lf_tools.dut_idx_mapping[i][4] + ' (1)'
-                print(dut_5g)
-            if lf_tools.dut_idx_mapping[i][3] == "2G":
-                dut_2g = dut_name + ' ' + lf_tools.dut_idx_mapping[i][0] + ' ' + lf_tools.dut_idx_mapping[i][4] + ' (2)'
-                print(dut_2g)
-        mimo_obj = lf_test.downlink_mu_mimo(radios_2g=radios_2g, radios_5g=radios_5g, radios_ax=radios_ax, mode=mode,
-                                            vlan_id=vlan, dut_name=dut_name, dut_5g=dut_5g, dut_2g=dut_2g, skip_2g=True,
-                                            skip_5g=False)
-        report_name = mimo_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-        lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Downlink MU-MIMO Test")
-        assert True
-
-    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6849",
-                     name="WIFI-6849")
-    @pytest.mark.wpa2_personal
-    @pytest.mark.twog
-    def test_mu_mimo_wpa2_personal_nat_2g(self, lf_tools, lf_test, create_lanforge_chamberview_dut):
+    def test_mu_mimo_wpa2_personal_nat_5g(self, get_test_library, setup_configuration, check_connectivity):
         """
             Downlink MU-MIMO Test: Nat Mode
-            pytest -m downlink_mu_mimo and nat and wpa2_personal and twog
-            """
-        print('lf tool')
-        dut_name = create_lanforge_chamberview_dut
-        mode = "NAT"
+            pytest -m downlink_mu_mimo and nat and wpa2_personal and fiveg
+        """
+        profile_data = {"ssid_name": "ssid_wpa2_5g", "appliedRadios": ["5G"], "security_key": "something"}
+        ssid_name = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "NAT-WAN"
+        band = "fiveg"
+        mode = "BRIDGE"
         vlan = 1
+        dut_name = list(setup_configuration.keys())[0]
         dut_5g, dut_2g = "", ""
         radios_2g, radios_5g, radios_ax = [], [], []
-        data = lf_tools.json_get(_req_url="/port?fields=alias,port,mode")
+        data = get_test_library.json_get(_req_url="/port?fields=alias,port,mode")
         data = data['interfaces']
         port, port_data = "", []
         for i in data:
@@ -117,7 +73,6 @@ class TestMuMimoNat(object):
                 temp = list(map(int, temp))
                 temp = list(map(str, temp))
                 port = ".".join(temp)
-                print(port)
                 if port_data[item][p]['mode'] == '802.11bgn-AC':
                     radios_2g.append(port + " " + port_data[item][p]['alias'])
                 if port_data[item][p]['mode'] == '802.11an-AC':
@@ -125,21 +80,19 @@ class TestMuMimoNat(object):
                 if port_data[item][p]['mode'] == '802.11abgn-AX':
                     radios_ax.append(port + " " + port_data[item][p]['alias'])
 
-        print(lf_tools.dut_idx_mapping)
-        for i in lf_tools.dut_idx_mapping:
-            if lf_tools.dut_idx_mapping[i][3] == "5G":
-                dut_5g = dut_name + ' ' + lf_tools.dut_idx_mapping[i][0] + ' ' + lf_tools.dut_idx_mapping[i][
-                    4] + ' (1)'
-                print(dut_5g)
-            if lf_tools.dut_idx_mapping[i][3] == "2G":
-                dut_2g = dut_name + ' ' + lf_tools.dut_idx_mapping[i][0] + ' ' + lf_tools.dut_idx_mapping[i][
-                    4] + ' (2)'
-                print(dut_2g)
-        mimo_obj = lf_test.downlink_mu_mimo(radios_2g=radios_2g, radios_5g=radios_5g, radios_ax=radios_ax,
-                                            mode=mode,
-                                            vlan_id=vlan, dut_name=dut_name, dut_5g=dut_5g, dut_2g=dut_2g,
-                                            skip_2g=False,
-                                            skip_5g=True)
-        report_name = mimo_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-        lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Downlink MU-MIMO Test")
-        assert True
+        for i in setup_configuration[dut_name]['ssid_data']:
+            get_test_library.dut_idx_mapping[str(i)] = list(setup_configuration[dut_name]['ssid_data'][i].values())
+            if get_test_library.dut_idx_mapping[str(i)][3] == "5G":
+                dut_5g = dut_name + ' ' + get_test_library.dut_idx_mapping[str(i)][0] + ' ' \
+                                                                                        '' + \
+                         get_test_library.dut_idx_mapping[str(i)][4] + f' (1)'
+        result, description = get_test_library.tr398(radios_2g=radios_2g, radios_5g=radios_5g, radios_ax=radios_ax,
+                                                     dut_name=dut_name, dut_5g=dut_5g, dut_2g=dut_2g,  mode=mode,
+                                                     vlan_id=vlan,  skip_2g=True, skip_5g=False, test="mu_mimo",
+                                                     ssid_name=ssid_name, security_key=security_key, security=security,
+                                                     move_to_influx=False, dut_data=setup_configuration, sniff_packets=False,
+                                                     tr398=True)
+        if result:
+            assert True
+        else:
+            assert False, description
