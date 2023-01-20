fb6201456865bb60823085fd93b5ffce910c419c
karthikaeyetea <karthika.subramani@candelatech.com>
2023-01-20 21:18:24 +0530

    Refactored dual band performance test-bridge-wpa2
    Signed-off-by: karthikaeyetea <karthika.subramani@candelatech.com>

--- tests/e2e/basic/performance_tests/dual_band_performance_test/open/test_bridge_mode.py
+++ tests/e2e/basic/performance_tests/dual_band_performance_test/open/test_bridge_mode.py
@@ -10,7 +10,6 @@ import allure
 import pytest
 
 pytestmark = [pytest.mark.dual_band_test, pytest.mark.bridge, pytest.mark.performance_release]#,
-#              pytest.mark.usefixtures("setup_test_run")]
 
 setup_params_general = {
     "mode": "BRIDGE",
@@ -41,31 +40,26 @@ class TestDualbandPerformanceBridge(object):
     @pytest.mark.open
     @pytest.mark.twog
     @pytest.mark.fiveg
-    def test_client_open_bridge(self,  lf_tools,
-                                  create_lanforge_chamberview_dut, lf_test, get_configuration):
+    def test_client_open_bridge(self,  get_test_library, setup_configuration, check_connectivity):
         profile_data = setup_params_general["ssid_modes"]["open"]
         ssid_2G = profile_data[0]["ssid_name"]
         ssid_5G = profile_data[0]["ssid_name"]
-        dut_name = create_lanforge_chamberview_dut
+        dut_name = list(setup_configuration.keys())[0]
         mode = "BRIDGE"
         vlan = 1
-        print(lf_tools.dut_idx_mapping)
         dut_5g = ""
         dut_2g = ""
-        for i in lf_tools.dut_idx_mapping:
-            if lf_tools.dut_idx_mapping[i][3] == "5G":
-                dut_5g = dut_name + ' ' + lf_tools.dut_idx_mapping[i][0] + ' ' + lf_tools.dut_idx_mapping[i][4]
+        for i in get_test_library.dut_idx_mapping:
+            if get_test_library.dut_idx_mapping[i][3] == "5G":
+                dut_5g = dut_name + ' ' + get_test_library.dut_idx_mapping[i][0] + ' ' + get_test_library.dut_idx_mapping[i][4]
                 print(dut_5g)
-            if lf_tools.dut_idx_mapping[i][3] == "2G":
-                dut_2g = dut_name + ' ' + lf_tools.dut_idx_mapping[i][0] + ' ' + lf_tools.dut_idx_mapping[i][4]
+            if get_test_library.dut_idx_mapping[i][3] == "2G":
+                dut_2g = dut_name + ' ' + get_test_library.dut_idx_mapping[i][0] + ' ' + get_test_library.dut_idx_mapping[i][4]
                 print(dut_2g)
-        if ssid_2G and ssid_5G not in get_vif_state:
-            allure.attach(name="retest,vif state ssid not available:", body=str(get_vif_state))
-            pytest.xfail("SSID's NOT AVAILABLE IN VIF STATE")
 
-        dbpt_obj = lf_test.dualbandperformancetest(mode=mode, ssid_2G=ssid_2G, ssid_5G=ssid_5G,
+        dbpt_obj = get_test_library.dualbandperformancetest(mode=mode, ssid_2G=ssid_2G, ssid_5G=ssid_5G,
                                                    instance_name="dbp_instance_openp_bridge_p",
                                                    vlan_id=vlan, dut_5g=dut_5g, dut_2g=dut_2g)
         report_name = dbpt_obj.report_name[0]['LAST']["response"].split(":::")[1].split("/")[-1]
-        lf_tools.attach_report_graphs(report_name=report_name, pdf_name="Dual Band Performance Test")
+        get_test_library.attach_report_graphs(report_name=report_name, pdf_name="Dual Band Performance Test")
         assert True
