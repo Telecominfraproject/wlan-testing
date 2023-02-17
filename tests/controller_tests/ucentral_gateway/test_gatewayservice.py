1973afaaf0afe2a708a33a9df7792e7b2ae233e2
GopiRaga7 <gopi.raga@candelatech.com>
2023-02-17 09:47:18 +0530

    Asb script two testcases added
    Signed-off-by: GopiRaga7 <gopi.raga@candelatech.com>

--- tests/controller_tests/ucentral_gateway/test_gatewayservice.py
+++ tests/controller_tests/ucentral_gateway/test_gatewayservice.py
@@ -491,7 +491,10 @@ class TestUcentralGatewayService(object):
         # allure.attach(name="Device RTTY parameters", body=str(resp.json()), #attachment_type=#allure.#attachment_type.JSON)
         assert resp.status_code == 200
 
-    def test_gw_service_asb_script(self, get_target_object, get_testbed_details):
+    @pytest.mark.gw_asb_non_restricted
+    @allure.title("Asb script on non restricted AP")
+    @allure.testcase(name="WIFI-12235", url="https://telecominfraproject.atlassian.net/browse/WIFI-12235")
+    def test_asb_on_non_restricted_ap(self, get_target_object, get_testbed_details):
         device_name = get_testbed_details['device_under_tests'][0]['identifier']
         payload = {
             "serialNumber": device_name,
@@ -504,18 +507,10 @@ class TestUcentralGatewayService(object):
             "deferred": True,
             "uri": ""
         }
-        resp = get_target_object.controller_library_object.asb_script(device_name, payload)
-        return resp
-
-    @pytest.mark.gw_asb_non_restricted
-    @allure.title("ASB SCRIPT NON RESTRICTED AP")
-    @allure.testcase(name="WIFI-12235", url="https://telecominfraproject.atlassian.net/browse/WIFI-12235")
-    def test_asb_on_non_restricted_ap(self, get_target_object, get_testbed_details):
-        device_name = get_testbed_details['device_under_tests'][0]['identifier']
         resp = resp = get_target_object.controller_library_object.check_restrictions(device_name)
         if not resp:
             logging.info("AP is in not restricted and we can trigger script")
-            uuid = self.test_gw_service_asb_script(get_target_object, get_testbed_details)
+            uuid = get_target_object.controller_library_object.asb_script(device_name, payload)
             resp = get_target_object.controller_library_object.get_file(device_name, uuid)
             assert resp.status_code == 200
         else:
@@ -524,7 +519,7 @@ class TestUcentralGatewayService(object):
             resp = resp = get_target_object.controller_library_object.check_restrictions(device_name)
             if not resp:
                 logging.info("Removed Restrictions")
-                uuid = self.test_gw_service_asb_script(get_target_object, get_testbed_details)
+                uuid = get_target_object.controller_library_object.asb_script(device_name, payload)
                 resp = get_target_object.controller_library_object.get_file(device_name, uuid)
                 assert resp.status_code == 200
             else:
@@ -532,22 +527,32 @@ class TestUcentralGatewayService(object):
                 assert False
 
     @pytest.mark.gw_asb_restricted
-    @allure.title("ASB SCRIPT RESTRICTED AP")
+    @allure.title("Asb script on restricted AP")
     @allure.testcase(name="WIFI-12236", url="https://telecominfraproject.atlassian.net/browse/WIFI-12236")
     def test_asb_on_restricted_ap(self, get_target_object, get_testbed_details):
         device_name = get_testbed_details['device_under_tests'][0]['identifier']
-
+        payload = {
+            "serialNumber": device_name,
+            "timeout": 30,
+            "type": "diagnostic",
+            "script": "",
+            "scriptId": "",
+            "when": 0,
+            "signature": "",
+            "deferred": True,
+            "uri": ""
+        }
         restrictions_file = 'echo \"{\\"country\\":[\\"US\\", \\"CA\\"],\\"dfs\\": true,\\"rtty\\": true,\\"tty\\": ' \
                             'true,\\"developer\\": true,\\"sysupgrade\\": true,\\"commands\\": true,\\"key_info\\": {' \
                             '\\"vendor\\": \\"dummy\\",\\"algo\\": \\"static\\"}}\" > /certificates/restrictions.json ' \
                             '&& echo \"True\"'
-        developer_mode = 'fw_setenv developer 0'
+        developer_mode = "fw_setenv developer 0"
         output = get_target_object.get_dut_library_object().add_restrictions(restrictions_file=restrictions_file,
                                                                              developer_mode=developer_mode)
         resp = resp = get_target_object.controller_library_object.check_restrictions(device_name)
         if resp:
             logging.info("From GW it's confirmed that AP is restricted now")
-            uuid = self.test_gw_service_asb_script(get_target_object, get_testbed_details)
+            uuid = get_target_object.controller_library_object.asb_script(device_name, payload)
             resp = get_target_object.controller_library_object.get_file(device_name, uuid)
             assert resp.status_code == 200
         else:
