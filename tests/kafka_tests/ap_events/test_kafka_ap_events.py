5db1493349574a29d67754973307d13c9acc69d9
anil-tegala <anil.tegala@candelatech.com>
2023-06-06 19:07:52 +0530

    dns and dhcp event msgs validation added
    Signed-off-by: anil-tegala <anil.tegala@candelatech.com>

--- tests/kafka_tests/ap_events/test_kafka_ap_events.py
+++ tests/kafka_tests/ap_events/test_kafka_ap_events.py
@@ -951,14 +951,14 @@ class TestKafkaApEvents(object):
         payload_msg = "ssh"
         record_messages = []
         run_once = False
-        for i in range(len(config_data["interfaces"])):
-            if "services" in config_data["interfaces"][i]:
-                if "ssh" not in config_data["interfaces"][i]["services"]:
-                    config_data["interfaces"][i]["services"].append("ssh")
-        if 'types' in config_data["metrics"]["realtime"]:
-            config_data["metrics"]["realtime"]["types"] = ["ssh"]
-        logging.info(config_data)
         for ap in range(len(get_target_object.device_under_tests_info)):
+            for i in range(len(config_data["interfaces"])):
+                if "services" in config_data["interfaces"][i]:
+                    if "ssh" not in config_data["interfaces"][i]["services"]:
+                        config_data["interfaces"][i]["services"].append("ssh")
+            if 'types' in config_data["metrics"]["realtime"]:
+                config_data["metrics"]["realtime"]["types"] = ["ssh"]
+            logging.info(config_data)
             serial_number = get_target_object.device_under_tests_info[ap]['identifier']
             payload = {"configuration": json.dumps(config_data), "serialNumber": serial_number, "UUID": 1}
             uri = get_target_object.firmware_library_object.sdk_client.build_uri(
@@ -981,12 +981,13 @@ class TestKafkaApEvents(object):
             pattern = re.search(r'inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', cmd_output)
             if pattern:
                 ip_address = pattern.group(1)
-                logging.info(f"The IP address of up0v0 is: {pattern}")
+                logging.info(f"The IP address of up0v0 is: {ip_address}")
             else:
                 logging.info(f"No IP address found for up0v0")
                 pytest.fail("up0v0 Interface don't has IP address")
             host_ip, host_username, host_password = get_target_object.device_under_tests_info[ap]['host_ip'], \
-                get_target_object.device_under_tests_info[ap]['host_username'], get_target_object.device_under_tests_info[ap][
+                get_target_object.device_under_tests_info[ap]['host_username'], \
+            get_target_object.device_under_tests_info[ap][
                 'host_password']
             upstream = get_target_object.device_under_tests_info[ap]['wan_port'].split(".")[2]
             ssh_client = paramiko.SSHClient()
@@ -1029,6 +1030,214 @@ class TestKafkaApEvents(object):
                                     is_valid = True
                                     allure.attach(
                                         name="Check ssh event Message",
+                                        body=str(record))
+                                    msg_found = True
+                                    break
+                                else:
+                                    continue
+                elif msg_found:
+                    break
+                else:
+                    # No messages received, sleep for a bit
+                    time.sleep(1)
+        allure.attach(name="Messages Recorded in Test Execution", body=str(record_messages))
+
+        # Assert that the message is valid
+        assert is_valid, f'{payload_msg} Message not found'
+
+    @allure.title("Test to check Health DNS event")
+    @pytest.mark.health_dns
+    def test_kafka_health_dns(self, get_target_object, get_testbed_details, kafka_consumer_deq):
+        is_valid = False
+        msg_found = False
+        payload_msg = "health.dns"
+        record_messages = []
+        run_once = False
+        for ap in range(len(get_target_object.device_under_tests_info)):
+            for i in range(len(config_data["interfaces"])):
+                if config_data["interfaces"][i]["name"] == "WAN":
+                    if "ssids" in config_data["interfaces"][i]:
+                        config_data["interfaces"][i].pop("ssids")
+                if config_data["interfaces"][i]["name"] == "LAN" and "ssids" not in config_data["interfaces"][i]:
+                    config_data["interfaces"][i]["ssids"] = [
+                        {
+                            "name": "OpenWifi",
+                            "wifi-bands": [
+                                "2G", "5G"
+                            ],
+                            "bss-mode": "ap",
+                            "encryption": {
+                                "proto": "psk2",
+                                "key": "OpenWifi@123",
+                                "ieee80211w": "optional"
+                            }
+                        }
+                    ]
+            if 'health' not in config_data["metrics"]["realtime"]["types"]:
+                config_data["metrics"]["realtime"]["types"] = ["health"]
+            logging.info(config_data)
+            serial_number = get_target_object.device_under_tests_info[ap]['identifier']
+            payload = {"configuration": json.dumps(config_data), "serialNumber": serial_number, "UUID": 1}
+            uri = get_target_object.firmware_library_object.sdk_client.build_uri(
+                "device/" + serial_number + "/configure")
+            logging.info("Sending Command: " + "\n" + str(uri) + "\n" +
+                         "TimeStamp: " + str(datetime.datetime.utcnow()) + "\n" +
+                         "Data: " + str(json.dumps(payload, indent=2)) + "\n" +
+                         "Headers: " + str(get_target_object.firmware_library_object.sdk_client.make_headers()))
+            allure.attach(name="Sending Command:", body="Sending Command: " + "\n" + str(uri) + "\n" +
+                                                        "TimeStamp: " + str(datetime.datetime.utcnow()) + "\n" +
+                                                        "Data: " + str(payload) + "\n" +
+                                                        "Headers: " + str(
+                get_target_object.firmware_library_object.sdk_client.make_headers()))
+            resp = requests.post(uri, data=json.dumps(payload),
+                                 headers=get_target_object.firmware_library_object.sdk_client.make_headers(),
+                                 verify=False, timeout=120)
+            logging.info(resp.json())
+            allure.attach(name=f"Response - {resp.status_code}{resp.reason}", body=str(resp.json()))
+
+            timeout = 120  # Timeout in seconds
+            start_time = time.time()
+            while time.time() - start_time < timeout:
+                # Poll for new messages
+                messages = kafka_consumer_deq.poll(timeout_ms=120000)
+                # change the interface ip from configured gateway to some other ip to capture dns event
+                if not run_once:
+                    cmd_output = get_target_object.dut_library_object.run_generic_command(cmd="ifconfig down1v0")
+                    allure.attach(name="down1v0 interface info before ip change", body=str(cmd_output))
+                    pattern = re.search(r'inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', cmd_output)
+                    ip_address = pattern.group(1)
+                    if ip_address:
+                        logging.info(f"The IP address of down1v0 is: {ip_address}")
+                        cmd_output = get_target_object.dut_library_object.run_generic_command(cmd="ifconfig down1v0 "
+                                                                                                  "192.146.5.6")
+                        allure.attach(name="down1v0 interface info after ip change", body=str(cmd_output))
+                        pattern1 = re.search(r'inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', cmd_output)
+                        ip_address1 = pattern1.group(1)
+                        if ip_address1 == '192.146.5.6':
+                            run_once = True
+                    else:
+                        logging.info(f"No IP address found for down1v0")
+                        pytest.fail("down1v0 Interface do not have an IP address")
+                # Check if any messages were returned
+                if messages and not msg_found:
+                    logging.info(f"Polled messages: {messages}")
+                    for topic, records in messages.items():
+                        logging.info(f"Kafka Topic {topic}")
+                        logging.info(f"Messages in Record: {records}")
+                        for record in records:
+                            record_messages.append(record)
+                            if 'type' in record.value['payload']:
+                                event_type = record.value['payload']['type']
+                                # Validate the message value here
+                                if event_type == payload_msg:
+                                    logging.info(f"{payload_msg} has found in the Message")
+                                    is_valid = True
+                                    allure.attach(
+                                        name="Check health DNS event Message",
+                                        body=str(record))
+                                    msg_found = True
+                                    break
+                                else:
+                                    continue
+                elif msg_found:
+                    break
+                else:
+                    # No messages received, sleep for a bit
+                    time.sleep(1)
+        allure.attach(name="Messages Recorded in Test Execution", body=str(record_messages))
+
+        # Assert that the message is valid
+        assert is_valid, f'{payload_msg} Message not found'
+
+    @allure.title("Test to check Health DHCP Event")
+    @pytest.mark.health_dhcp
+    def test_kafka_health_dhcp(self, get_test_library, get_target_object, kafka_consumer_deq):
+        is_valid = False
+        msg_found = False
+        payload_msg = "health.dhcp"
+        record_messages = []
+        run_once = False
+        logging.info(config_data)
+        for ap in range(len(get_target_object.device_under_tests_info)):
+            serial_number = get_target_object.device_under_tests_info[ap]['identifier']
+            for i in range(len(config_data["interfaces"])):
+                if config_data["interfaces"][i]["name"] == "WAN":
+                    if "ssids" in config_data["interfaces"][i]:
+                        config_data["interfaces"][i].pop("ssids")
+                if config_data["interfaces"][i]["name"] == "LAN" and "ssids" not in config_data["interfaces"][i]:
+                    config_data["interfaces"][i]["ssids"] = [
+                        {
+                            "name": "OpenWifi",
+                            "wifi-bands": [
+                                "2G", "5G"
+                            ],
+                            "bss-mode": "ap",
+                            "encryption": {
+                                "proto": "psk2",
+                                "key": "OpenWifi@123",
+                                "ieee80211w": "optional"
+                            }
+                        }
+                    ]
+            if 'health' not in config_data["metrics"]["realtime"]["types"]:
+                config_data["metrics"]["realtime"]["types"] = ["health"]
+            payload = {"configuration": json.dumps(config_data), "serialNumber": serial_number, "UUID": 1}
+            uri = get_target_object.firmware_library_object.sdk_client.build_uri(
+                "device/" + serial_number + "/configure")
+            logging.info("Sending Command: " + "\n" + str(uri) + "\n" +
+                         "TimeStamp: " + str(datetime.datetime.utcnow()) + "\n" +
+                         "Data: " + str(json.dumps(payload, indent=2)) + "\n" +
+                         "Headers: " + str(get_target_object.firmware_library_object.sdk_client.make_headers()))
+            allure.attach(name="Sending Command:", body="Sending Command: " + "\n" + str(uri) + "\n" +
+                                                        "TimeStamp: " + str(datetime.datetime.utcnow()) + "\n" +
+                                                        "Data: " + str(payload) + "\n" +
+                                                        "Headers: " + str(
+                get_target_object.firmware_library_object.sdk_client.make_headers()))
+            resp = requests.post(uri, data=json.dumps(payload),
+                                 headers=get_target_object.firmware_library_object.sdk_client.make_headers(),
+                                 verify=False, timeout=120)
+            logging.info(resp.json())
+            allure.attach(name=f"Response - {resp.status_code}{resp.reason}", body=str(resp.json()))
+
+            timeout = 120  # Timeout in seconds
+            start_time = time.time()
+            while time.time() - start_time < timeout:
+                # Poll for new messages
+                messages = kafka_consumer_deq.poll(timeout_ms=120000)
+                # change the interface ip from configured gateway to some other ip to capture dhcp event
+                if not run_once:
+                    cmd_output = get_target_object.dut_library_object.run_generic_command(cmd="ifconfig down1v0")
+                    allure.attach(name="down1v0 interface info before ip change", body=str(cmd_output))
+                    pattern = re.search(r'inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', cmd_output)
+                    ip_address = pattern.group(1)
+                    if ip_address:
+                        logging.info(f"The IP address of down1v0 is: {ip_address}")
+                        cmd_output = get_target_object.dut_library_object.run_generic_command(cmd="ifconfig down1v0 "
+                                                                                                  "192.146.5.6")
+                        allure.attach(name="down1v0 interface info after ip change", body=str(cmd_output))
+                        pattern1 = re.search(r'inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', cmd_output)
+                        ip_address1 = pattern1.group(1)
+                        if ip_address1 == '192.146.5.6':
+                            run_once = True
+                    else:
+                        logging.info(f"No IP address found for down1v0")
+                        pytest.fail("down1v0 Interface do not have an IP address")
+                # Check if any messages were returned
+                if messages and not msg_found:
+                    logging.info(f"Polled messages: {messages}")
+                    for topic, records in messages.items():
+                        logging.info(f"Kafka Topic {topic}")
+                        logging.info(f"Messages in Record: {records}")
+                        for record in records:
+                            record_messages.append(record)
+                            if 'type' in record.value['payload']:
+                                event_type = record.value['payload']['type']
+                                # Validate the message value here
+                                if event_type == payload_msg:
+                                    logging.info(f"{payload_msg} has found in the Message")
+                                    is_valid = True
+                                    allure.attach(
+                                        name="Check health DHCP Event Message",
                                         body=str(record))
                                     msg_found = True
                                     break
