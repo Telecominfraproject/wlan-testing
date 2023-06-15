47907aaf66161f0794c59997e8cbd7fd85312888
jitendracandela <jitendra.kushavah@candelatech.com>
2023-06-15 14:43:40 +0530

    Removed unwanted marker
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/e2e/basic/performance_tests/peak_throughput_tests/nat_mode/open_encryption/test_peak_throughput_open_20Mhz_2g.py
+++ tests/e2e/basic/performance_tests/peak_throughput_tests/nat_mode/open_encryption/test_peak_throughput_open_20Mhz_2g.py
@@ -42,7 +42,6 @@ class Test20Mhz2GChannel1PeakThroughput(object):
 
     @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-6934", name="WIFI-6934")
     @pytest.mark.tcp_download
-    @pytest.mark.mum
     def test_client_open_nat_tcp_dl(self, get_test_library, get_dut_logs_per_test_case,
                                     get_test_device_logs, num_stations, setup_configuration):
         """ Wifi Capacity Test NAT mode
