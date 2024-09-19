be7fe82de49b5aaf9a11438ba2d7dccabe3af879
Jitendrakumar Kushavah <78074038+jitendracandela@users.noreply.github.com>
2024-09-19 10:56:37 +0530

    Added all device_statistics and device_reboot_logs in teardown section for each test case (#996)
    * Added all device_statistics and device_reboot_logs in teardown section for each test case
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>
    
    * Added get_device_statistics_teardown and get_device_reboot_logs methods for attaching data in teradown section
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>
    
    * Modified get_dut_logs_per_test_case for attaching the data in teardown
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>
    
    ---------
    
    Signed-off-by: jitendracandela <jitendra.kushavah@candelatech.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -498,7 +498,8 @@ def get_dut_logs_per_test_case(request, run_lf, get_testbed_details, get_target_
             get_target_object.get_dut_library_object().run_generic_command(
                 cmd="logger start testcase: " + instance_name,
                 idx=i)
-
+        start_time = int(time.time())
+        logging.info("start time:- " + str(start_time))
         def collect_logs():
             for i in range(len(get_testbed_details["device_under_tests"])):
                 get_target_object.get_dut_library_object().run_generic_command(
@@ -514,6 +515,32 @@ def get_dut_logs_per_test_case(request, run_lf, get_testbed_details, get_target_
                 uci_show = get_target_object.get_dut_library_object().run_generic_command(idx=i, attach_allure=False,
                     cmd="uci show", print_log=False)
                 allure.attach(body=uci_show, name="uci show")
+                end_time = int(time.time())
+                logging.info("end time:- " + str(end_time))
+                device_name = get_testbed_details["device_under_tests"][i]["identifier"]
+                query_ = f"?startDate={start_time}&endDate={end_time}"
+                resp = get_target_object.controller_library_object.get_device_statistics_teardown(device_name, query=query_)
+                if resp.status_code == 200:
+                    allure.attach(body=str(resp.json()), name="device_statistics_per_test_case")
+                else:
+                    logging.info("resp.status_code:- " + str(resp.status_code))
+                # Check reboot logs
+                query_ = f"?logType=2&startDate={start_time}&endDate={end_time}"
+                resp = get_target_object.controller_library_object.get_device_reboot_logs(device_name, query=query_)
+                if resp.status_code == 200:
+                    allure.attach(body=str(resp.json()), name="device_reboot_logs_per_test_case")
+                    response = resp.json()
+                    # crash log validation
+                    if response["values"]:
+                        resp = get_target_object.controller_library_object.get_device_reboot_logs(device_name,
+                                                                                                  query="?logType=2")
+                        allure.attach(body=str(resp.json()), name="all_device_reboot_logs")
+                        logging.info("Reboot detected on AP side")
+                        pytest.exit("Reboot detected on AP side")
+                else:
+                    logging.info("resp.status_code:- " + str(resp.status_code))
+
+
 
         request.addfinalizer(collect_logs)
 
