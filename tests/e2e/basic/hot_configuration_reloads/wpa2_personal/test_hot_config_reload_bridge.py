2845839563c1d5805605a3d71c26940257ad109d
Anil Kumar Tegala <anil.tegala@candelatech.com>
2024-10-17 16:07:54 +0530

    Add hot reload config test (channel_width parameter) (#1001)
    * added a test case
    
    * Revert "added a test case"
    
    This reverts commit 47291c443f668adfa31412993f3e5075877eabd5.
    
    * add channel_width paramter config test case

--- tests/e2e/basic/hot_configuration_reloads/wpa2_personal/test_hot_config_reload_bridge.py
+++ tests/e2e/basic/hot_configuration_reloads/wpa2_personal/test_hot_config_reload_bridge.py
@@ -77,3 +77,39 @@ class TestHotReloadConfigSuite(object):
 
         assert passes == "PASS", result
 
+
+
+    @pytest.mark.wpa2_personal
+    @pytest.mark.fiveg
+    @pytest.mark.channel_width
+    @allure.story('wpa2_personal 5 GHZ Band')
+    @allure.title("BRIDGE Mode Hot Reload with wpa2_personal encryption 5 GHz Band(Channel width Parameter)")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-14138", name="WIFI-14138")
+    def test_hot_config_reload_channel_width(self, get_test_library, get_test_device_logs,
+                                             get_testbed_details,
+                                             get_dut_logs_per_test_case, get_target_object,
+                                             num_stations, check_connectivity, setup_configuration,
+                                             client_type):
+        """
+            Test Description: The hot reload config feature allows real-time configuration updates without restarting services, minimizing downtime and service disruptions. It’s essential for maintaining continuous operation, especially in critical systems or environments with frequent updates. This feature enhances flexibility, improves scalability, and reduces the risk of human error. Here Channel Width parameter comes under radio (PHY level) of AP, hence it’s modification should restart the service
+            Marker:
+            hot_reload and bridge and general and wpa2_personal and fiveg and channel_width
+        """
+
+        print("----------------setup_configuration:", setup_configuration)
+        profile_data = {"ssid_name": "OpenWifi", "appliedRadios": ["5G"], "security_key": "OpenWifi"}
+        ssid_name = profile_data["ssid_name"]
+        security_key = profile_data["security_key"]
+        security = "wpa2"
+        mode = "BRIDGE"
+        band = "fiveg"
+        reconfig = "channel_width"
+
+        # Hot Reload Configuration
+        passes, result = get_test_library.hot_config_reload_test(ssid=ssid_name, security=security,
+                                                                 dut_data=setup_configuration,
+                                                                 passkey=security_key, mode=mode, band=band,
+                                                                 num_sta=num_stations, tip_2x_obj=get_target_object,
+                                                                 reconfig=reconfig)
+
+        assert passes == "PASS", result
