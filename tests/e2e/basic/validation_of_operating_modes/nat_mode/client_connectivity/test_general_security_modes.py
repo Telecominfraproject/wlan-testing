bab037274fc2314dc7fca5f3c9ed51b340ee76c4
jitendra-kushavah <jitendra.kushavah@candelatech.com>
2025-10-16 07:42:35 +0000

    Added OWE and OWE-Transition test cases for NAT mode
    Signed-off-by: jitendra-kushavah <jitendra.kushavah@candelatech.com>

--- tests/e2e/basic/validation_of_operating_modes/nat_mode/client_connectivity/test_general_security_modes.py
+++ tests/e2e/basic/validation_of_operating_modes/nat_mode/client_connectivity/test_general_security_modes.py
@@ -467,3 +467,323 @@ class TestNatModeConnectivitySuiteTwo(object):
                                                                    num_sta=num_stations, client_type=client_type)
 
         assert passes == "PASS", result
+
+
+setup_params_owe_br = {
+    "mode": "NAT",
+    "ssid_modes": {
+        "owe": [
+            {"ssid_name": "ssid_owe_2g_br", "appliedRadios": ["2G"], "security_key": "something"},
+            {"ssid_name": "ssid_owe_5g_br", "appliedRadios": ["5G"], "security_key": "something"},
+            {"ssid_name": "ssid_owe_6g_br", "appliedRadios": ["6G"], "security_key": "something"},
+
+        ]
+    },
+    "rf": {},
+    "radius": False
+}
+
+
+@allure.feature("Client Connectivity")
+@allure.parent_suite("Client Connectivity Tests")
+@allure.suite(suite_name="NAT Mode")
+@allure.sub_suite(sub_suite_name="OWE security mode Client Connectivity")
+@pytest.mark.parametrize(
+    'setup_configuration',
+    [setup_params_owe_br],
+    indirect=True,
+    scope="class"
+)
+@pytest.mark.usefixtures("setup_configuration")
+class TestNatModeConnectivityOWE(object):
+    """
+        NAT Client Connectivity (OWE)
+        pytest -m "client_connectivity_tests and nat and owe"
+    """
+
+    @pytest.mark.owe
+    @pytest.mark.twog
+    @allure.story('OWE 2.4 GHZ Band')
+    @allure.title("NAT Mode Client Connectivity Test with OWE encryption 2.4 GHz Band")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13669", name="WIFI-13669")
+    def test_nat_owe_2g_client_connectivity(self, get_test_library, get_dut_logs_per_test_case,
+                                               get_test_device_logs, num_stations, check_connectivity,
+                                               setup_configuration, client_type):
+        """
+            NAT Mode Client Connectivity Test with OWE encryption 2.4 GHz Band
+            marker:- client_connectivity_tests and nat and owe and twog
+        """
+        profile_data = {"ssid_name": "ssid_owe_2g_br", "appliedRadios": ["2G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "owe"
+        mode = "NAT-WAN"
+        band = "twog"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+    @pytest.mark.owe
+    @pytest.mark.fiveg
+    @allure.story('OWE 5 GHZ Band')
+    @allure.title("NAT Mode Client Connectivity Test with OWE encryption 5 GHz Band")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13669", name="WIFI-13669")
+    def test_nat_owe_5g_client_connectivity(self, get_test_library, get_dut_logs_per_test_case,
+                                               get_test_device_logs, num_stations, check_connectivity,
+                                               setup_configuration, client_type):
+        """
+            NAT Mode Client Connectivity Test with OWE encryption 5 GHz Band
+            marker:- client_connectivity_tests and nat and owe and fiveg
+        """
+        profile_data = {"ssid_name": "ssid_owe_5g_br", "appliedRadios": ["5G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "owe"
+        mode = "NAT-WAN"
+        band = "fiveg"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+    @pytest.mark.owe
+    @pytest.mark.twog
+    @pytest.mark.sixg
+    @allure.story('OWE 6 GHZ Band')
+    @allure.title("NAT Mode Client Connectivity Test with OWE encryption 6 GHz Band")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13669", name="WIFI-13669")
+    def test_nat_owe_6g_client_connectivity(self, get_test_library, get_dut_logs_per_test_case,
+                                               get_test_device_logs, num_stations, check_connectivity,
+                                               setup_configuration, client_type):
+        """
+            NAT Mode Client Connectivity Test with OWE encryption 6 GHz Band
+            marker:- client_connectivity_tests and nat and owe and sixg
+        """
+        profile_data = {"ssid_name": "ssid_owe_6g_br", "appliedRadios": ["6G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "owe"
+        mode = "NAT-WAN"
+        band = "sixg"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+
+setup_params_owe_transition_br = {
+    "mode": "NAT",
+    "ssid_modes": {
+        "owe_transition": [
+            {"ssid_name": "ssid_owe_transition_2g_br", "appliedRadios": ["2G"], "security_key": "something"},
+            {"ssid_name": "ssid_owe_transition_5g_br", "appliedRadios": ["5G"], "security_key": "something"},
+            {"ssid_name": "ssid_owe_transition_6g_br", "appliedRadios": ["6G"], "security_key": "something"}
+
+        ]
+    },
+    "rf": {},
+    "radius": False
+}
+
+
+@allure.feature("Client Connectivity")
+@allure.parent_suite("Client Connectivity Tests")
+@allure.suite(suite_name="NAT Mode")
+@allure.sub_suite(sub_suite_name="OWE-Transition security mode Client Connectivity")
+@pytest.mark.parametrize(
+    'setup_configuration',
+    [setup_params_owe_transition_br],
+    indirect=True,
+    scope="class"
+)
+@pytest.mark.usefixtures("setup_configuration")
+class TestNatModeConnectivityOWETransition(object):
+    """
+        NAT Client Connectivity (OWE-Transition)
+        pytest -m "client_connectivity_tests and nat and owe_transition"
+    """
+
+    @pytest.mark.owe_transition
+    @pytest.mark.twog
+    @pytest.mark.owe_client
+    @allure.story('OWE-Transition 2.4 GHZ Band - OWE Client')
+    @allure.title("NAT Mode OWE-Transition: OWE client connects to hidden SSID")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13674", name="WIFI-13674")
+    def test_nat_owe_transition_2g_owe_client(self, get_test_library, get_dut_logs_per_test_case,
+                                                 get_test_device_logs, num_stations, check_connectivity,
+                                                 setup_configuration, client_type):
+        """
+            NAT Mode OWE-Transition: OWE client connects to SSID in OWE mode
+            marker:- client_connectivity_tests and nat and owe_transition and twog and owe_client
+        """
+        profile_data = {"ssid_name": "ssid_owe_transition_2g_br", "appliedRadios": ["2G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "owe"
+        mode = "NAT-WAN"
+        band = "twog"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+    @pytest.mark.owe_transition
+    @pytest.mark.twog
+    @pytest.mark.non_owe_client
+    @allure.story('OWE-Transition 2.4 GHZ Band - Non-OWE Client')
+    @allure.title("NAT Mode OWE-Transition: Non-OWE client connects to SSID in open mode")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13673", name="WIFI-13673")
+    def test_nat_owe_transition_2g_non_owe_client(self, get_test_library, get_dut_logs_per_test_case,
+                                                     get_test_device_logs, num_stations, check_connectivity,
+                                                     setup_configuration, client_type):
+        """
+            NAT Mode OWE-Transition: Non-OWE client connects to SSID in open mode
+            marker:- client_connectivity_tests and nat and owe_transition and twog and non_owe_client
+        """
+        profile_data = {"ssid_name": "ssid_owe_transition_2g_br", "appliedRadios": ["2G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "open"
+        mode = "NAT-WAN"
+        band = "twog"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+    @pytest.mark.owe_transition
+    @pytest.mark.fiveg
+    @pytest.mark.owe_client
+    @allure.story('OWE-Transition 5 GHZ Band - OWE Client')
+    @allure.title("NAT Mode OWE-Transition: OWE client connects to hidden SSID")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13674", name="WIFI-13674")
+    def test_nat_owe_transition_5g_owe_client(self, get_test_library, get_dut_logs_per_test_case,
+                                                 get_test_device_logs, num_stations, check_connectivity,
+                                                 setup_configuration, client_type):
+        """
+            NAT Mode OWE-Transition: OWE client connects to SSID in OWE mode
+            marker:- client_connectivity_tests and nat and owe_transition and fiveg and owe_client
+        """
+        profile_data = {"ssid_name": "ssid_owe_transition_5g_br", "appliedRadios": ["5G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "owe"
+        mode = "NAT-WAN"
+        band = "fiveg"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+    @pytest.mark.owe_transition
+    @pytest.mark.fiveg
+    @pytest.mark.non_owe_client
+    @allure.story('OWE-Transition 5 GHZ Band - Non-OWE Client')
+    @allure.title("NAT Mode OWE-Transition: Non-OWE client connects to SSID in open mode")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13673", name="WIFI-13673")
+    def test_nat_owe_transition_5g_non_owe_client(self, get_test_library, get_dut_logs_per_test_case,
+                                                     get_test_device_logs, num_stations, check_connectivity,
+                                                     setup_configuration, client_type):
+        """
+            NAT Mode OWE-Transition: Non-OWE client connects to SSID in open mode
+            marker:- client_connectivity_tests and nat and owe_transition and fiveg and non_owe_client
+        """
+        profile_data = {"ssid_name": "ssid_owe_transition_5g_br", "appliedRadios": ["5G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "open"
+        mode = "NAT-WAN"
+        band = "fiveg"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+    @pytest.mark.owe_transition
+    @pytest.mark.twog
+    @pytest.mark.sixg
+    @pytest.mark.owe_client
+    @allure.story('OWE-Transition 6 GHZ Band - OWE Client')
+    @allure.title("NAT Mode OWE-Transition: OWE client connects to hidden SSID")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13674", name="WIFI-13674")
+    def test_nat_owe_transition_6g_owe_client(self, get_test_library, get_dut_logs_per_test_case,
+                                                 get_test_device_logs, num_stations, check_connectivity,
+                                                 setup_configuration, client_type):
+        """
+            NAT Mode OWE-Transition: OWE client connects to SSID in OWE mode
+            marker:- client_connectivity_tests and nat and owe_transition and sixg and owe_client
+        """
+        profile_data = {"ssid_name": "ssid_owe_transition_6g_br", "appliedRadios": ["6G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "owe"
+        mode = "NAT-WAN"
+        band = "sixg"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+    @pytest.mark.owe_transition
+    @pytest.mark.twog
+    @pytest.mark.sixg
+    @pytest.mark.non_owe_client
+    @allure.story('OWE-Transition 6 GHZ Band - Non-OWE Client')
+    @allure.title("NAT Mode OWE-Transition: Non-OWE client connects to SSID in open mode")
+    @allure.testcase(url="https://telecominfraproject.atlassian.net/browse/WIFI-13673", name="WIFI-13673")
+    def test_nat_owe_transition_6g_non_owe_client(self, get_test_library, get_dut_logs_per_test_case,
+                                                     get_test_device_logs, num_stations, check_connectivity,
+                                                     setup_configuration, client_type):
+        """
+            NAT Mode OWE-Transition: Non-OWE client connects to SSID in open mode
+            marker:- client_connectivity_tests and nat and owe_transition and sixg and non_owe_client
+        """
+        profile_data = {"ssid_name": "ssid_owe_transition_6g_br", "appliedRadios": ["6G"], "security_key": ""}
+        ssid_name = profile_data["ssid_name"]
+        security_key = "[BLANK]"
+        security = "open"
+        mode = "NAT-WAN"
+        band = "sixg"
+
+        passes, result = get_test_library.client_connectivity_test(
+            ssid=ssid_name, security=security,
+            dut_data=setup_configuration,
+            passkey=security_key, mode=mode, band=band,
+            num_sta=num_stations, client_type=client_type, extra_sta_rows=["security"]
+        )
+        assert passes == "PASS", result
+
+
